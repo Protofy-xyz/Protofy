6a231012f6458c1d302d0e9dbf0ca504fadc3806
MiquelCos <miquel@protofy.xyz>
2023-09-22 12:52:07 +0200

    Adds default icon to icon component

--- packages/visualui/src/components/Icon.tsx
+++ packages/visualui/src/components/Icon.tsx
@@ -1,7 +1,7 @@
 import { icons } from 'lucide-react';
 
 const Icon = ({ name, color, size }) => {
-    const LucideIcon = icons[name];
+    const LucideIcon = icons[name] ?? icons['Component'];
     return <LucideIcon color={color} size={size} />;
 };
 
