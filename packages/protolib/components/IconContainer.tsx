61a056bc673111bab1cba8ea520f59306bbcc873
jcarlosn <jcarlos.norte@gmail.com>
2023-09-26 00:28:20 +0200

    Extend icon container

--- packages/protolib/components/IconContainer.tsx
+++ packages/protolib/components/IconContainer.tsx
@@ -1,7 +1,7 @@
-import {Stack, StackProps} from 'tamagui'
+import {Stack, StackProps, XStack} from 'tamagui'
 
 export const IconContainer = (props:StackProps) => (
-    <Stack paddingHorizontal={"$3"} opacity={0.5} cursor="pointer" hoverStyle={{ opacity: 0.8 }} {...props}>
+    <XStack paddingHorizontal={"$3"} opacity={0.5} cursor="pointer" hoverStyle={{ opacity: 0.8 }} {...props}>
         {props.children}
-    </Stack>
+    </XStack>
 )
