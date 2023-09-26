5ccb740f62dafd299436589260718ae92bb771ce
MiquelCos <miquel@protofy.xyz>
2023-09-26 10:30:51 +0200

    Changes page component background color

--- packages/protolib/components/Page.tsx
+++ packages/protolib/components/Page.tsx
@@ -3,7 +3,7 @@ import { StackProps, YStack } from "tamagui";
 
 export const Page = React.forwardRef((props: StackProps, ref: any) => {
     return (
-        <YStack ref={ref} flex={1} height={"100%"} style={{overflowX:"hidden"}} {...props}>
+        <YStack ref={ref} flex={1} backgroundColor={"$background"} height={"100%"} style={{overflowX:"hidden"}} {...props}>
             {props.children}
         </YStack>
     );
