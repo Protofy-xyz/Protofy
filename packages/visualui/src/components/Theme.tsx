5d0ca6245f48194edc1b146860e707637b5f4cc4
MiquelCos <miquel@protofy.xyz>
2023-09-27 11:17:20 +0200

    Changes flow theme colors

--- packages/visualui/src/components/Theme.tsx
+++ packages/visualui/src/components/Theme.tsx
@@ -1,11 +1,12 @@
 export default {
     nodeBackgroundColor: '#252526',
     borderColor: 'grey',
-    edgeColor: '#252526',
-    blockPort: '#252526',
-    flowPort: '#252526',
-    dataPort: '#252526',
+    blockPort: 'grey',
+    flowPort: 'grey',
+    dataPort: 'grey',
     textColor: '#CCCCCC',
     dataOutputColor: 'grey',
-    nodeBorderColor: '#CCCCCC'
+    nodeBorderColor: 'grey',
+    edgeColor: 'grey',
+    titleColor: 'black'
 }
