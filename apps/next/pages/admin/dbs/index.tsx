eb3f74b6ce9099b6adda0a94fe9bdbad03ef0a97
jcarlosn <jcarlos.norte@gmail.com>
2023-09-27 23:12:50 +0200

    Refactor admin panel to split pages into its own files

---
+++ apps/next/pages/admin/dbs/index.tsx
@@ -0,0 +1,58 @@
+import AdminPanel from 'app/features/admin'
+import Head from 'next/head'
+import { SSR } from '../../../conf'
+import { NextPageContext } from 'next'
+import { API, redirect, withSession } from 'protolib'
+import dynamic from 'next/dynamic'
+import { useRouter } from 'next/router';
+import { promises as fs } from 'fs';
+import fsSync from 'fs'
+
+const FileBrowser = dynamic(() => import('../../../components/FileBrowser'), {
+  ssr: false,
+})
+
+export default function FilesPage(props:any) {
+  const router = useRouter();
+  const { name } = router.query;
+  return (
+    <>
+      <Head>
+        <title>Protofy - Admin Panel</title>
+      </Head>
+      <AdminPanel {...props} FileBrowser={FileBrowser} />
+    </>
+  )
+}
+
+export const getServerSideProps = SSR(async (context:NextPageContext) => {
+    const nameSegments = context.query.name as string[];
+    
+    let props = {}
+    const dbs = await API.get('/adminapi/v1/databases') ?? { data: [] }
+
+
+    if (!context.query.name && dbs.data.length) {
+        return redirect('/admin/dbs/' + dbs.data[0].name)
+    }
+
+    const db = dbs.data.find((db:any) => db.name == nameSegments[0])
+    if (!db) {
+        return dbs.data.length ?
+            redirect('/admin/dbs/' + dbs.data[0].name)
+            :
+            redirect('/admin')
+    }
+    props = {
+      data: {
+        databasesState: dbs,
+        currentDbState: db.name,
+        contentState: dbs.data.length ? await API.get('/adminapi/v1/databases/' + db.name) : []
+      }
+    }
+
+    return withSession(context, ['admin'], {
+      ...props,
+      workspace: await API.get('/adminapi/v1/workspaces')
+    })
+})
---
+++ apps/next/pages/admin/files/index.tsx
@@ -0,0 +1,49 @@
+import AdminPanel from 'app/features/admin'
+import Head from 'next/head'
+import { SSR } from '../../../conf'
+import { NextPageContext } from 'next'
+import { API, redirect, withSession } from 'protolib'
+import dynamic from 'next/dynamic'
+import { useRouter } from 'next/router';
+import { promises as fs } from 'fs';
+import fsSync from 'fs'
+
+const FileBrowser = dynamic(() => import('../../../components/FileBrowser'), {
+  ssr: false,
+})
+
+export default function FilesPage(props:any) {
+  const router = useRouter();
+  const { name } = router.query;
+  return (
+    <>
+      <Head>
+        <title>Protofy - Admin Panel</title>
+      </Head>
+      <AdminPanel {...props} FileBrowser={FileBrowser} />
+    </>
+  )
+}
+
+export const getServerSideProps = SSR(async (context:NextPageContext) => {
+    const nameSegments = context.query.name as string[];
+    
+    let props = {}
+    const path = nameSegments ? nameSegments.join('/') : '';
+    //@ts-ignore
+    const currentFile = context.query.file ? context.query.file.split('/')[0] : ''
+    props = {
+      data: {
+        filesState: await API.get('/adminapi/v1/files/'+path) ?? { data: [] },
+        CurrentPath: path,
+        CurrentFile: currentFile
+      }
+    }
+    console.log('Requesting to: *****************', '/adminapi/v1/files/'+path)
+    
+
+    return withSession(context, ['admin'], {
+      ...props,
+      workspace: await API.get('/adminapi/v1/workspaces')
+    })
+})
