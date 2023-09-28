6f1fef8bb70c9760e1dafead29212288a9ae01fc
jcarlosn <jcarlos.norte@gmail.com>
2023-09-28 14:11:19 +0200

    Fixes incorrect import

--- apps/admin-api/src/modules/workspaces.ts
+++ apps/admin-api/src/modules/workspaces.ts
@@ -2,7 +2,7 @@ import {app} from '../lib/app';
 import { handler } from '../lib/handler';
 import * as fs from 'fs';
 import { getDatabases } from './databases';
-import { WorkspaceModel } from 'common';
+import { WorkspaceModel } from 'protolib';
 
 app.get('/adminapi/v1/workspaces', handler(async (req, res) => {
     const dbs = await getDatabases()
