2b3df4355cb3f64927e206cb46dbf4fc0a831b14
jcarlosn <jcarlos.norte@gmail.com>
2023-10-05 16:44:24 +0200

    Last fix to make it work

--- packages/app/features/visualui/components/site/index.tsx
+++ packages/app/features/visualui/components/site/index.tsx
@@ -1,5 +1,5 @@
 import { DefaultLayout } from '../../../../layout/DefaultLayout'
-import { getComponentWrapper, BasicPlaceHolder } from 'protolib'
+import { getComponentWrapper, BasicPlaceHolder } from 'protolib/visualui/visualuiWrapper'
 
 export default {
     ...(getComponentWrapper("@/layout/DefaultLayout"))(DefaultLayout, 'EyeOff', 'DefaultLayout', {}, {}, { children: <BasicPlaceHolder /> }),
