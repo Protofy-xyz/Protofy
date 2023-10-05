59f5743dc3a3b93ad64e6dd43fd2c6cf4f16ba8d
jcarlosn <jcarlos.norte@gmail.com>
2023-10-05 05:37:41 +0200

    Extracts BaseAPI

---
+++ packages/protolib/base/BaseApi.ts
@@ -0,0 +1,52 @@
+import { connectDB, getDB, handler } from "../api";
+
+export const BaseApi = (app, entityName, modelClass, initialData) => {
+    const dbPath = '../../data/databases/'+entityName
+    connectDB(dbPath, initialData) //preconnect database
+
+    //list
+    app.get('/api/v1/'+entityName, handler(async (req, res, session) => {
+        const db = getDB(dbPath)
+        const total: any[] = []
+        for await (const [key, value] of db.iterator()) {
+            if(key != 'initialized' && !modelClass.unserialize(value, session).isDeleted()) total.push(modelClass.unserialize(value, session).list())
+        }
+        res.send(total)
+    }));
+
+    //create
+    app.post('/api/v1/'+entityName, handler(async (req, res, session) => {
+        const db = getDB(dbPath)
+        const entityModel = modelClass.load(req.body, session).create()
+        await db.put(entityModel.getId(), entityModel.serialize())
+        res.send(entityModel.read())
+    }));
+
+    //read
+    app.get('/api/v1/'+entityName+'/:key', handler(async (req, res, session) => {
+        const db = getDB(dbPath)
+        try {
+            const content = modelClass.unserialize(await db.get(req.params.key), session).read()
+            res.send(content)
+        } catch(e) {
+            console.error("Error reading from database: ", e)
+            res.status(404).send({result: "not found"})
+        }
+    }));
+
+    //update
+    app.post('/api/v1/'+entityName+'/:key', handler(async (req, res, session) => {
+        const db = getDB(dbPath)
+        const entityModel = modelClass.unserialize(await db.get(req.params.key), session).update(modelClass.load(req.body, session).validate())              
+        await db.put(entityModel.getId(), entityModel.serialize())
+        res.send(entityModel.read())
+    }));
+
+    //delete
+    app.get('/api/v1/'+entityName+'/:key/delete', handler(async (req, res, session) => {
+        const db = getDB(dbPath)
+        const entityModel = modelClass.unserialize(await db.get(req.params.key), session).delete()
+        await db.put(entityModel.getId(), entityModel.serialize())
+        res.send({"result": "deleted"})
+    }));
+}
