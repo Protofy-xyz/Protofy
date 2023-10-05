185c09e57a9ed38c2721ce7603177539952d8ae4
MarcPous <marc@protofy.xyz>
2023-10-05 09:59:33 +0000

    [fix] changes route name dbs for database

--- packages/protolib/adminpanel/next/database.tsx
+++ packages/protolib/adminpanel/next/database.tsx
@@ -29,19 +29,19 @@ export const getServerSideProps = SSR(async (context:NextPageContext) => {
 
 
     if (!context.query.name && dbs.data.length) {
-        return redirect('/admin/dbs/' + dbs.data[0].name)
+        return redirect('/admin/database/' + dbs.data[0].name)
     }
 
-    const db = dbs.data.find((db:any) => db.name == nameSegments[0])
+    const db = dbs.data?.find((db:any) => db.name == nameSegments[0])
     if (!db) {
-        return dbs.data.length ?
-            redirect('/admin/dbs/' + dbs.data[0].name)
+        return dbs.data?.length ?
+            redirect('/admin/database/' + dbs.data[0].name)
             :
             redirect('/admin')
     }
     props = {
       data: {
-        contentState: dbs.data.length ? await API.get('/adminapi/v1/databases/' + db.name) : []
+        contentState: dbs.data?.length ? await API.get('/adminapi/v1/databases/' + db.name) : []
       }
     }
 
