ec8cc715ada891c163f875ce75971b77077df57d
jcarlosn <jcarlos.norte@gmail.com>
2023-10-06 01:15:32 +0200

    Deleted notes api and use ObjectListView in note page

--- apps/api/src/modules/notes.ts
+++
@@ -1,9 +0,0 @@
-import { connectDB, getDB, response, app } from 'protolib/api';
-import { getInitialData } from 'app/initialData';
-
-const dbPath = '../../data/databases/db'
-connectDB(dbPath, getInitialData(dbPath)) //preconnect database
-
-app.get('/api/v1/notes', async (req, res) => {
-    response(getDB(dbPath).get('notes'), res)
-});
--- packages/app/features/notes.tsx
+++ packages/app/features/notes.tsx
@@ -1,29 +1,10 @@
 
 import { DefaultLayout } from '../layout/DefaultLayout'
-import { XStack, YStack, Stack } from 'tamagui'
-import { useAtom, useTouchSensors, SortableItem, Page, getPendingResult, EditableText, AsyncView, usePendingEffect, createApiAtom, API, Grid, SpotLight, Section, TamaCard, BigTitle } from 'protolib'
-import { arrayMove, SortableContext, rectSortingStrategy} from '@dnd-kit/sortable';
-import { DndContext, closestCenter } from '@dnd-kit/core';
-
-const notesAtom = createApiAtom([])
+import { XStack, YStack } from 'tamagui'
+import { Page, SpotLight, Section, BigTitle } from 'protolib'
+import { ObjectListView } from 'protolib/base'
 
 export default function Notes({ initialNotes }) {
-  const [notes,setNotes] = useAtom(notesAtom, initialNotes)
-  usePendingEffect((s) => API.get('/api/v1/notes', s), setNotes, initialNotes)
-  
-  const handleDragEnd = (event) => {
-    const { active, over } = event;
-    if(over && over.id && active && active.id) {
-      setNotes((items) => {
-        const oldIndex = items.data.findIndex(ele => ele.id == active.id);
-        const newIndex = items.data.findIndex(ele => ele.id == over.id);
-        return getPendingResult('loaded', arrayMove(items.data, oldIndex, newIndex));
-      });
-    }
-  }
-
-  const sensors = useTouchSensors()
-
   return (
     <Page>
       <DefaultLayout title="notes" header={null} footer={null}>
@@ -36,24 +17,7 @@ export default function Notes({ initialNotes }) {
                 <span className="all ease-in ms250 rainbow clip-text">Notes</span>
               </BigTitle>
             </XStack>
-            <AsyncView atom={notes}>
-              <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
-                <SortableContext
-                  items={notes?.data}
-                  strategy={rectSortingStrategy}
-                >
-                  <Grid itemMinWidth={400}>
-                    {notes?.data?.map(({ title, description, id }, index) => <SortableItem key={id} id={id}>
-                      <Stack style={{touchAction: 'none'}}>
-                        <TamaCard grow={0} title={title} >
-                            <EditableText placeHolder={''} text={description} />
-                        </TamaCard>
-                      </Stack>
-                    </SortableItem>)}
-                  </Grid>
-                </SortableContext>
-              </DndContext>
-            </AsyncView>
+            <ObjectListView initialElements={initialNotes} name={'notes'}/>
           </YStack>
         </Section>
       </DefaultLayout>
