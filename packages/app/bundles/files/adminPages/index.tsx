37f19c4d0cc379049d62fb73662d713c671c1649
jcarlosn <jcarlos.norte@gmail.com>
2023-10-08 01:03:56 +0200

    Move files bundle into protolib

--- packages/app/bundles/files/adminPages/index.tsx
+++
@@ -1,5 +0,0 @@
-import FilesPage, {getServerSideProps} from 'protolib/adminpanel/next/files'
-
-export default {
-    'admin/files/**': {component: FilesPage, getServerSideProps: getServerSideProps},
-}
--- packages/app/bundles/nextPages.ts
+++ packages/app/bundles/nextPages.ts
@@ -1,10 +1,13 @@
 import notesPages from './notes/nextPages'
 import databasePages from './database/adminPages'
 import devicePages from './devices/adminPages'
-import filesPages from './files/adminPages'
+import filesPages from 'protolib/adminpanel/bundles/files/adminPages'
+import usersPages from 'protolib/adminpanel/bundles/users/adminPages'
+
 export default {
     ...notesPages,
     ...databasePages,
     ...devicePages,
-    ...filesPages
+    ...filesPages,
+    ...usersPages
 }
