c4a517f34ac3b25ab9a57c6bde4419f903a7076f
jcarlosn <jcarlos.norte@gmail.com>
2023-10-07 03:40:52 +0200

    Adds props to center

--- packages/protolib/components/Center.tsx
+++ packages/protolib/components/Center.tsx
@@ -1,8 +1,8 @@
 import { StackProps, YStack } from "tamagui"
 import React from 'react'
 
-const Center = React.forwardRef(({children}: StackProps, ref:any) => (
-    <YStack flex={1} alignItems="center" justifyContent="center" space="$4" ref={ref}>
+const Center = React.forwardRef(({children, ...props}: StackProps, ref:any) => (
+    <YStack flex={1} alignItems="center" justifyContent="center" space="$4" ref={ref} {...props}>
         {children}
     </YStack>
 ))
