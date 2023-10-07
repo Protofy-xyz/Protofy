02af6d081fdacc6e0d3875b7ec1780362824cd02
jcarlosn <jcarlos.norte@gmail.com>
2023-10-07 23:57:29 +0200

    Move devices into a bundle

---
+++ packages/app/bundles/devices/adminPages/index.tsx
@@ -0,0 +1,6 @@
+import DevicePage, {getServerSideProps} from 'protolib/adminpanel/next/devices'
+
+
+export default {
+    'admin/devices/*': {component: DevicePage, getServerSideProps: getServerSideProps},
+}
--- packages/app/bundles/nextPages.ts
+++ packages/app/bundles/nextPages.ts
@@ -1,7 +1,8 @@
 import notesPages from './notes/nextPages'
 import databasePages from './database/adminPages'
-
+import devicePages from './devices/adminPages'
 export default {
     ...notesPages,
-    ...databasePages
+    ...databasePages,
+    ...devicePages
 }
