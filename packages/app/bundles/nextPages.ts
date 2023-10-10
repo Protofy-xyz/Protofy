8894a8b23095a431681d1b6c39c4d0db42e7123b
jcarlosn <jcarlos.norte@gmail.com>
2023-10-10 05:47:21 +0200

    connects workflow and events pages

--- packages/app/bundles/nextPages.ts
+++ packages/app/bundles/nextPages.ts
@@ -3,11 +3,14 @@ import databasePages from 'protolib/adminpanel/bundles/database/adminPages'
 import devicePages from 'protolib/adminpanel/bundles/devices/adminPages'
 import filesPages from 'protolib/adminpanel/bundles/files/adminPages'
 import usersPages from 'protolib/adminpanel/bundles/users/adminPages'
-
+import eventsPages from 'protolib/adminpanel/bundles/events/adminPages'
+import automationPages from 'protolib/adminpanel/bundles/automation/adminPages'
 export default {
     ...notesPages,
     ...databasePages,
     ...devicePages,
     ...filesPages,
-    ...usersPages
+    ...usersPages,
+    ...eventsPages,
+    ...automationPages
 }
