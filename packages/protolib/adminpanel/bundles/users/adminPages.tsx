9e7fb94ad1621542e49670b25d4d501fbe412a95
jcarlosn <jcarlos.norte@gmail.com>
2023-10-12 05:35:48 +0200

    Simplify datatable visually

--- packages/protolib/adminpanel/bundles/users/adminPages.tsx
+++ packages/protolib/adminpanel/bundles/users/adminPages.tsx
@@ -22,11 +22,11 @@ export default {
                         return finalData
                     }}
                     columns={DataTable2.columns(
-                        DataTable2.column("email", "username", true, row => <Chip text={row.username} color={'$color5'} />),
+                        DataTable2.column("email", "username", true),
                         DataTable2.column("type", "type", true, row => <Chip text={row.type.toUpperCase()} color={row.type == 'admin' ? '$blue5':'$color5'} />),
                         DataTable2.column("from", "from", true, row => <Chip text={row.from.toUpperCase()} color={row.from == 'cmd' ? '$blue5':'$color5'} />),
-                        DataTable2.column("created", "createdAt", true, row => <Chip text={moment(row.createdAt).format(format)} color={'$color5'} />),
-                        DataTable2.column("last login", "lastLogin",true, row => row.lastLogin ? <Chip text={moment(row.lastLogin).format(format)} color={'$color5'} /> : <Chip text={'NEVER'} color={'$red5'} /> )
+                        DataTable2.column("created", "createdAt", true, row => moment(row.createdAt).format(format)),
+                        DataTable2.column("last login", "lastLogin",true, row => row.lastLogin ? <Chip text={moment(row.lastLogin).format(format)} color={'$color5'} /> : <Chip text={'NEVER'} color={'$yellow6'} /> )
                     )}
                     extraFields={{ 
                         repassword: z.string().min(6).label('repeat password').after('password').hint('**********').secret()
