86e7f5abc5158dc53e40e0bb666710fd32db511d
jcarlosn <jcarlos.norte@gmail.com>
2023-10-12 02:34:31 +0200

    Improved error handling

--- packages/ui/src/lib/error.tsx
+++ packages/ui/src/lib/error.tsx
@@ -1,25 +1,27 @@
 import { YStack, Text, Spacer } from "tamagui"
 
 export const getErrorMessage = (err) => {
-    if(typeof err == 'string') return err
-    return (
-      <YStack>
+  console.log('error: ', err)
+  return (
+    <YStack>
+      {typeof err == 'string' ? <Text color="$red9" fontWeight={"bold"}>Error: {err}</Text> : <>
         <Text>Error:</Text>
         <Spacer />
         {Object.keys(err.fieldErrors).map((field) => {
           return <Text>
             - [<Text color="$red9" fontWeight={"bold"}>{field}</Text>] {err.fieldErrors[field]}
           </Text>
-        })}
-      </YStack>
-    )
+        })}</>
+      }
+    </YStack>
+  )
 }
 
 export const getValidation = (field: string, state: any) => {
-  const isError = state?.error?.fieldErrors? state?.error?.fieldErrors[field] : false
+  const isError = state?.error?.fieldErrors ? state?.error?.fieldErrors[field] : false
   const props = isError ? {
     boc: "$red9"
-  }:{}
+  } : {}
   return {
     ...props,
     hoverStyle: {
