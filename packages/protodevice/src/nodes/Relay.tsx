0fb3ceb05abef0a750d1410db10ab2869521d0f9
lluis@protofy.xyz <lluis@protofy.xyz>
2023-10-16 06:06:10 +0200

    remove unused modifiers for input fierl

--- packages/protodevice/src/nodes/Relay.tsx
+++ packages/protodevice/src/nodes/Relay.tsx
@@ -3,7 +3,7 @@ import { Node, Field, HandleOutput, NodeParams } from 'protoflow';
 
 const Relay = (node:any={}, nodeData={}, children) => {
     const nodeParams: Field[] = [
-        { label: 'Name', static:true, field: 'param1', type: 'input', pre:(str) =>str.replace(/['"]+/g, ''), post: (str) => '"'+str.toLowerCase()+'"' },
+        { label: 'Name', static:true, field: 'param1', type: 'input'},
         { label: 'Restore Mode', static:true, field: 'param2', type: 'select', data:['"ALWAYS_ON"', '"ALWAYS_OFF"'] }
     ] as Field[]
     return (
