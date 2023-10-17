e859f0aef7f68a23687299d82424b10290693f97
jcarlosn <jcarlos.norte@gmail.com>
2023-10-17 03:53:09 +0200

    Adds widget selectlist

--- packages/protolib/components/EditableObject.tsx
+++ packages/protolib/components/EditableObject.tsx
@@ -1,6 +1,6 @@
 import { Button, Fieldset, Input, Label, Stack, XStack, YStack, Paragraph, Spinner, Text, Dialog, SelectProps, Select, Adapt, Sheet, getFontSize } from "tamagui";
 import { Pencil, ChevronDown, ChevronUp, Check } from '@tamagui/lucide-icons';
-import { AsyncView, usePendingEffect, API, Tinted, Notice, getPendingResult } from 'protolib'
+import { AsyncView, usePendingEffect, API, Tinted, Notice, getPendingResult, SelectList } from 'protolib'
 import { ProtoModel } from "protolib/base";
 import React, { useEffect, useMemo, useState } from "react";
 import { getErrorMessage } from "@my/ui";
@@ -25,125 +25,6 @@ type EditableObjectProps = {
     spinnerSize?:number,
     name?:string
 }
-
-function SelectList({title, value, elements, setValue, ...props}: SelectProps & {title: any, elements: any[], value: any, setValue: Function}) {
-    const [val, setVal] = useState('apple')
-  
-    return (
-      <Select
-        id="select"
-        value={val}
-        onValueChange={setVal}
-        disablePreventBodyScroll
-        {...props}
-      >
-        <Select.Trigger width={220} iconAfter={ChevronDown}>
-          <Select.Value placeholder="..." />
-        </Select.Trigger>
-  
-        <Adapt when="sm" platform="touch">
-          <Sheet
-            native={!!props.native}
-            modal
-            dismissOnSnapToBottom
-            animationConfig={{
-              type: 'spring',
-              damping: 20,
-              mass: 1.2,
-              stiffness: 250,
-            }}
-          >
-            <Sheet.Frame>
-              <Sheet.ScrollView>
-                <Adapt.Contents />
-              </Sheet.ScrollView>
-            </Sheet.Frame>
-            <Sheet.Overlay
-              animation="lazy"
-              enterStyle={{ opacity: 0 }}
-              exitStyle={{ opacity: 0 }}
-            />
-          </Sheet>
-        </Adapt>
-  
-        <Select.Content zIndex={200000}>
-          <Select.ScrollUpButton
-            alignItems="center"
-            justifyContent="center"
-            position="relative"
-            width="100%"
-            height="$3"
-          >
-            <YStack zIndex={10}>
-              <ChevronUp size={20} />
-            </YStack>
-
-          </Select.ScrollUpButton>
-  
-          <Select.Viewport
-            // to do animations:
-            // animation="quick"
-            // animateOnly={['transform', 'opacity']}
-            // enterStyle={{ o: 0, y: -10 }}
-            // exitStyle={{ o: 0, y: 10 }}
-            minWidth={200}
-          >
-            <Select.Group>
-              <Select.Label>{title}</Select.Label>
-              {/* for longer lists memoizing these is useful */}
-              {useMemo(
-                () =>
-                elements.map((item, i) => {
-                    return (
-                      <Select.Item
-                        debug="verbose"
-                        index={i}
-                        key={item}
-                        value={item}
-                      >
-                        <Select.ItemText>{item}</Select.ItemText>
-                        <Select.ItemIndicator marginLeft="auto">
-                          <Check size={16} />
-                        </Select.ItemIndicator>
-                      </Select.Item>
-                    )
-                  }),
-                [elements]
-              )}
-            </Select.Group>
-            {/* Native gets an extra icon */}
-            {props.native && (
-              <YStack
-                position="absolute"
-                right={0}
-                top={0}
-                bottom={0}
-                alignItems="center"
-                justifyContent="center"
-                width={'$4'}
-                pointerEvents="none"
-              >
-                <ChevronDown size={getFontSize((props.size ?? '$true') as any)} />
-              </YStack>
-            )}
-          </Select.Viewport>
-  
-          <Select.ScrollDownButton
-            alignItems="center"
-            justifyContent="center"
-            position="relative"
-            width="100%"
-            height="$3"
-          >
-            <YStack zIndex={10}>
-              <ChevronDown size={20} />
-            </YStack>
-
-          </Select.ScrollDownButton>
-        </Select.Content>
-      </Select>
-    )
-  }
 
 const capitalize = s => s && s[0].toUpperCase() + s.slice(1)
 
---
+++ packages/protolib/components/SelectList.tsx
@@ -0,0 +1,122 @@
+import { YStack, SelectProps, Select, Adapt, Sheet, getFontSize } from "tamagui";
+import { ChevronDown, ChevronUp, Check } from '@tamagui/lucide-icons';
+import { useMemo, useState } from "react";
+
+export function SelectList({title, value, elements, setValue, ...props}: SelectProps & {title: any, elements: any[], value: any, setValue: Function}) {
+    const [val, setVal] = useState('apple')
+  
+    return (
+      <Select
+        id="select"
+        value={val}
+        onValueChange={setVal}
+        disablePreventBodyScroll
+        {...props}
+      >
+        <Select.Trigger width={220} iconAfter={ChevronDown}>
+          <Select.Value placeholder="..." />
+        </Select.Trigger>
+  
+        <Adapt when="sm" platform="touch">
+          <Sheet
+            native={!!props.native}
+            modal
+            dismissOnSnapToBottom
+            animationConfig={{
+              type: 'spring',
+              damping: 20,
+              mass: 1.2,
+              stiffness: 250,
+            }}
+          >
+            <Sheet.Frame>
+              <Sheet.ScrollView>
+                <Adapt.Contents />
+              </Sheet.ScrollView>
+            </Sheet.Frame>
+            <Sheet.Overlay
+              animation="lazy"
+              enterStyle={{ opacity: 0 }}
+              exitStyle={{ opacity: 0 }}
+            />
+          </Sheet>
+        </Adapt>
+  
+        <Select.Content zIndex={200000}>
+          <Select.ScrollUpButton
+            alignItems="center"
+            justifyContent="center"
+            position="relative"
+            width="100%"
+            height="$3"
+          >
+            <YStack zIndex={10}>
+              <ChevronUp size={20} />
+            </YStack>
+
+          </Select.ScrollUpButton>
+  
+          <Select.Viewport
+            // to do animations:
+            // animation="quick"
+            // animateOnly={['transform', 'opacity']}
+            // enterStyle={{ o: 0, y: -10 }}
+            // exitStyle={{ o: 0, y: 10 }}
+            minWidth={200}
+          >
+            <Select.Group>
+              <Select.Label>{title}</Select.Label>
+              {/* for longer lists memoizing these is useful */}
+              {useMemo(
+                () =>
+                elements.map((item, i) => {
+                    return (
+                      <Select.Item
+                        debug="verbose"
+                        index={i}
+                        key={item}
+                        value={item}
+                      >
+                        <Select.ItemText>{item}</Select.ItemText>
+                        <Select.ItemIndicator marginLeft="auto">
+                          <Check size={16} />
+                        </Select.ItemIndicator>
+                      </Select.Item>
+                    )
+                  }),
+                [elements]
+              )}
+            </Select.Group>
+            {/* Native gets an extra icon */}
+            {props.native && (
+              <YStack
+                position="absolute"
+                right={0}
+                top={0}
+                bottom={0}
+                alignItems="center"
+                justifyContent="center"
+                width={'$4'}
+                pointerEvents="none"
+              >
+                <ChevronDown size={getFontSize((props.size ?? '$true') as any)} />
+              </YStack>
+            )}
+          </Select.Viewport>
+  
+          <Select.ScrollDownButton
+            alignItems="center"
+            justifyContent="center"
+            position="relative"
+            width="100%"
+            height="$3"
+          >
+            <YStack zIndex={10}>
+              <ChevronDown size={20} />
+            </YStack>
+
+          </Select.ScrollDownButton>
+        </Select.Content>
+      </Select>
+    )
+  }
