9cf5a3ac41bf802f3f4c908d49159c76598a1868
MiquelCos <miquel@protofy.xyz>
2023-10-18 14:03:54 +0200

    Fixes crash when no from property

--- packages/protolib/adminpanel/bundles/users/adminPages.tsx
+++ packages/protolib/adminpanel/bundles/users/adminPages.tsx
@@ -38,7 +38,7 @@ export default {
                     columns={DataTable2.columns(
                         DataTable2.column("email", "username", true),
                         DataTable2.column("type", "type", true, row => <Chip text={row.type.toUpperCase()} color={row.type == 'admin' ? '$color5':'$gray5'} />),
-                        DataTable2.column("from", "from", true, row => <Chip text={row.from.toUpperCase()} color={row.from == 'cmd' ? '$blue5':'$gray5'} />),
+                        DataTable2.column("from", "from", true, row => <Chip text={row.from?.toUpperCase()} color={row.from == 'cmd' ? '$blue5':'$gray5'} />),
                         DataTable2.column("created", "createdAt", true, row => moment(row.createdAt).format(format)),
                         DataTable2.column("last login", "lastLogin",true, row => row.lastLogin ? <Chip text={moment(row.lastLogin).format(format)} color={'$gray5'} /> : <Chip text={'NEVER'} color={'$yellow6'} /> )
                     )}
