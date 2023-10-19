3da1d07c56407af338e413bf6b1d145e9721b353
Jose Carlos Norte <jcarlos.norte@gmail.com>
2023-10-19 05:04:03 +0200

    Update SideMenu.tsx

--- packages/app/layout/SideMenu.tsx
+++ packages/app/layout/SideMenu.tsx
@@ -1,11 +1,14 @@
 import { NextLink, Logo, ContainerLarge, ExternalIcon, ParagraphLink } from 'protolib'
 import React from 'react'
 import { H4, Paragraph, Text, XStack, YStack } from 'tamagui'
+import { Scrollbars } from 'react-custom-scrollbars-2';
 
 export const SideMenu = ({sideBarColor='$background', children, ...props}:any) => {
     return <YStack bc={sideBarColor} {...props}>
-        <YStack $sm={{display:'none'}} width={300} height="100%" overflow='scroll'>
-            {children}
-        </YStack>
+            <YStack $sm={{display:'none'}} width={300} height="100%">
+                <Scrollbars universal height="100%">
+                    {children}
+                </Scrollbars>
+            </YStack>
     </YStack>
 }
