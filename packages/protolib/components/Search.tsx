65929f04b253f2f944c489d9e3b22b8aac812a55
Marccc <marc@protofy.xyz>
2023-10-20 12:00:03 +0200

    Adds default open prop to Search

--- packages/protolib/components/Search.tsx
+++ packages/protolib/components/Search.tsx
@@ -1,15 +1,15 @@
 import { Input, XStack } from "tamagui"
-import {Search as IconSearch} from '@tamagui/lucide-icons'
+import { Search as IconSearch } from '@tamagui/lucide-icons'
 import { useState, forwardRef, useRef, useEffect } from "react"
-import { useUpdateEffect} from 'usehooks-ts'
+import { useUpdateEffect } from 'usehooks-ts'
 
-export const Search = forwardRef(({initialState, onCancel=() => {},onSearch=() => {}, placeholder='Search...', width=400, widthmd=300, closedWidth=50, ...props}:any, ref:any) => {
-    const [opened, setOpened] = useState(initialState?true:false)
+export const Search = forwardRef(({ defaultOpen , initialState, onCancel = () => { }, onSearch = () => { }, placeholder = 'Search...', width = 400, widthmd = 300, closedWidth = 50, ...props }: any, ref: any) => {
+    const [opened, setOpened] = useState(defaultOpen || initialState ? true : false)
     const input = useRef()
     const [content, setContent] = useState(initialState)
 
     useUpdateEffect(() => {
-        if(opened) {
+        if (opened) {
             //@ts-ignore
             input.current?.focus()
         } else {
@@ -17,31 +17,31 @@ export const Search = forwardRef(({initialState, onCancel=() => {},onSearch=() =
         }
     }, [opened])
 
-    
+
     //@ts-ignore
-    return <XStack 
-        width={opened?width:closedWidth}
-        $md={{width: opened?widthmd: closedWidth}}
-        $sm={{ width: opened?'100%':closedWidth }} 
-        onPress={() => setOpened(true)} 
+    return <XStack
+        width={opened ? width : closedWidth}
+        $md={{ width: opened ? widthmd : closedWidth }}
+        $sm={{ width: opened ? '100%' : closedWidth }}
+        onPress={() => setOpened(true)}
         ref={ref}
         {...props}
     >
-        {opened?<Input
+        {opened ? <Input
             o={1}
             backgroundColor={'$color1'}
             value={content}
             disabled={!opened}
-            focusStyle={{outlineWidth:0}}
+            focusStyle={{ outlineWidth: 0 }}
             height={'$3'}
             //@ts-ignore
             ref={input}
-            width={opened?"100%":closedWidth} 
-            placeholder={opened?placeholder:''} 
-            onBlur={() => !content?setOpened(false):false}
-            onChangeText={(text) => {setContent(text);if(!text) onSearch('')}}
-            onSubmitEditing={(e)=> {onSearch(content)}}
-        />:null}
+            width={opened ? "100%" : closedWidth}
+            placeholder={opened ? placeholder : ''}
+            onBlur={() => !content ? setOpened(false) : false}
+            onChangeText={(text) => { setContent(text); if (!text) onSearch('') }}
+            onSubmitEditing={(e) => { onSearch(content) }}
+        /> : null}
         <XStack position={"absolute"} right={15} top={6} cursor="pointer" opacity={0.7} hoverStyle={{ opacity: 1 }}>
             <IconSearch color="var(--color)" />
         </XStack>
