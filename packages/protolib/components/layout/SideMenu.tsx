f558926e9a671a7a2a62a51a95e2a82b3505f0a6
jcarlosn <jcarlos.norte@gmail.com>
2023-10-23 23:51:27 +0200

    Extracted sidemenu

---
+++ packages/protolib/components/layout/SideMenu.tsx
@@ -0,0 +1,12 @@
+import { YStack } from 'tamagui'
+import { Scrollbars } from 'react-custom-scrollbars-2';
+
+export const SideMenu = ({sideBarColor='$background', children, ...props}:any) => {
+    return <YStack bw={0} bc={sideBarColor} {...props}>
+            <YStack $sm={{display:'none'}} width={300} height="100%">
+                <Scrollbars universal height="100%">
+                    {children}
+                </Scrollbars>
+            </YStack>
+    </YStack>
+}
