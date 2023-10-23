81598e070aa3669e589716ab7cffb7a8a098e3c8
Noemí Blázquez <ap0calipse@gmail.com>
2023-10-23 03:40:16 +0200

    Connect events

--- packages/protolib/adminpanel/bundles/events/adminPages.tsx
+++ packages/protolib/adminpanel/bundles/events/adminPages.tsx
@@ -20,14 +20,6 @@ export default {
                     numColumnsForm={2}
                     name="event"
                     // hideAdd={true}
-                    columns={DataTable2.columns(
-                        DataTable2.column("path", "path", true),
-                        DataTable2.column("from", "from", true, row => <Chip text={row.type.toUpperCase()} color={'$blue5'} />),
-                        DataTable2.column("user", "user", true, row => <Chip text={row.user.toUpperCase()} color={'$color5'} />),
-                        DataTable2.column("created", "created", true, row => moment(row.created).format(format)),
-                        DataTable2.column("status", "status",true, row => <Chip text={row.status.toUpperCase()} color={'$yellow6'} /> ),
-                        DataTable2.column("lastUpdated", "lastUpdated", true, row => moment(row.lastUpdated).format(format))
-                    )}
                     model={EventModel} 
                     pageState={pageState}
                     icons={EventIcons}
--- packages/protolib/adminpanel/bundles/events/eventsAPI.ts
+++ packages/protolib/adminpanel/bundles/events/eventsAPI.ts
@@ -1,4 +1,4 @@
 import { EventModel } from ".";
-import {CreateApi} from 'protolib/api'
+import {CreateApi} from '../../../api'
 
 export const EventsAPI = CreateApi('events', EventModel, __dirname, '/adminapi/v1/')
