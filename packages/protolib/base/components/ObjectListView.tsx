aed0ebfb944bd81dd4ad030d8420abf29c47282e
MiquelCos <miquel@protofy.xyz>
2023-10-25 13:23:34 +0200

    Adds key to object list views to fix front bug when update list

--- packages/protolib/base/components/ObjectListView.tsx
+++ packages/protolib/base/components/ObjectListView.tsx
@@ -1,16 +1,13 @@
 
-import { XStack, YStack, Stack } from 'tamagui'
-import { ItemCard, useAtom, useTouchSensors, SortableItem, Page, getPendingResult, EditableText, AsyncView, usePendingEffect, createApiAtom, API, Grid, SpotLight, Section, TamaCard, BigTitle } from 'protolib'
-import { arrayMove, SortableContext, rectSortingStrategy } from '@dnd-kit/sortable';
-import { DndContext, closestCenter } from '@dnd-kit/core';
-import { useState } from 'react';
+import { YStack, Stack } from 'tamagui'
+import { ItemCard, AsyncView, Grid } from 'protolib'
 import React from 'react';
 
 export function ObjectListView({ elements, listItem }) {
   return (
     <AsyncView atom={elements}>
           <Grid itemMinWidth={300}>
-            {elements?.data?.items?.map((data, index) => <Stack style={{ touchAction: 'none' }} $sm={{mx:"$0"}} mx={"$5"} p={"$1"} py={"$5"}>
+            {elements?.data?.items?.map((data, index) => <Stack key={index+Math.random()} style={{ touchAction: 'none' }} $sm={{mx:"$0"}} mx={"$5"} p={"$1"} py={"$5"}>
                 <ItemCard
                     maxWidth={700}
                     $md={{ maxWidth: 450 }}
