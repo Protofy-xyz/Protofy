8ec605f8348f3bdaf42e8c2aac8d38e3d3b4539c
MiquelCos <miquel@protofy.xyz>
2023-10-25 18:50:40 +0200

    Enables uploader define file types

--- packages/protolib/adminpanel/next/components/Uploader.tsx
+++ packages/protolib/adminpanel/next/components/Uploader.tsx
@@ -1,32 +1,33 @@
 import Dropzone from 'react-dropzone-uploader'
 
-export const Uploader = ({path, onUpload, setShowUploadDialog}) => {
-    // specify upload params and url for your files
-    const getUploadParams = ({ meta }) => { return { url: '/adminapi/v1/files/'+path } }
-    
-    // called every time a file's `status` changes
-    const handleChangeStatus = ({ meta, file }, status) => { 
-      if (status=="done") {
-        console.log("CHANGE STATUS", status, meta, file) 
-        onUpload({ meta, file });
-      }
-      
+export const Uploader = ({ path, onUpload, setShowUploadDialog, accept = "image/*,audio/*,video/*" }) => {
+  // specify upload params and url for your files
+  const getUploadParams = ({ meta }) => { return { url: '/adminapi/v1/files/' + path } }
+
+  // called every time a file's `status` changes
+  const handleChangeStatus = ({ meta, file }, status) => {
+    if (status == "done") {
+      console.log("CHANGE STATUS", status, meta, file)
+      onUpload({ meta, file });
     }
-    
-    // receives array of files that are done uploading when submit button is clicked
-    const handleSubmit = (files, allFiles) => {
-      console.log(files.map(f => f.meta))
-      allFiles.forEach(f => f.remove())
-      setShowUploadDialog(false)
-    }
-  
-    return (
-      <Dropzone
-        styles={{ dropzone: { border: 0, overflow: 'hidden',height: '100%' } }}
-        getUploadParams={getUploadParams}
-        onChangeStatus={handleChangeStatus}
-        onSubmit={handleSubmit}
-        submitButtonContent="Close"
-      />
-    )
+
   }
+
+  // receives array of files that are done uploading when submit button is clicked
+  const handleSubmit = (files, allFiles) => {
+    console.log(files.map(f => f.meta))
+    allFiles.forEach(f => f.remove())
+    setShowUploadDialog(false)
+  }
+
+  return (
+    <Dropzone
+      styles={{ dropzone: { border: 0, overflow: 'hidden', height: '100%' } }}
+      getUploadParams={getUploadParams}
+      onChangeStatus={handleChangeStatus}
+      onSubmit={handleSubmit}
+      submitButtonContent="Close"
+      accept={accept}
+    />
+  )
+}
