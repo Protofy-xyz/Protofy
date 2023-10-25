354466313b784d7d22e8546e48c792b457aa86ee
MiquelCos <miquel@protofy.xyz>
2023-10-25 12:47:59 +0200

    Updates search button

--- packages/protolib/components/Search.tsx
+++ packages/protolib/components/Search.tsx
@@ -2,6 +2,7 @@ import { Button, Input, XStack } from "tamagui"
 import { Search as IconSearch } from '@tamagui/lucide-icons'
 import { useState, forwardRef, useRef, useEffect } from "react"
 import { useUpdateEffect } from 'usehooks-ts'
+import { Tinted } from "./Tinted"
 
 export const Search = forwardRef(({ initialState, onCancel = () => { }, onSearch = () => { }, placeholder = 'Search...', width = 400, widthmd = 300, closedWidth = 50, ...props }: any, ref: any) => {
     const [opened, setOpened] = useState(initialState ? true : false)
@@ -43,9 +44,11 @@ export const Search = forwardRef(({ initialState, onCancel = () => { }, onSearch
             onSubmitEditing={(e) => { onSearch(content) }}
         /> : null}
         <XStack position={"absolute"} right={0} top={-4} cursor="pointer" opacity={0.7} hoverStyle={{ opacity: 1 }}>
-            <Button disabled={opened} bc={"transparent"}>
-                <IconSearch fillOpacity={0} color="var(--color)" />
-            </Button>
+            <Tinted>
+                <Button disabled={opened} hoverStyle={{ o: 1 }} o={0.7} circular chromeless={true}>
+                    <IconSearch fillOpacity={0} color="var(--color)" />
+                </Button>
+            </Tinted>
         </XStack>
     </XStack>
 })
