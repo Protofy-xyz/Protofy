594b62a13868e9ad8c530a612eec816205b93e16
Noemí Blázquez <ap0calipse@gmail.com>
2023-10-27 02:51:18 +0200

    Extract item menu into a component

--- packages/protolib/components/DataTableList.tsx
+++ packages/protolib/components/DataTableList.tsx
@@ -7,58 +7,8 @@ import { Tinted } from "./Tinted";
 import { CheckCheck, Check, MoreVertical, Trash2 } from '@tamagui/lucide-icons'
 import React from "react";
 import Scrollbars from "react-custom-scrollbars-2";
+import { ItemMenu } from "./ItemMenu";
 
-const MenuPopover = ({id, sourceUrl, onDelete}) => {
-    const [menuOpened, setMenuOpened] = useState(false)
-    const [open, setOpen] = useState(false)
-
-    return <>
-        <AlertDialog
-            acceptCaption="Delete"
-            setOpen={setOpen}
-            open={open}
-            onAccept={async (setOpen) => {
-                await API.get(sourceUrl +'/'+ id + '/delete')
-                await onDelete(id)
-                setOpen(false)
-            }}
-            title={'Delete '}
-            description={"Are you sure want to delete this item?"}
-            w={280}
-        >
-            <YStack f={1} jc="center" ai="center">
-                
-            </YStack>
-        </AlertDialog>
-        <Popover onOpenChange={setMenuOpened} open={menuOpened} placement="bottom">
-            <Popover.Trigger>
-                <XStack cursor="pointer" onPress={() => setMenuOpened(true)}>
-                    <Stack ml={"$3"}
-                        o={0.5}
-                        br={"$5"} p={"$2"}
-                        als="flex-start" cursor='pointer'
-                        pressStyle={{ o: 0.7 }}
-                        hoverStyle={{ o: 1, bc: "$color5" }}>
-                        <MoreVertical size={18} color={'var(--color9)'} strokeWidth={2}></MoreVertical>
-                    </Stack>
-                </XStack>
-            </Popover.Trigger>
-            <Popover.Content padding={0} space={0} left={"$7"} top={"$2"} bw={1} boc="$borderColor" bc={"$color1"} >
-                <Tinted>
-                    <YStack alignItems="center" justifyContent="center" padding={"$3"} paddingVertical={"$3"}>
-                        <XStack>
-                            <XStack ml={"$1"} o={1} br={"$5"} p={"$3"} als="flex-start" cursor='pointer' pressStyle={{ o: 0.7 }} hoverStyle={{ bc: "$color5" }}
-                                onPress={() => { setOpen(true); setMenuOpened(false) }}>
-                                <Text mr={"$3"} >Delete</Text>
-                                <Trash2 size={"$1"} color="var(--color9)" strokeWidth={2} />
-                            </XStack>
-                        </XStack>
-                    </YStack>
-                </Tinted>
-            </Popover.Content>
-        </Popover>
-    </>
-}
 
 export const DataTableList = ({sourceUrl, onDelete=()=>{}}) => {
     const { items, model, selected, setSelected, state, push, replace, mergePush, tableColumns, rowIcon, onSelectItem } = useContext(DataViewContext);
@@ -119,7 +69,7 @@ export const DataTableList = ({sourceUrl, onDelete=()=>{}}) => {
                                     </Checkbox.Indicator>
                                 </Checkbox>
                             </Stack>
-                            <MenuPopover sourceUrl={sourceUrl} id={model.load(row).getId()} onDelete={onDelete}/>
+                            <ItemMenu sourceUrl={sourceUrl} id={model.load(row).getId()} onDelete={onDelete}/>
                         </XStack></Theme>, true, '85px'), ...finalColumns]}
                     rows={items?.data?.items}
                     onRowPress={(rowData) => onSelectItem ? onSelectItem(model.load(rowData)) : replace('item', model.load(rowData).getId())}
