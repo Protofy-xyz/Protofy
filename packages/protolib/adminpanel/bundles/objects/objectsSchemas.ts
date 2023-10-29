df65236c5c65d80391f1f08f3a811a36ac488052
Noemí Blázquez <ap0calipse@gmail.com>
2023-10-29 04:51:36 +0100

    Fix delete icon position

--- packages/protolib/adminpanel/bundles/objects/objectsSchemas.ts
+++ packages/protolib/adminpanel/bundles/objects/objectsSchemas.ts
@@ -7,7 +7,36 @@ export const BaseObjectSchema = z.object({
     name: z.string().search().display(),
     keys: z.array(z.object({
         name: z.string(),
-        id: z.string()
+        type: z.union([
+            z.literal("string"),
+            z.literal("number"),
+            z.literal("boolean"),
+            z.literal("array"),
+            z.literal("object"),
+            z.literal("record"),
+            z.literal("union")
+        ]),
+        modifiers: z.array(z.object({
+            name: z.union([
+                z.literal("id"),
+                z.literal("search"),
+                z.literal("generate"),
+                z.literal("display"),
+                z.literal("optional"),
+                z.literal("email"),
+                z.literal("label"),
+                z.literal("hint"),
+                z.literal("static"),
+                z.literal("min"),
+                z.literal("max"),
+                z.literal("secret"),
+                z.literal("onCreate"),
+                z.literal("onUpdate"),
+                z.literal("onRead"),
+                z.literal("onDelete"),
+                z.literal("onList"),
+            ])
+        }))
     })).display()
     // data: z.string().search().display(), //event type: / separated event category: files/create/file, files/create/dir, devices/device/online
     //from: z.string().search().display(), // system entity where the event was generated (next, api, cmd...)
