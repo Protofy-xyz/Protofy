dc9ffa4cbc3aba828c088f7c48390afed8e6ba8c
lluis@protofy.xyz <lluis@protofy.xyz>
2023-10-31 08:50:52 +0100

    Add mqtt connector on device

--- packages/protolib/adminpanel/features/components/devices.tsx
+++ packages/protolib/adminpanel/features/components/devices.tsx
@@ -1,6 +1,7 @@
 import { YStack } from 'tamagui'
 import {Center, BigTitle, RainbowText} from 'protolib'
 import DeviceScreen from 'protodevice';
+import {Connector} from 'mqtt-react-hooks'
 
 type DevicesAdminProps = {
 
@@ -8,12 +9,13 @@ type DevicesAdminProps = {
 
 export default function DevicesAdmin({}:DevicesAdminProps) {
     return (
-
+        <Connector brokerUrl="ws://bo-firmware.protofy.xyz/ws">
         <Center>
             {/* <BigTitle>
                 Hello <RainbowText>devices</RainbowText>
             </BigTitle> */}
             <DeviceScreen></DeviceScreen>
         </Center>
+        </Connector>
     )
 }
