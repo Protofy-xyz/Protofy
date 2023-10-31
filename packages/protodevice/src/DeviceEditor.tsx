84a285ce6c7a1418b9bbc6989e28c8203bc1e54c
lluis@protofy.xyz <lluis@protofy.xyz>
2023-10-31 14:24:58 +0100

    Adjust initial code

--- packages/protodevice/src/DeviceEditor.tsx
+++ packages/protodevice/src/DeviceEditor.tsx
@@ -114,7 +114,7 @@ const saveYaml = async (yaml) => {
 
 const DeviceScreen = ({ isActive,topics}) => {
   const topicData = topics;
-  const p = {"config":"[\n  \"mydevice\",\n  \"esp32dev\",\n  \"POLOFAI\",\n  \"PR0T0FY100\",\n  \"none\",\n  \"51.68.45.86\",\n  false,\n  \"10\",\n  \"10\",\n  34,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  relay(\"light\", \"ALWAYS_OFF\"),\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n];\n\n"}
+  const p = {"config":"[\n  \"mydevice\",\n  \"esp32dev\",\n  \"POROTOVICE\",\n  \"********\",\n  \"none\",\n  \"51.68.45.86\",\n  false,\n  \"10\",\n  \"10\",\n  34,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  relay(\"light\", \"ALWAYS_OFF\"),\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n  null,\n];\n\n"}
   const [sourceCode, setSourceCode] = useState(p.config)
   const currentDevice = useDeviceStore(state => state.electronicDevice);
   const setCurrentDevice = useDeviceStore(state => state.setElectronicDevice);
