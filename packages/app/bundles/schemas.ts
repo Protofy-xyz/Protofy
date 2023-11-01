b91ba359a1d69d14e27e61ed9f9adbf83ad3c198
jcarlosn <jcarlos.norte@gmail.com>
2023-11-01 03:25:03 +0100

    Rename def() to Protofy()

--- packages/app/bundles/schemas.ts
+++ packages/app/bundles/schemas.ts
@@ -1,10 +1,10 @@
-import {def} from 'protolib/base'
+import {Protofy} from 'protolib/base'
 import { UserSchema } from 'protolib/adminpanel/bundles/users/usersSchemas'
 import { GroupSchema } from 'protolib/adminpanel/bundles/groups/groupsSchemas'
 import { EventSchema } from 'protolib/adminpanel/bundles/events/eventsSchemas'
 import { ObjectSchema } from 'protolib/adminpanel/bundles/objects/objectsSchemas'
 
-const Schemas = def("schemas", {
+const Schemas = Protofy("schemas", {
     event: EventSchema,
     user: UserSchema,
     group: GroupSchema,
