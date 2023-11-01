2b8d3f050b4008a530d4666f0fff31b72dcf6928
jcarlosn <jcarlos.norte@gmail.com>
2023-11-01 02:08:53 +0100

    Adds bundle custom

---
+++ packages/app/bundles/custom/pages.tsx
@@ -0,0 +1,6 @@
+
+import {AdminPage, PaginatedDataSSR} from 'protolib/adminpanel/features/next'
+import {DataView} from 'protolib'
+import {def} from 'protolib/base'
+
+export default def("pages", {})
