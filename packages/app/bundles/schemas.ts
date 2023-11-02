ab8a528fc94827d236f2c051d5b168c957583a60
jcarlosn <jcarlos.norte@gmail.com>
2023-11-02 01:31:27 +0100

    Adds local schemas

--- packages/app/bundles/schemas.ts
+++ packages/app/bundles/schemas.ts
@@ -1,14 +1,15 @@
-import {Protofy} from 'protolib/base'
 import { UserSchema } from 'protolib/adminpanel/bundles/users/usersSchemas'
 import { GroupSchema } from 'protolib/adminpanel/bundles/groups/groupsSchemas'
 import { EventSchema } from 'protolib/adminpanel/bundles/events/eventsSchemas'
 import { ObjectSchema } from 'protolib/adminpanel/bundles/objects/objectsSchemas'
+import LocalSchemas from './custom/schemas'
 
-const Schemas = Protofy("schemas", {
+const Schemas = {
     event: EventSchema,
     user: UserSchema,
     group: GroupSchema,
     object: ObjectSchema,
-})
+    ...LocalSchemas,
+}
 
 export default Schemas
