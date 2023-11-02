652e2f1db92acf4eeb178eebfc10bf4f3991ab9a
jcarlosn <jcarlos.norte@gmail.com>
2023-11-02 04:25:52 +0100

    Adds library system to bundles

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -7,6 +7,7 @@ import http from 'http';
 import WebSocket, { Server } from 'ws';
 import net from 'net';
 import app from './api'
+import {generateEvent} from 'app/bundles/library'
 
 const aedesInstance = new aedes();
 
@@ -42,6 +43,14 @@ mqttServer.listen(1883, () => {
   console.log('MQTT server listening on port 1883');
 });
 
+
+generateEvent({
+  path: 'services/start/api', //event type: / separated event category: files/create/file, files/create/dir, devices/device/online
+  from: 'api', // system entity where the event was generated (next, api, cmd...)
+  user: 'system', // the original user that generates the action, 'system' if the event originated in the system itself
+  payload: {}, // event payload, event-specific data
+  status: 'pending'
+})
 
 
 
---
+++ packages/app/bundles/library.ts
@@ -0,0 +1,1 @@
+export * from 'protolib/adminpanel/bundles/events/eventsLibrary'
