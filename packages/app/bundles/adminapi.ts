05e272fc3e4da7edf12301588f71917c4b4ddcf7
Gerard <gerard@protofy.xyz>
2023-11-03 10:35:14 +0100

    adds adminapi devicecores api

--- packages/app/bundles/adminapi.ts
+++ packages/app/bundles/adminapi.ts
@@ -3,6 +3,7 @@ import {EventsAPI} from 'protolib/adminpanel/bundles/events/eventsAPI'
 import {GroupsAPI} from 'protolib/adminpanel/bundles/groups/groupsAPI'
 import {ObjectsAPI} from 'protolib/adminpanel/bundles/objects/objectsAPI'
 import {PagesAPI} from 'protolib/adminpanel/bundles/pages/pagesAPI'
+import { DeviceCoresAPI } from 'protolib/adminpanel/bundles/devices/devicecores/devicecoresAPI'
 
 export default (app) => {
     UsersAPI(app)
@@ -10,4 +11,5 @@ export default (app) => {
     EventsAPI(app)
     ObjectsAPI(app)
     PagesAPI(app)
+    DeviceCoresAPI(app)
 }
