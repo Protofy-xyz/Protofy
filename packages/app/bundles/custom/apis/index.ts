800d9e1f14b2da4e2aaddb675cf6c1b9082e3310
jcarlosn <jcarlos.norte@gmail.com>
2023-11-04 02:56:00 +0100

    refactor custom api system to simplify lowcodification

--- packages/app/bundles/custom/apis/index.ts
+++ packages/app/bundles/custom/apis/index.ts
@@ -1,3 +1,9 @@
+import {Protofy} from 'protolib/base'
+
+const autoApis = Protofy("apis", {})
+
 export default (app) => {
-    
+    Object.keys(autoApis).forEach((k) => {
+        autoApis[k](app)
+    })
 }
