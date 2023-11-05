ecdf5b8e96518661e1eedc02b967d03f0cf0f1b3
jcarlosn <jcarlos.norte@gmail.com>
2023-11-05 23:58:07 +0100

    Add children option to objectgrid

--- packages/protolib/components/ObjectGrid.tsx
+++ packages/protolib/components/ObjectGrid.tsx
@@ -1,4 +1,4 @@
-import { Paragraph, Stack, StackProps, YStack } from "tamagui"
+import { Paragraph, Stack, StackProps, XStack, YStack } from "tamagui"
 import { Grid } from './Grid';
 import { EditableObject } from "./EditableObject";
 import { getPendingResult } from '../lib/createApiAtom'
@@ -28,31 +28,31 @@ const GridElementCard = ({ index, data, width }) => {
             hasPicture: true
         } : {})}
     >
-        {data.getBody ? data.getBody(element, width) : 
-        <Stack mb={"$4"} key={element.key} width={width}>
-            <EditableObject
-                title={''}
-                initialData={data.element}
-                name={modelItem.getId()}
-                spinnerSize={75}
-                loadingText={<YStack ai="center" jc="center">Loading data...<Paragraph fontWeight={"bold"}></Paragraph></YStack>}
-                objectId={modelItem.getId()}
-                sourceUrl={data.sourceUrl + '/' + modelItem.getId()}
-                mode={'preview'}
-                model={data.model}
-                extraFields={data.extraFields}
-                icons={data.icons}
-                customFields={data.customFields}
-                columnWidth={width - data.contentMargin}
-                columnMargin={0}
-                onDelete={data.onDelete}
-            />
-        </Stack>
+        {data.getBody ? data.getBody(element, width) :
+            <Stack mb={"$4"} key={element.key} width={width}>
+                <EditableObject
+                    title={''}
+                    initialData={data.element}
+                    name={modelItem.getId()}
+                    spinnerSize={75}
+                    loadingText={<YStack ai="center" jc="center">Loading data...<Paragraph fontWeight={"bold"}></Paragraph></YStack>}
+                    objectId={modelItem.getId()}
+                    sourceUrl={data.sourceUrl + '/' + modelItem.getId()}
+                    mode={'preview'}
+                    model={data.model}
+                    extraFields={data.extraFields}
+                    icons={data.icons}
+                    customFields={data.customFields}
+                    columnWidth={width - data.contentMargin}
+                    columnMargin={0}
+                    onDelete={data.onDelete}
+                />
+            </Stack>
         }
     </ItemCard>
 }
 
-export const ObjectGrid = ({ gridElementCard=undefined, itemMinWidth = 400, itemHeight, rightGap = 30, contentMargin = 40, onSelectItem = (id) => { }, spacing = 20, getPicture, getBody, model, items, sourceUrl, customFields, onDelete, extraFields, icons, ...props }: any & StackProps) => {
+export const ObjectGrid = ({ gridElementCard = undefined, itemMinWidth = 400, itemHeight, rightGap = 30, contentMargin = 40, onSelectItem = (id) => { }, spacing = 20, getPicture, getBody, model, items, sourceUrl, customFields, onDelete, extraFields, icons, children, ...props }: any & StackProps) => {
     const containerRef = useRef(null)
     const data = items.map((element, i) => {
         return {
@@ -74,9 +74,12 @@ export const ObjectGrid = ({ gridElementCard=undefined, itemMinWidth = 400, item
         }
     })
 
-    return <Stack height={1} f={1} {...props}>
-        <Scrollbars universal={true} ref={containerRef}>
-            <Grid key={data.length} rightGap={rightGap} containerRef={containerRef} spacing={spacing} data={data} card={gridElementCard??GridElementCard} itemMinWidth={itemMinWidth} />
-        </Scrollbars>
-    </Stack>
+    return <XStack f={1}>
+        <XStack {...props}>
+            <Scrollbars universal={true} ref={containerRef}>
+                <Grid key={data.length} rightGap={rightGap} containerRef={containerRef} spacing={spacing} data={data} card={gridElementCard ?? GridElementCard} itemMinWidth={itemMinWidth} />
+            </Scrollbars>
+        </XStack>
+        {children}
+    </XStack>
 }
