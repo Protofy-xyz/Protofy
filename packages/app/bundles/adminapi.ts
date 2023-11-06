20cc867569c9eb3e733457ba9c538aed5920fdd7
cristian-sanchez-dev <cristian.sanchez@protofy.xyz>
2023-11-06 11:24:26 +0000

    Added deviceDefinitionsAPI

--- packages/app/bundles/adminapi.ts
+++ packages/app/bundles/adminapi.ts
@@ -1,19 +1,21 @@
-import {UsersAPI} from 'protolib/adminpanel/bundles/users/usersAPI'
-import {EventsAPI} from 'protolib/adminpanel/bundles/events/eventsAPI'
-import {GroupsAPI} from 'protolib/adminpanel/bundles/groups/groupsAPI'
-import {ObjectsAPI} from 'protolib/adminpanel/bundles/objects/objectsAPI'
-import {PagesAPI} from 'protolib/adminpanel/bundles/pages/pagesAPI'
-import {APIsAPI} from 'protolib/adminpanel/bundles/apis/api'
+import { UsersAPI } from 'protolib/adminpanel/bundles/users/usersAPI'
+import { EventsAPI } from 'protolib/adminpanel/bundles/events/eventsAPI'
+import { GroupsAPI } from 'protolib/adminpanel/bundles/groups/groupsAPI'
+import { ObjectsAPI } from 'protolib/adminpanel/bundles/objects/objectsAPI'
+import { PagesAPI } from 'protolib/adminpanel/bundles/pages/pagesAPI'
+import { APIsAPI } from 'protolib/adminpanel/bundles/apis/api'
 import { DeviceCoresAPI } from 'protolib/adminpanel/bundles/devices/devicecores/devicecoresAPI'
 import { DeviceBoardsAPI } from 'protolib/adminpanel/bundles/devices/deviceBoards/deviceBoardsAPI'
+import { DeviceDefinitionsAPI } from 'protolib/adminpanel/bundles/devices/deviceDefinitions/deviceDefinitionsAPI'
 
 export default (app) => {
-    UsersAPI(app)
-    GroupsAPI(app)
-    EventsAPI(app)
-    ObjectsAPI(app)
-    PagesAPI(app)
-    APIsAPI(app)
-    DeviceCoresAPI(app), 
-    DeviceBoardsAPI(app)
+  UsersAPI(app)
+  GroupsAPI(app)
+  EventsAPI(app)
+  ObjectsAPI(app)
+  PagesAPI(app)
+  APIsAPI(app)
+  DeviceCoresAPI(app),
+  DeviceBoardsAPI(app),
+  DeviceDefinitionsAPI(app)
 }
