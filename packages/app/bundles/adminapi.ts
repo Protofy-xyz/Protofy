c70bc86e71e40f183d4acd99d16a76cbf5d93cb2
cristian-sanchez-dev <cristian.sanchez@protofy.xyz>
2023-11-07 15:23:42 +0000

    Added DevicesAPI to adminapi

--- packages/app/bundles/adminapi.ts
+++ packages/app/bundles/adminapi.ts
@@ -4,10 +4,11 @@ import { GroupsAPI } from 'protolib/adminpanel/bundles/groups/groupsAPI'
 import { ObjectsAPI } from 'protolib/adminpanel/bundles/objects/objectsAPI'
 import { PagesAPI } from 'protolib/adminpanel/bundles/pages/pagesAPI'
 import { APIsAPI } from 'protolib/adminpanel/bundles/apis/api'
+import { DevicesAPI } from 'protolib/adminpanel/bundles/devices/devices/devicesAPI'
 import { DeviceCoresAPI } from 'protolib/adminpanel/bundles/devices/devicecores/devicecoresAPI'
 import { DeviceBoardsAPI } from 'protolib/adminpanel/bundles/devices/deviceBoards/deviceBoardsAPI'
 import { DeviceDefinitionsAPI } from 'protolib/adminpanel/bundles/devices/deviceDefinitions/deviceDefinitionsAPI'
-import {TaskApi } from 'protolib/adminpanel/bundles/tasks/api/taskApi'
+import { TaskApi } from 'protolib/adminpanel/bundles/tasks/api/taskApi'
 
 export default (app) => {
   UsersAPI(app)
@@ -16,8 +17,9 @@ export default (app) => {
   ObjectsAPI(app)
   PagesAPI(app)
   APIsAPI(app)
-  DeviceCoresAPI(app),
-  DeviceBoardsAPI(app),
+  DevicesAPI(app)
+  DeviceCoresAPI(app)
+  DeviceBoardsAPI(app)
   DeviceDefinitionsAPI(app)
   TaskApi(app)
 }
