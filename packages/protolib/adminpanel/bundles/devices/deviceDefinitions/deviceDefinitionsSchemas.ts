9662087744fa469f95452d3f868b2f55672914dd
cristian-sanchez-dev <cristian.sanchez@protofy.xyz>
2023-11-07 10:45:57 +0000

    Now onDeviceDefinition sdks are rendered and dependsOn board property

--- packages/protolib/adminpanel/bundles/devices/deviceDefinitions/deviceDefinitionsSchemas.ts
+++ packages/protolib/adminpanel/bundles/devices/deviceDefinitions/deviceDefinitionsSchemas.ts
@@ -5,7 +5,7 @@ import { AutoModel } from 'protolib/base'
 export const DeviceDefinitionSchema = Schema.object({
   name: z.string().hint("Protofy screen controller...").display().static().id(),
   board: z.string(),
-  sdk: z.string().optional(),
+  sdk: z.string(),
   subsystems: z.record(z.string(), z.any()).optional(),
   config: z.record(z.string(), z.any()).display(),
 })
