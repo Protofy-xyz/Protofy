178fc772199642e5da683248865b80b4cec695d9
cristian-sanchez-dev <cristian.sanchez@protofy.xyz>
2023-11-07 09:44:41 +0000

    Added dependsOn method to the zod type

--- packages/app/zod-extensions.d.ts
+++ packages/app/zod-extensions.d.ts
@@ -7,6 +7,7 @@ interface ZodExtensions {
     generate(val: any): this;
     before(field: string): this;
     after(field: string): this;
+    dependsOn(field: string): this;
     secret(): this;
     static(): this;
     id(): this;
