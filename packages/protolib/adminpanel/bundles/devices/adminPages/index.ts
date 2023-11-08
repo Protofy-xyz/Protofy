8eea494aa9faa1cf1ddc039d5bdf59554951ded6
cristian-sanchez-dev <cristian.sanchez@protofy.xyz>
2023-11-08 10:32:37 +0000

    Added Sdk Model

--- packages/protolib/adminpanel/bundles/devices/adminPages/index.ts
+++ packages/protolib/adminpanel/bundles/devices/adminPages/index.ts
@@ -3,10 +3,12 @@ import deviceCorePage from '../devicecores/devicecoresPage';
 import deviceBoardPage from '../deviceBoards/deviceBoardsPage';
 import deviceDefinitionsPage from '../deviceDefinitions/deviceDefinitionsPage';
 import devicesPage from '../devices/devicesPage';
+import deviceSdksPage from '../deviceSdks/deviceSdksPage';
 
 export default {
-  'admin/device-test/**': { component: DevicePage, getServerSideProps: getServerSideProps }, 
+  'admin/device-test/**': { component: DevicePage, getServerSideProps: getServerSideProps },
   'admin/devices/**': devicesPage,
+  'admin/deviceSdks/**': deviceSdksPage,
   'admin/deviceCores/**': deviceCorePage,
   'admin/deviceBoards/**': deviceBoardPage,
   'admin/deviceDefinitions/**': deviceDefinitionsPage
---
+++ packages/protolib/adminpanel/bundles/devices/deviceSdks/deviceSdksPage.tsx
@@ -0,0 +1,31 @@
+import { AdminPage, PaginatedDataSSR } from 'protolib/adminpanel/features/next'
+import { Cpu, Layers, Tag, Code } from '@tamagui/lucide-icons';
+import { DeviceSdkModel } from './deviceSdksSchema';
+import { Chip, DataTable2, DataView } from 'protolib'
+
+const DeviceSdkIcons = { name: Tag, sdk: Layers }
+export default {
+  component: ({ workspace, pageState, sourceUrl, initialItems, itemData, pageSession }: any) => {
+    return (<AdminPage title="Device Sdks" workspace={workspace} pageSession={pageSession}>
+      <DataView
+        itemData={itemData}
+        rowIcon={Code}
+        sourceUrl={sourceUrl}
+        initialItems={initialItems}
+        numColumnsForm={1}
+        name="Sdk"
+        onAdd={data => { return data }}
+        onEdit={data => { return data }}
+        columns={DataTable2.columns(
+          DataTable2.column("id", "id", true),
+          DataTable2.column("name", "name", true),
+        )}
+        model={DeviceSdkModel}
+        pageState={pageState}
+        icons={DeviceSdkIcons}
+        dataTableGridProps={{ itemMinWidth: 300, spacing: 20 }}
+      />
+    </AdminPage >)
+  },
+  getServerSideProps: PaginatedDataSSR('/adminapi/v1/devicesdks')
+}
