f16c66b40148af9533f814210f9b09c420ea2cb6
jcarlosn <jcarlos.norte@gmail.com>
2023-11-08 03:36:02 +0100

    Link tasks page

--- packages/app/bundles/nextPages.ts
+++ packages/app/bundles/nextPages.ts
@@ -6,10 +6,10 @@ import usersPages from 'protolib/adminpanel/bundles/users/adminPages'
 import groupsPages from 'protolib/adminpanel/bundles/groups/adminPages'
 import eventsPages from 'protolib/adminpanel/bundles/events/adminPages'
 import objectsPages from 'protolib/adminpanel/bundles/objects/adminPages'
+import tasksPages from 'protolib/adminpanel/bundles/tasks/adminPages'
 import pagesPages from 'protolib/adminpanel/bundles/pages/adminPages'
 import apisPages from 'protolib/adminpanel/bundles/apis/adminPages'
 import customPages from './custom/pages'
-
 
 export default {
     ...notesPages,
@@ -22,5 +22,6 @@ export default {
     ...objectsPages,
     ...pagesPages,
     ...apisPages,
+    ...tasksPages,
     ...customPages
 }
