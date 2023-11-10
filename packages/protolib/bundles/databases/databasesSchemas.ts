e3cb857a4b517c21d3809cafbff00f8bb5de360a
jcarlosn <jcarlos.norte@gmail.com>
2023-11-10 01:01:42 +0100

    Adds search to database

--- packages/protolib/bundles/databases/databasesSchemas.ts
+++ packages/protolib/bundles/databases/databasesSchemas.ts
@@ -1,21 +1,40 @@
 import { z } from "zod";
 import { Protofy, AutoModel, Schema } from 'protolib/base'
 import moment from "moment";
+import { ProtoModel } from "../../base";
+import { SessionDataType } from "../../api";
 
 export const DatabaseEntrySchema = Schema.object({
 	key: z.string(),
 	value: z.any()
 })
 
-
 export const DatabaseSchema = Schema.object(Protofy("schema", {
 	name: z.string().id().search().display().static(),
 	entries: z.array(DatabaseEntrySchema).generate(()=>[])
 }))
 
-
 export type DatabaseEntryType = z.infer<typeof DatabaseEntrySchema>;
-export const DatabaseEntryModel = AutoModel.createDerived<DatabaseEntryType>("DatabaseEntryModel", DatabaseEntrySchema);
+
+export class DatabaseEntryModel extends ProtoModel<DatabaseEntryModel> {
+    constructor(data: DatabaseEntryType, session?: SessionDataType) {
+        super(data, DatabaseEntrySchema, session);
+    }
+
+	list(search?): any {
+        if(search) {
+			if(JSON.stringify(this.data).toLowerCase().includes(search.toLowerCase())) {
+				return this.read()
+			}
+        } else {
+            return this.read();
+        }
+    }
+
+    protected static _newInstance(data: any, session?: SessionDataType): DatabaseEntryModel {
+        return new DatabaseEntryModel(data, session);
+    }
+}
 
 export type DatabaseType = z.infer<typeof DatabaseSchema>;
 export const DatabaseModel = AutoModel.createDerived<DatabaseType>("DatabaseModel", DatabaseSchema, 'databases', '/api/v1/');
