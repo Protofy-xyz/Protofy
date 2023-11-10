0f5e8760f06f513286080771198a8c5644a63715
jcarlosn <jcarlos.norte@gmail.com>
2023-11-10 14:21:36 +0100

    Adds X-Client-IP

--- apps/proxy/index.js
+++ apps/proxy/index.js
@@ -1,13 +1,21 @@
 console.log('API URL: ', process.env.API_URL ?? 'http://localhost:3001')
 console.log('SERVER URL: ', process.env.SITE_URL ?? 'http://localhost:3000')
 console.log('PORT: ', process.env.PORT ?? 8080)
+
+function addClientIpHeader(req) {
+    var clientIp = req.connection.remoteAddress;
+    req.headers['X-Client-IP'] = clientIp;
+}
+
 var customResolver1 = function (host, url, req) {
+    addClientIpHeader(req);
     if (/^\/api\//.test(url) || url === '/websocket') {
         return process.env.API_URL ?? 'http://localhost:3001';
     }
 };
 
 var customResolver2 = function (host, url, req) {
+    addClientIpHeader(req);
     if (/^\/adminapi\//.test(url)) {
         return process.env.API_URL ?? 'http://localhost:3002';
     }
