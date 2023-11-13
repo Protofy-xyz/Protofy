628274a1fca1fb6e49a9c136183939157ff33525
lluis@protofy.xyz <lluis@protofy.xyz>
2023-11-13 11:15:33 +0100

    remove

--- packages/protodevice/src/nodes/Relay.tsx
+++ packages/protodevice/src/nodes/Relay.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import { Node, Field, HandleOutput, NodeParams } from 'protoflow';
 import { getSubsystem } from "../device/Relay";
-import subsystem from "./utils/subsystem";
+// import subsystem from "./utils/subsystem";
 
 const Relay = (node:any={}, nodeData={}, children) => {
 
@@ -16,14 +16,14 @@ const Relay = (node:any={}, nodeData={}, children) => {
     // useEffect(()=>{
     //     console.log("AAAAAAAAAAAAAAAAAA", message)
     // },[message])
-    const subsystemData = getSubsystem()
-    const type = 'switch';
+    // const subsystemData = getSubsystem()
+    // const type = 'switch';
     return (
         <Node node={node} isPreview={!node.id} title='GPIO Switch' color="#FFDF82" id={node.id} skipCustom={true} disableInput disableOutput>
             <NodeParams id={node.id} params={nodeParams} />
-            <div style={{marginTop: "10px", marginBottom: "10px"}}>
+            {/* <div style={{marginTop: "10px", marginBottom: "10px"}}>
                 {subsystem(subsystemData, nodeData, type)}
-            </div>
+            </div> */}
         </Node>
     )
 }
