0031b6cc7837b6f60db62c8bbf2c4bcf2f8cb282
lluis@protofy.xyz <lluis@protofy.xyz>
2023-11-13 21:39:48 +0100

    Data should be not defined at the first time

--- packages/protolib/bundles/devices/devices/devicesSchemas.ts
+++ packages/protolib/bundles/devices/devices/devicesSchemas.ts
@@ -5,7 +5,7 @@ import { AutoModel } from 'protolib/base'
 export const DevicesSchema = Schema.object({
   name: z.string().hint("Device name").display().static().id(),
   deviceDefinition: z.string(),
-  data: z.array(z.record(z.string(), z.any())).display()
+  data: z.array(z.record(z.string(), z.any())).display().optional()
 })
 export type DevicesType = z.infer<typeof DevicesSchema>;
 export const DevicesModel = AutoModel.createDerived<DevicesType>("DevicesModel", DevicesSchema);
