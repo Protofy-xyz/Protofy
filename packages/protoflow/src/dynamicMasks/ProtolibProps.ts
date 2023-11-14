347b3e7a7b39758dbf820fec67595fb0bd0dbf0d
MiquelCos <miquel@protofy.xyz>
2023-11-14 12:21:15 +0100

    adds more protolib props

--- packages/protoflow/src/dynamicMasks/ProtolibProps.ts
+++ packages/protoflow/src/dynamicMasks/ProtolibProps.ts
@@ -7,10 +7,21 @@ export const getProtolibParams = (enabledProps = ["*"]) => {
     const metadata = useFlowsStore(state => state.metadata)
     const colors = Object.keys(metadata?.tamagui?.color ?? {}).filter(item => item.startsWith("$")).map(k => '"' + metadata?.tamagui?.color[k].key + '"')
     const sizes = Object.keys(metadata?.tamagui?.size ?? {}).filter(item => item.startsWith("$")).map(k => '"' + metadata?.tamagui?.size[k].key + '"')
+    const radiuses = Object.keys(metadata?.tamagui?.radius ?? {}).filter(item => item.startsWith("$")).map(k => '"' + metadata?.tamagui?.radius[k].key + '"')
+    const spaces = Object.keys(metadata?.tamagui?.space ?? {}).filter(item => item.startsWith("$")).map(k => '"' + metadata?.tamagui?.space[k].key + '"')
+    const zIndexes = Object.keys(metadata?.tamagui?.zIndex ?? {}).filter(item => item.startsWith("$")).map(k => '"' + metadata?.tamagui?.zIndex[k].key + '"')
+
+    console.log('DEV: METH', metadata)
     const defaultValue = undefined
     return [
         { field: 'prop-bgColor', label: 'bgColor', type: 'select', fieldType: 'prop', data: colors ? [defaultValue, ...colors] : [], static: true },
         { field: 'prop-fontSize', label: 'fontSize', type: 'select', fieldType: 'prop', data: sizes ? [defaultValue, ...sizes] : [], static: true },
+        { field: 'prop-color', label: 'color', type: 'select', fieldType: 'prop', data: colors ? [defaultValue, ...colors] : [], static: true },
+        { field: 'prop-br', label: 'br', type: 'select', fieldType: 'prop', data: radiuses ? [defaultValue, ...radiuses] : [], static: true },
+        { field: 'prop-bw', label: 'bw', type: 'select', fieldType: 'prop', data: sizes ? [defaultValue, ...sizes] : [], static: true },
+        { field: 'prop-size', label: 'size', type: 'select', fieldType: 'prop', data: sizes ? [defaultValue, ...sizes] : [], static: true },
+        { field: 'prop-space', label: 'space', type: 'select', fieldType: 'prop', data: spaces ? [defaultValue, ...spaces] : [], static: true },
+        { field: 'prop-zi', label: 'zi', type: 'select', fieldType: 'prop', data: zIndexes ? [defaultValue, ...zIndexes] : [], static: true },
     ].filter(item =>  allPropsEnabled || enabledProps.includes(item.label))
 }
 
