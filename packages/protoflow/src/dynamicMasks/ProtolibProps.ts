0705e0e500b8dabc1c31d7ca310fd6a794265967
MiquelCos <miquel@protofy.xyz>
2023-11-14 16:36:34 +0100

    Adds props to protolib props

--- packages/protoflow/src/dynamicMasks/ProtolibProps.ts
+++ packages/protoflow/src/dynamicMasks/ProtolibProps.ts
@@ -2,17 +2,20 @@ import { FlowStoreContext } from "../store/FlowsStore"
 import { useContext } from 'react';
 
 export const getProtolibParams = (enabledProps = ["*"]) => {
-    const allPropsEnabled = enabledProps[0] == "*"
     const useFlowsStore = useContext(FlowStoreContext)
     const metadata = useFlowsStore(state => state.metadata)
+    
+    const allPropsEnabled = enabledProps[0] == "*"
+
     const colors = Object.keys(metadata?.tamagui?.color ?? {}).filter(item => item.startsWith("$")).map(k => '"' + metadata?.tamagui?.color[k].key + '"')
     const sizes = Object.keys(metadata?.tamagui?.size ?? {}).filter(item => item.startsWith("$")).map(k => '"' + metadata?.tamagui?.size[k].key + '"')
     const radiuses = Object.keys(metadata?.tamagui?.radius ?? {}).filter(item => item.startsWith("$")).map(k => '"' + metadata?.tamagui?.radius[k].key + '"')
     const spaces = Object.keys(metadata?.tamagui?.space ?? {}).filter(item => item.startsWith("$")).map(k => '"' + metadata?.tamagui?.space[k].key + '"')
     const zIndexes = Object.keys(metadata?.tamagui?.zIndex ?? {}).filter(item => item.startsWith("$")).map(k => '"' + metadata?.tamagui?.zIndex[k].key + '"')
-
-    console.log('DEV: METH', metadata)
     const defaultValue = undefined
+
+    const textAlingments = ['"auto"' , '"left"' , '"right"' , '"center"' , '"justify"' ]
+
     return [
         { field: 'prop-bgColor', label: 'bgColor', type: 'select', fieldType: 'prop', data: colors ? [defaultValue, ...colors] : [], static: true },
         { field: 'prop-fontSize', label: 'fontSize', type: 'select', fieldType: 'prop', data: sizes ? [defaultValue, ...sizes] : [], static: true },
@@ -20,6 +23,9 @@ export const getProtolibParams = (enabledProps = ["*"]) => {
         { field: 'prop-br', label: 'br', type: 'select', fieldType: 'prop', data: radiuses ? [defaultValue, ...radiuses] : [], static: true },
         { field: 'prop-bw', label: 'bw', type: 'select', fieldType: 'prop', data: sizes ? [defaultValue, ...sizes] : [], static: true },
         { field: 'prop-size', label: 'size', type: 'select', fieldType: 'prop', data: sizes ? [defaultValue, ...sizes] : [], static: true },
+        { field: 'prop-textAlign', label: 'textAlign', type: 'select', fieldType: 'prop', data: [defaultValue, ...textAlingments], static: true },
+        { field: 'prop-margin', label: 'margin', type: 'select', fieldType: 'prop', data: sizes ? [defaultValue, ...sizes] : [], static: true },
+        { field: 'prop-padding', label: 'padding', type: 'select', fieldType: 'prop', data: sizes ? [defaultValue, ...sizes] : [], static: true },
         { field: 'prop-space', label: 'space', type: 'select', fieldType: 'prop', data: spaces ? [defaultValue, ...spaces] : [], static: true },
         { field: 'prop-zi', label: 'zi', type: 'select', fieldType: 'prop', data: zIndexes ? [defaultValue, ...zIndexes] : [], static: true },
     ].filter(item =>  allPropsEnabled || enabledProps.includes(item.label))
