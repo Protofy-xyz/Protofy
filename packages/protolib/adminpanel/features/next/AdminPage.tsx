f8112186b46dd37cb91f9d85367ee45de085712a
jcarlosn <jcarlos.norte@gmail.com>
2023-11-15 00:34:29 +0100

    Adds assitant

--- packages/protolib/adminpanel/features/next/AdminPage.tsx
+++ packages/protolib/adminpanel/features/next/AdminPage.tsx
@@ -1,11 +1,22 @@
 import { AdminPanel } from '../'
+import { useSession, Page, useUserSettings, useWorkspaces, Tinted } from 'protolib'
+import dynamic from 'next/dynamic';
 
-import { useSession, Page, useUserSettings, useWorkspaces } from 'protolib'
+const DynamicWidget = dynamic(() => import('react-chat-widget').then((mod) => mod.Widget), {
+  ssr: false
+});
 
+// console.log('widget: ', Widget)
 export function AdminPage({ pageSession, title, children }: any) {
   useSession(pageSession)
   return (
     <Page title={"Protofy - " + title}>
+      <Tinted>
+        <DynamicWidget
+          handleNewUserMessage={() => { }}
+        />
+      </Tinted>
+
       <AdminPanel>
         {children}
       </AdminPanel>
