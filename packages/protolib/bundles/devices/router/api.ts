64d65dc1f2db10d3869dff82c53297f60f0cb7c3
jcarlosn <jcarlos.norte@gmail.com>
2023-11-14 02:57:26 +0100

    Example of router in devices bundle

---
+++ packages/protolib/bundles/devices/router/api.ts
@@ -0,0 +1,23 @@
+import {generateEvent} from 'app/bundles/library'
+
+export const DeviceMessageRouter = (app, context) => {
+    console.log('context: ', context)
+    context.mqtt.subscribe("test", (err) => {
+        if(err) {
+            console.error("Error subscring to topic", err)
+        }
+        console.log("Subscribed to test")
+    });
+
+
+    context.mqtt.on("message", (topic, message) => {
+        const parsedMessage = message.toString() //or JSON.parse(message.toString())
+        console.log('# Message received in device router:', topic, )
+        generateEvent({
+            path: 'devices/xx/yy', //event type: / separated event category: files/create/file, files/create/dir, devices/device/online
+            from: 'device', // system entity where the event was generated (next, api, cmd...)
+            user: 'system', // the original user that generates the action, 'system' if the event originated in the system itself
+            payload: {test: parsedMessage} // event payload, event-specific data
+        })
+    })
+}
