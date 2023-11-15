dfebf60f8f63edff9a76ded080261d65e5315540
lluis@protofy.xyz <lluis@protofy.xyz>
2023-11-15 10:46:04 +0100

    add upload on devicesPage

--- packages/protolib/bundles/devices/devices/devicesPage.tsx
+++ packages/protolib/bundles/devices/devices/devicesPage.tsx
@@ -1,14 +1,112 @@
+import React, { useState,useEffect } from "react";
 import { AdminPage, PaginatedDataSSR } from 'protolib/adminpanel/features/next'
-import { BookOpen, Tag, Layers, Router } from '@tamagui/lucide-icons';
+import { BookOpen, Tag, Router } from '@tamagui/lucide-icons';
 import { DevicesModel } from './devicesSchemas';
-import { API, Chip, DataTable2, DataView } from 'protolib'
+import { API, DataTable2, DataView, ButtonSimple } from 'protolib'
 import { z } from 'zod';
 import { DeviceDefinitionModel } from '../deviceDefinitions';
+import { connectSerialPort, flash } from "../devicesUtils";
+import { Connector, useMqttState, useSubscription  } from 'mqtt-react-hooks';
 
 const DevicesIcons = { name: Tag, deviceDefinition: BookOpen }
 
 export default {
   component: ({ pageState, sourceUrl, initialItems, itemData, pageSession, extraData }: any) => {
+    const [modalFeedback, setModalFeedback] = useState<any>()
+    const [stage, setStage] = useState('')
+
+    const flashDevice = async (deviceName, deviceDefintionId) => {
+      console.log("Flash device: ", { deviceName, deviceDefintionId });
+      const response = await API.get('/adminapi/v1/deviceDefinitions/' + deviceDefintionId);
+      if (response.isError) {
+        alert(response.error)
+        return;
+      }
+
+      const jsCode = response.data.config;
+      const deviceCode = 'device(' + jsCode + ')';
+
+      //const deviceObj = eval(deviceCode)
+    }
+    const sendMessage = async (notUsed)=>{
+      await fetch('http://bo-firmware.protofy.xyz/api/v1/device/compile')
+    }
+    const { message } = useSubscription(['device/compile']);
+  
+    const compile = async () => {
+      setModalFeedback({ message: `Compiling firmware...`, details: { error: false } })
+      const compileMsg = { type: "spawn", configuration: "test.yaml" };
+      sendMessage(JSON.stringify(compileMsg));
+    }
+
+    const flashCb = (msgObj) => {
+      console.log(msgObj);
+      setModalFeedback(state => state = msgObj)
+    }
+
+    const onSelectPort = async () => {
+      const isError = await connectSerialPort()
+      if (isError) return
+      setStage('write')
+    }
+
+    const saveYaml = async (yaml) => {
+      console.log("Save Yaml")
+      console.log(await callText("http://bo-firmware.protofy.xyz/api/v1/device/edit?configuration=test.yaml", 'POST', yaml));
+    }
+
+    useEffect(() => {
+      const process = async () => {
+        if (stage == 'yaml') {
+          await saveYaml(yamlRef.current)
+          setStage('compile')
+        } else if (stage == 'compile') {
+          console.log("stage - compile")
+          await compile()
+        } else if (stage == 'write') {
+          try {
+            await flash(flashCb)
+            setStage('idle')
+          } catch (e) { flashCb({ message: 'Error writing the device. Check that the USB connection and serial port are correctly configured.', details: { error: true } }) }
+        } else if (stage == 'upload') {
+          // getWebSocket()?.close()
+          const chromiumBasedAgent =
+            (navigator.userAgent.includes('Chrome') ||
+              navigator.userAgent.includes('Edge') ||
+              navigator.userAgent.includes('Opera'))
+
+          if (chromiumBasedAgent) {
+            setModalFeedback({ message: 'Connect your device and click select to chose the port. ', details: { error: false } })
+            console.log('chormium based true')
+          } else {
+            console.log('chormium based very false')
+            setModalFeedback({ message: 'You need Chrome, Opera or Edge to upload the code to the device.', details: { error: true } })
+          }
+        }
+      }
+      process()
+    }, [stage])
+
+
+    useEffect(() => {
+      console.log("Compile Message: ", message);
+      try {
+        if (message?.message) {
+          const data = JSON.parse(message?.message.toString());
+          if (data.event == 'exit' && data.code == 0) {
+            console.log("Succesfully compiled");
+            setStage('upload')
+  
+          } else if (data.event == 'exit' && data.code != 0) {
+            console.error('Error compiling')
+            setModalFeedback({ message: `Error compiling code. Please check your flow configuration.`, details: { error: true } })
+          }
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }, [message])
+
     return (<AdminPage title="Devices" pageSession={pageSession}>
       <DataView
         itemData={itemData}
@@ -22,6 +120,7 @@ export default {
         columns={DataTable2.columns(
           DataTable2.column("name", "name", true),
           DataTable2.column("device definition", "deviceDefinition", true),
+          DataTable2.column("config", "config", false, (row) => <ButtonSimple onPress={(e) => { console.log("row from Edit: ", row); flashDevice(row.name, row.deviceDefinition); onSelectPort() }}>Upload</ButtonSimple>)
         )}
         extraFieldsForms={{
           deviceDefinition: z.union(extraData.deviceDefinitions.map(o => z.literal(o))).after('name').display(),
---
+++ packages/protolib/bundles/devices/devicesUtils.ts
@@ -0,0 +1,177 @@
+import { Transport, ESPLoader } from "protodevice/src/device/esptool-js";
+import { sleep } from "protodevice/src/sleep";
+import { Build, FlashError } from "protodevice/src/const";
+import { manifest } from "protodevice/src/manifest";
+
+const onlineCompilerUrl = "http://bo-firmware.protofy.xyz/api/v1";
+let port;
+
+const resetTransport = async (transport: Transport) => {
+    await transport.device.setSignals({
+        dataTerminalReady: false,
+        requestToSend: true,
+    });
+    await transport.device.setSignals({
+        dataTerminalReady: false,
+        requestToSend: false,
+    });
+};
+
+export const connectSerialPort = async () => {
+    let isError = true
+    try {
+        port = await navigator.serial.requestPort();
+    } catch (err: any) {
+        if ((err as DOMException).name === "NotFoundError") {
+            //setConfigError(state => ('Please select a port.'))
+            return isError;
+        }
+        //setConfigError(state => ('Error configuring port.'))
+        return isError;
+    }
+
+    if (!port) {
+        //setConfigError(state => ('Error trying to request port. Please check your browser drivers.'))
+        return isError;
+    }
+    try {
+        await port.open({ baudRate: 115200 });
+        //setConfigError('') 
+    } catch (err: any) {
+        //setConfigError(state => ('Error can not open serial port.'))
+        return isError;
+    }
+}
+
+export const flash = async (cb) => {
+    cb({ message: 'Please hold "Boot" button of your ESP32 board.' })
+    let build: Build | undefined;
+    let chipFamily: Build["chipFamily"];
+    const transport = new Transport(port);
+    const esploader = new ESPLoader(
+        transport,
+        115200,
+        // Wrong type, fixed in https://github.com/espressif/esptool-js/pull/75/files
+        undefined as any
+    );
+
+    //ESptools like to reopen the port
+    await port.close();
+    // For debugging
+    (window as any).esploader = esploader;
+    try {
+        await esploader.main_fn();
+        //await esploader.flash_id(); //not used -> seems taht not affects to correct function
+    } catch (err: any) {
+        console.error(err);
+        cb({ message: "Failed to initialize. Try resetting your device or holding the BOOT button while clicking INSTALL.", details: { error: FlashError.FAILED_INITIALIZING, details: err } });
+        await resetTransport(transport);
+        await transport.disconnect();
+        throw "Failed to initialize. Try resetting your device or holding the BOOT button while clicking INSTALL."
+    }
+    chipFamily = esploader.chip.CHIP_NAME as any;
+    console.log("chipFamily: ", chipFamily);
+    if (!esploader.chip.ROM_TEXT) {
+        cb({ message: `Chip ${chipFamily} is not supported`, details: { error: FlashError.NOT_SUPPORTED, details: `Chip ${chipFamily} is not supported` } })
+        await resetTransport(transport);
+        await transport.disconnect();
+        throw `Chip ${chipFamily} is not supported`
+    }
+    cb({ message: `Initialized. Found ${chipFamily}`, details: { done: true } })
+
+    build = manifest.builds.find((b) => b.chipFamily === chipFamily);
+
+    if (!build) {
+        cb({ message: `Your ${chipFamily} board is not supported.`, details: { error: FlashError.NOT_SUPPORTED, details: chipFamily } })
+        await resetTransport(transport);
+        await transport.disconnect();
+        throw `Your ${chipFamily} board is not supported.`
+    }
+
+    cb({ message: "Preparing installation...", details: { done: false } })
+
+    const filePromises = build.parts.map(async (part) => {
+
+        // const url = "http://bo-firmware.protofy.xyz/api/v1" + "/electronics/download.bin?configuration=" + "test.yaml" + "&type=firmware-factory.bin"
+        const url = onlineCompilerUrl + "/device/download?configuration=" + "test.yaml" + "&type=firmware-factory.bin"
+
+        const resp = await fetch(url);
+        if (!resp.ok) {
+            throw new Error(
+                `Downlading firmware ${url} failed: ${resp.status}`
+            );
+        }
+
+        const reader = new FileReader();
+        const blob = await resp.blob();
+
+        return new Promise<string>((resolve) => {
+            reader.addEventListener("load", () => resolve(reader.result as string));
+            reader.readAsBinaryString(blob);
+        });
+
+    });
+    const fileArray: Array<{ data: string; address: number }> = [];
+    let totalSize = 0;
+
+    for (let part = 0; part < filePromises.length; part++) {
+        try {
+            const data = await filePromises[part];
+            fileArray.push({ data, address: build.parts[part].offset });
+            totalSize += data.length;
+        } catch (err: any) {
+            cb({ message: err.message, details: { error: FlashError.FAILED_FIRMWARE_DOWNLOAD, details: err.message } })
+            await resetTransport(transport);
+            await transport.disconnect();
+            return;
+        }
+    }
+    cb({ message: "Installation prepared", details: { done: true } })
+
+    if (true) {
+        cb({ message: "Erasing device...", details: { done: false } })
+        await esploader.erase_flash();
+        cb({ message: "Device erased", details: { done: true } })
+    }
+    cb({ message: `Writing progress: 0%`, details: { bytesTotal: totalSize, bytesWritten: 0, percentage: 0 } })
+
+    let totalWritten = 0;
+
+    try {
+        await esploader.write_flash(
+            fileArray,
+            "keep",
+            "keep",
+            "keep",
+            false,
+            true,
+            // report progress
+            (fileIndex: number, written: number, total: number) => {
+                const uncompressedWritten =
+                    (written / total) * fileArray[fileIndex].data.length;
+
+                const newPct = Math.floor(
+                    ((totalWritten + uncompressedWritten) / totalSize) * 100
+                );
+                if (written === total) {
+                    totalWritten += uncompressedWritten;
+                    return;
+                }
+                cb({ message: `Writing progress: ${newPct}%`, details: { bytesTotal: totalSize, bytesWritten: totalWritten + written, percentage: newPct } })
+            }
+        );
+    } catch (err: any) {
+        cb({ message: err.message, details: { error: FlashError.WRITE_FAILED, details: err } })
+        await resetTransport(transport);
+        await transport.disconnect();
+        return;
+    }
+    cb({ message: "Writing complete", details: { bytesTotal: totalSize, bytesWritten: totalWritten, percentage: 100 } })
+
+    await sleep(100);
+    console.log("HARD RESET");
+    await resetTransport(transport);
+    console.log("DISCONNECT");
+    await transport.disconnect();
+    cb({ message: "All done!" })
+}
