42a9716d05e175f6fa9e893dfc55c62bf9173a4b
lluis@protofy.xyz <lluis@protofy.xyz>
2023-11-15 12:54:18 +0100

    Add support for mqtt in deviceEditor

--- packages/protolib/adminpanel/features/components/devices.tsx
+++ packages/protolib/adminpanel/features/components/devices.tsx
@@ -1,5 +1,6 @@
 import {Center} from 'protolib'
 import DeviceScreen from 'protodevice';
+import { Connector } from 'mqtt-react-hooks';
 
 type DevicesAdminProps = {
     deviceDefinition: any
@@ -8,8 +9,10 @@ type DevicesAdminProps = {
 
 export default function DevicesAdmin({deviceDefinition}:DevicesAdminProps) {
     return (
-        <Center>
-            <DeviceScreen deviceDefinition={deviceDefinition}></DeviceScreen>
-        </Center>
+        <Connector brokerUrl="ws://bo-firmware.protofy.xyz/ws">
+            <Center>
+                <DeviceScreen deviceDefinition={deviceDefinition}></DeviceScreen>
+            </Center>
+        </Connector>
     )
 }
