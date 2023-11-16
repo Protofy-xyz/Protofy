975e298f608338fb740d6c82cea89a94fb61e65c
TonProtofy <ton.miserachs@protofy.xyz>
2023-11-16 17:24:54 +0100

    Removed unnecessary stuff

--- packages/protodevice/src/nodes/BinarySensor.tsx
+++ packages/protodevice/src/nodes/BinarySensor.tsx
@@ -1,25 +1,17 @@
 import React from "react";
 import { Field, Node, NodeParams } from 'protoflow';
 import NodeBus, { cleanName, generateTopic } from "../NodeBus";
-import { useDeviceStore } from "../oldThings/DeviceStore";
-import { getSubsystem } from "../device/BinarySensor";
-import subsystem from "./utils/subsystem";
 
 const BinarySensor = (node:any={}, nodeData={}, children) => {
     const [name,setName] = React.useState(cleanName(nodeData['param1']))
     const nodeParams: Field[] = [
         { label: 'Name', static: true, field: 'param1', type: 'input', onBlur:()=>{setName(cleanName(nodeData['param1']))}}
     ] as Field[]
-
-    const currentDevice = useDeviceStore(state => state.electronicDevice);
-
     const type = 'binary_sensor';
 
-    const subsystemData = getSubsystem()
     return (
         <Node node={node} isPreview={!node.id} title='Button' color="#A5D6A7" id={node.id} skipCustom={true}>
             <NodeParams id={node.id} params={nodeParams} />
-            {subsystem(subsystemData, nodeData, type)}
             <NodeBus componentName={name} type={type}/>
         </Node>
     )
