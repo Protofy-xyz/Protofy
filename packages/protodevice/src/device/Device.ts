d085b2d5df0d4840aa1b1b979274c7330e541337
TonProtofy <ton.miserachs@protofy.xyz>
2023-11-16 17:09:38 +0100

    removed unnecessary stuff

--- packages/protodevice/src/device/Device.ts
+++ packages/protodevice/src/device/Device.ts
@@ -1,43 +1,14 @@
 const jsYaml = require("js-yaml");
 class Device {
     components;
-
-    mqttPrefix;
-
-
     pinTable;
 
     constructor(components) {
         this.pinTable = []
-        this.mqttPrefix = ""
-        this.components = components.slice(10)
-
-    }
-
-
-    setMqttPrefix(mqttPrefix) {
-        this.mqttPrefix = mqttPrefix
-        return this
-    }
-
-    setWifiPowerMode(mode) {
-        if (this.wifiPowerMode === 'none') {
-            if (mode === 'light' || mode == 'high') {
-                this.wifiPowerMode = mode
-            }
-
-            return
-        }
-        if (this.wifiPowerMode === 'light' && mode === 'high') {
-            this.wifiPowerMode = mode
-            return
-        }
-
-        this.wifiPowerMode === mode // for "high" mode cases on this.wifiPowerMode
+        this.components = components.slice(2)
     }
     
     getComponents(deviceName, deviceDefinition){
-        console.log("🚀 ~ file: Device.ts:69 ~ Device ~ getComponents ~ deviceDefinition:", deviceDefinition)
         var deviceComponents = {
             esphome: {
                 name: deviceName,
@@ -46,31 +17,9 @@ class Device {
             logger: {}
         }
 
-        // const exctractComponents = (componentObjects) => {
-        //     componentObjects.forEach((element, j)=>{
-        //         if(!components[element.name]){
-        //             components[element.name] = element.config
-        //         }else{
-        //             if(!Array.isArray(components[element.name])){
-        //                 components[element.name] = [components[element.name]]
-        //             }
-        //             components[element.name] = [...components[element.name],element.config]
-        //         }
-        //     })
-        // }
         this.components?.forEach((component, i) => {
             console.log("🚀 ~ file: Device.ts:62 ~ Device ~ this.components?.forEach ~ component:", component)
             if(component){
-                // try {
-                //     component.setMqttTopicPrefix(`${this.mqttPrefix != '' ? this.mqttPrefix + '/' + this.name : this.name}`);
-                // } catch (e) {
-                    
-                // }
-                // try {
-                //     component.setDeepSleep(this.deepSleep);
-                // } catch (e) {
-
-                // }
                 deviceComponents = component.attach(
                     !isNaN(parseInt(this.pinTable[i])) 
                       ? parseInt(this.pinTable[i])
@@ -78,8 +27,7 @@ class Device {
                   );
             }
         })
-        // if(this.deepSleep) exctractComponents(this.dsComponents)
-        console.log("🚀 ~ file: Device.ts:120 ~ Device ~ getComponents ~ deviceComponents:", deviceComponents)
+        //console.log("🚀 ~ file: Device.ts:120 ~ Device ~ getComponents ~ deviceComponents:", deviceComponents)
         return deviceComponents
 
     }
@@ -92,7 +40,7 @@ class Device {
         });
 
         var components = this.getComponents(deviceName, deviceDefinition)
-        console.log("🚀 ~ file: Device.ts:275 ~ Device ~ create ~ jsYaml.dump(components):", jsYaml.dump(components, {lineWidth: -1}))
+        //console.log("🚀 ~ file: Device.ts:275 ~ Device ~ create ~ jsYaml.dump(components):", jsYaml.dump(components, {lineWidth: -1}))
         return jsYaml.dump(components, {lineWidth: -1});
     }
 }
