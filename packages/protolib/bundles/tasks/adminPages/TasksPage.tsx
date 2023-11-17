5bd288342dcb59e2f2b2be512888ac27afcc16c8
Noemí Blázquez <ap0calipse@gmail.com>
2023-11-17 04:14:06 +0100

    Add run tasks in menu item

--- packages/protolib/bundles/tasks/adminPages/TasksPage.tsx
+++ packages/protolib/bundles/tasks/adminPages/TasksPage.tsx
@@ -1,17 +1,17 @@
 
-import {AdminPage, PaginatedDataSSR} from 'protolib/adminpanel/features/next'
+import { AdminPage, PaginatedDataSSR } from 'protolib/adminpanel/features/next'
 import { TaskModel } from '../models/Task'
-import {DataView, DataTable2, Chip, API} from 'protolib'
+import { DataView, DataTable2, Chip, API } from 'protolib'
 import { useRouter } from "next/router"
-import { Pencil, Zap } from '@tamagui/lucide-icons';
+import { Pencil, Zap, Play } from '@tamagui/lucide-icons';
 import { usePageParams } from '../../../next';
 import { Spinner } from 'tamagui';
 
 export const TasksPage = {
-    component: ({pageState, sourceUrl, initialItems, pageSession}:any) => {
+    component: ({ pageState, sourceUrl, initialItems, pageSession }: any) => {
         const router = useRouter()
-        
-        const {replace} = usePageParams(pageState)
+
+        const { replace } = usePageParams(pageState)
         return (<AdminPage title="Tasks" pageSession={pageSession}>
             <DataView
                 defaultView={'list'}
@@ -23,29 +23,37 @@ export const TasksPage = {
                 rowIcon={Zap}
                 columns={DataTable2.columns(
                     DataTable2.column("name", "name", true, undefined, true, '350px'),
-                    DataTable2.column("api", "api", true, row => <Chip text={row.api?'yes':'no'} color={row.api ? '$color5':'$gray5'} />, true),
+                    DataTable2.column("api", "api", true, row => <Chip text={row.api ? 'yes' : 'no'} color={row.api ? '$color5' : '$gray5'} />, true),
                     DataTable2.column("api route", "apiRoute", true, undefined, true, '350px'),
                     DataTable2.column("num. executions", "numExecutions", true, undefined, true, '150px'),
-                    DataTable2.column("status", "status", true, row => <Chip loading={row.status == 'running'} text={row.status} color={row.status == 'running'? '$color5':'$gray5'} />)
+                    DataTable2.column("status", "status", true, row => <Chip loading={row.status == 'running'} text={row.status} color={row.status == 'running' ? '$color5' : '$gray5'} />)
                     // DataTable2.column("type", "type", true, row => <Chip text={row.type.toUpperCase()} color={row.type == 'admin' ? '$color5':'$gray5'} />),
                     // DataTable2.column("from", "from", true, row => <Chip text={row.from?.toUpperCase()} color={row.from == 'cmd' ? '$blue5':'$gray5'} />),
                     // DataTable2.column("created", "createdAt", true, row => moment(row.createdAt).format(format)),
                     // DataTable2.column("last login", "lastLogin",true, row => row.lastLogin ? <Chip text={moment(row.lastLogin).format(format)} color={'$gray5'} /> : <Chip text={'NEVER'} color={'$yellow6'} /> )
                 )}
                 // hideAdd={true}
-                extraMenuActions = {[
+                extraMenuActions={[
                     {
-                        text:"Edit Task file", 
-                        icon:Pencil, 
+                        text: "Run task",
+                        icon: Play,
+                        action: (element) => { alert("run!") },
+                        isVisible: (data) => true
+                    },
+                    {
+                        text: "Edit Task file",
+                        icon: Pencil,
                         action: (element) => { replace('editFile', element.getDefaultFilePath()) },
-                        isVisible: (data)=>true}
+                        isVisible: (data) => true
+                    }
+
                 ]}
-                model={TaskModel} 
+                model={TaskModel}
                 pageState={pageState}
                 icons={{}}
             />
         </AdminPage>)
-    }, 
+    },
     getServerSideProps: PaginatedDataSSR('/adminapi/v1/tasks', ['admin', 'editor'], {
         orderBy: 'status',
         orderDirection: 'desc'
