57ba5853ff52ba77788e27a9baf69ca6d0985d00
jcarlosn <jcarlos.norte@gmail.com>
2023-11-18 19:12:42 +0100

    Styles in headercontent

--- packages/app/layout/HeaderContents.tsx
+++ packages/app/layout/HeaderContents.tsx
@@ -1,14 +1,14 @@
 import {HeaderContents as ProtoHeaderContents, HeaderContentsProps, HeaderLink, useSession, ConnectionIndicator, Tinted} from 'protolib'
 import {Text} from 'tamagui'
 import { useAtom } from 'jotai'
-import { Theme, XStack } from '@my/ui';
+import { Paragraph, Theme, XStack } from '@my/ui';
 import { Cloud, CloudOff } from '@tamagui/lucide-icons';
 
 export const HeaderContents = (props: HeaderContentsProps) => {
   const [session] = useSession();
 
   return <ProtoHeaderContents 
-      logo={<Text>Protofy</Text>} 
+      logo={<Paragraph mr={"$2"}><Text fontSize={20} fontWeight={"400"}>Protofy</Text></Paragraph>} 
       rightArea={<XStack ai="center">
         {props.topBar }
         <Tinted>{session.loggedIn ? <HeaderLink href="/profile">{session.user.id}</HeaderLink>:<HeaderLink href="/auth/login">Login</HeaderLink>}</Tinted>
