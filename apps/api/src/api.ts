c47dbafcc3b9d6d6c0b384180c61da9531ca9d2b
Marccc <marc@protofy.xyz>
2023-11-27 17:32:37 +0100

    Adds deviceSub&Pub functions to api & Refactors IotRouter.tpl

--- apps/api/src/api.ts
+++ apps/api/src/api.ts
@@ -1,10 +1,37 @@
-import {app, getMQTTClient} from 'protolib/api'
+import { app, getMQTTClient } from 'protolib/api'
 import * as path from 'path';
 import * as fs from 'fs';
 import BundleAPI from 'app/bundles/apis'
 const modulesDir = path.join(__dirname, 'modules');
 
+const mqtt = getMQTTClient()
+
+const topicSub = (topic, cb) => {
+    mqtt.subscribe(topic)
+    mqtt.on("message", (topic, message) => {
+        const parsedMessage = message.toString();
+        cb(parsedMessage, topic)
+    });
+};
+
+const topicPub = (topic, data) => {
+    mqtt.publish(topic, data)
+}
+
+const deviceSub = (deviceName, component, componentName, cb) => {
+    return topicSub(deviceName + '/' + component + '/' + componentName + '/state', cb)
+}
+
+const devicePub = async (deviceName, component, componentName, command) => {
+    if (typeof command == "string") {
+        topicPub(deviceName + '/' + component + '/' + componentName + '/command', command)
+    } else {
+        topicPub(deviceName + '/' + component + '/' + componentName + '/command', JSON.stringify(command))
+    }
+}
+
 fs.readdir(modulesDir, (error, files) => {
+
     if (error) {
         return console.error('Error reading modules directory: ' + error);
     }
@@ -12,11 +39,11 @@ fs.readdir(modulesDir, (error, files) => {
     files.forEach((file) => {
         if (path.extname(file) === '.ts') {
             require(path.join(modulesDir, file));
-            console.log(`API Module loaded: ${file.substr(0, file.length-3)}`);
+            console.log(`API Module loaded: ${file.substr(0, file.length - 3)}`);
         }
     })
 })
 
-BundleAPI(app, {mqtt:getMQTTClient()})
+BundleAPI(app, { mqtt, devicePub, deviceSub })
 
 export default app
--- packages/protolib/bundles/apis/templates/IOT Router.tpl
+++ packages/protolib/bundles/apis/templates/IOT Router.tpl
@@ -1,37 +1,14 @@
 export default (app, context) => {
 
-    const { mqtt } = context
-    const listeners = [];
+    const { devicePub, deviceSub } = context
 
-    mqtt.on("message", (topic, message) => {
-        const parsedMessage = message.toString();
-        listeners.filter(l => topic.startsWith(l.topic)).forEach(l => l.cb(parsedMessage, topic));
-    });
+    ///YOUR LOGIC HERE:
 
-    mqtt.on('connect', function () {
-        mqtt.subscribe('#')
-    })
-
-    const topicSub = (topic, cb) => {
-        listeners.push({ topic: topic, cb: cb });
-    };
-
-    const topicPub = (topic, data) => {
-        mqtt.publish(topic, data)
-    }
-
-    const deviceSub = (deviceName, component, componentName, cb) => {
-        console.log(topicSub(deviceName + '/' + component + '/' + componentName + '/state', cb))
-    }
-
-    const devicePub = async (deviceName, component, componentName, command) => {
-        if (typeof command == "string") {
-            topicPub(deviceName + '/' + component + '/' + componentName + '/command', command)
-        } else {
-            topicPub(deviceName + '/' + component + '/' + componentName + '/command', JSON.stringify(command))
-        }
-    }
-
-    ///YOUR LOGIC HERE :
+    //IoT device flow example:
+    // deviceSub('testdevice', 'binary_sensor', 'testbutton', (message) => {
+    //     message == 'ON' ?
+    //         devicePub('testdevice', 'switch', 'testrelay', 'OFF')
+    //         : devicePub('testdevice', 'switch', 'testrelay', 'ON')
+    // })
 
 }
