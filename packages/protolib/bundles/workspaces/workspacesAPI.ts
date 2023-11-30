9bb651dd9743a9893316813a2a1398b1426f7fdc
jcarlosn <jcarlos.norte@gmail.com>
2023-11-30 05:16:47 +0100

    Dynamic root in workspaceapi

--- packages/protolib/bundles/workspaces/workspacesAPI.ts
+++ packages/protolib/bundles/workspaces/workspacesAPI.ts
@@ -1,16 +1,16 @@
 import { promises as fs } from 'fs';
 import * as fspath from 'path';
-import { CreateApi } from '../../api'
+import { CreateApi, getRoot } from '../../api'
 import { WorkspaceModel } from './WorkspaceModel';
 
 
-const PROJECT_WORKSPACE_DIR = process.env.FILES_ROOT ?? "../../";
-const workspacesDir = fspath.join(PROJECT_WORKSPACE_DIR,"/packages/app/bundles/custom/workspaces/")
+
+const workspacesDir = (root) => fspath.join(root,"/packages/app/bundles/custom/workspaces/")
 
 const customGetDB = (path, req, session) => {
   const db = {
     async *iterator() {
-        const workspaces = (await fs.readdir(workspacesDir)).filter(f => f != 'index.tsx')     
+        const workspaces = (await fs.readdir(workspacesDir(getRoot(req)))).filter(f => f != 'index.tsx')     
         for (const workspace of workspaces) {
           if(workspace) yield [workspace, JSON.stringify({name:workspace.split('.')[0]})];
         }
