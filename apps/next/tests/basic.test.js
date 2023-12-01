1d2bf5b64a646243597dbb03d03498311c4898d1
jcarlosn <jcarlos.norte@gmail.com>
2023-12-01 16:38:56 +0100

    Simplify test code

--- apps/next/tests/basic.test.js
+++ apps/next/tests/basic.test.js
@@ -13,19 +13,18 @@ let Zombie = require('zombie')
 describe("Basic tests", () => {
     const browser = new Zombie()
     browser.site = 'http://127.0.0.1:8080/'; // 0.0.0.0:8080
-    const onRequest = (request, done) => {
-        // console.log(request.url)
-        if(request.url.includes("_next/static/chunks/react-refresh.js")) {
-            browser.removeListener('request', onRequest);
-            done()
-        }
-    }
 
     afterAll(() => {
         browser.destroy()
     })
+
     beforeAll((done) => {
-        browser.on('request', (request) => onRequest(request, done))
+        browser.on('request', (request) => {
+            // console.log(request.url)
+            if(request.url.includes("_next/static/chunks/react-refresh.js")) {
+                done()
+            }
+        })
         browser.visit('/', () => {})
     });
 
