5cddec92255ae24e5e6eeee52368d6932defcf31
jcarlosn <jcarlos.norte@gmail.com>
2023-12-01 16:38:02 +0100

    Basic e2e test passing

--- apps/next/tests/basic.test.js
+++ apps/next/tests/basic.test.js
@@ -7,23 +7,28 @@ window.matchMedia = window.matchMedia || function () {
     };
 };
 
+const { after } = require('node:test');
 let Zombie = require('zombie')
 
 describe("Basic tests", () => {
     const browser = new Zombie()
-    // browser.on('event', (event, target) => {console.log('EVENT RECIEVED', JSON.stringify(event))})
     browser.site = 'http://127.0.0.1:8080/'; // 0.0.0.0:8080
-    beforeAll((done) => {
-        browser.on('request', (request) => {
-            // console.log(request.url)
-            if(request.url.includes("_next/static/chunks/react-refresh.js")) {
-                done()
-            }
-        })
-        browser.visit('/', () => {
+    const onRequest = (request, done) => {
+        // console.log(request.url)
+        if(request.url.includes("_next/static/chunks/react-refresh.js")) {
+            browser.removeListener('request', onRequest);
             done()
-        })
+        }
+    }
+
+    afterAll(() => {
+        browser.destroy()
+    })
+    beforeAll((done) => {
+        browser.on('request', (request) => onRequest(request, done))
+        browser.visit('/', () => {})
     });
+
     it("should be able to interact with the web", () => {
         const content = browser.query('#__next')
         expect(content).not.toBeNull()
