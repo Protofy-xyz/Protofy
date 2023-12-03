abd37fd49afe48c40c8075237d983838e783263c
Noemí Blázquez <ap0calipse@gmail.com>
2023-12-03 16:37:37 +0100

    Check node version and fail < 18

--- prepare.js
+++ prepare.js
@@ -1,5 +1,13 @@
 const fs = require('fs')
 const { execSync } = require('child_process');
+const semver = require('semver');
+const requiredVersion = '>=18.0.0';
+
+if (!semver.satisfies(process.version, requiredVersion)) {
+    console.error(`Protofy requires node version: ${requiredVersion}. Current version ${process.version}.`);
+    console.error('If you need help, join our discord: https://discord.gg/VpeZxMFfYW')
+    process.exit(1);
+  }
 
 const isLatest = process.argv.length > 2 && process.argv[2] == '--latest'
 
