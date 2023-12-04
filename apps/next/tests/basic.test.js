c480f87849d530e08420d6944e297d30d9382ef5
Gerard <gerard@protofy.xyz>
2023-12-04 09:59:34 +0100

    adds test that navigates to login

--- apps/next/tests/basic.test.js
+++ apps/next/tests/basic.test.js
@@ -3,7 +3,7 @@ let Zombie = require('zombie')
 
 describe("Basic tests", () => {
     const browser = new Zombie()
-    browser.site = 'http://127.0.0.1:8080/'; // 0.0.0.0:8080
+    browser.site = 'http://127.0.0.1:8080/';
 
     afterAll(() => {
         browser.destroy()
@@ -12,15 +12,23 @@ describe("Basic tests", () => {
     beforeAll((done) => {
         browser.on('request', (request) => {
             // console.log(request.url)
-            if(request.url.includes("_next/static/chunks/react-refresh.js")) {
+            if (request.url.includes("_next/static/chunks/react-refresh.js")) {
                 done()
             }
         })
-        browser.visit('/', () => {})
+        browser.visit('/', () => { })
     });
 
     it("should be able to interact with the web", () => {
         const content = browser.query('#__next')
         expect(content).not.toBeNull()
+    })
+
+    it("should detect login element at header and navigate to login page", () => {
+        const loginElementId = "header-login-link"
+        const loginElement = browser.query(`#${loginElementId}`)
+        if (!loginElement) fail(`Login element with id "${loginElementId}" not found`)
+        browser.clickLink(`#${loginElementId}`)
+        expect(browser.location.href.split(browser.site)[1]).toBe("auth/login")
     })
 })
