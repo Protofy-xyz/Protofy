d8a21ec52e8b7b7ed7a9b282c54c4de87ad22d93
noreplydev <contact.cristiansanchez@gmail.com>
2023-12-05 13:25:38 +0100

    Now homepage accepts props

--- apps/next/pages/index.tsx
+++ apps/next/pages/index.tsx
@@ -1,11 +1,11 @@
 import nextPages from 'app/bundles/nextPages'
 import { useSession } from 'protolib'
 
-export default function Page(props:any) {
+export default function Page(props: any) {
   useSession(props.pageSession)
   return (
     <>
-      {nextPages["/"].component()}
+      {nextPages["/"].component(props)}
     </>
   )
 }
