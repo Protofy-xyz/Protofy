916a5535ed9975bb9f5e271b9bfd26e17f1fbe28
noreplydev <contact.cristiansanchez@gmail.com>
2023-12-08 17:38:18 +0000

    Create an abstraction for the flows component

--- apps/next/pages/protoflow.tsx
+++ apps/next/pages/protoflow.tsx
@@ -4,25 +4,8 @@ import { useSession, withSession, SSR } from 'protolib'
 import theme from 'visualui/src/components/Theme'
 
 import { useEditorStore } from 'visualui/src/store/EditorStore';
-import { RenderNode } from 'visualui/src/components/RenderNode';
-import paletteComponents from 'visualui/src/palettes';
-import EditorLayout from "visualui/src/components/EditorLayout";
-import { Sidebar } from "visualui/src/components/Sidebar";
-import MainPanel from "visualui/src/components/MainPanel";
-import Monaco from "visualui/src/components/Monaco";
-import { getMissingJsxImports, getSource } from "visualui/src/utils/utils";
-import { withTopics } from "react-topics";
 import { ToggleGroup, Button, XStack } from "@my/ui"
-import UIMasks from 'visualui/src/masks/UI.mask.json';
-import { SidebarItem } from "visualui/src/components/Sidebar/SideBarItem";
-import dynamic from 'next/dynamic';
-import { useThemeSetting } from '@tamagui/next-theme';
-const FlowsWidget = dynamic(() => import('protolib/adminpanel/features/components/FlowsWidget'), {
-  loading: () => <>
-    Loading
-  </>,
-  ssr: false
-})
+import {Flows } from 'protolib/adminpanel/features/components/Flows';
 
 export default function ProfilePage(props: any) {
     return (
@@ -31,7 +14,7 @@ export default function ProfilePage(props: any) {
                 <title>Protofy - Profile</title>
             </Head>
             <XStack height="100vh" width="100vw">
-                <FlowsWidget sourceCode={"// this is a test"}/>
+                <Flows sourceCode={"// Test of sourcecode"}/>
             </XStack>
 
         </>
