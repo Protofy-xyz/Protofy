62b99440b664e511cccf264ed9bf0e848d886543
jcarlosn <jcarlos.norte@gmail.com>
2023-12-10 15:21:20 +0100

    Removes environment system

--- apps/proxy/index.js
+++ apps/proxy/index.js
@@ -1,10 +1,6 @@
 const Redbird = require('redbird');
 const cookie = require('cookie');
-const fs = require('fs');
 
-const SiteConfigData = (fs.readFileSync('../../packages/app/conf.js').toString().split('\n').filter(line => !line.startsWith('export')).join('\n'))+"\nreturn SiteConfig;\n"
-const wrappedString = `(function() { ${SiteConfigData} })()`;
-const SiteConfig = eval(wrappedString);
 const isProduction = process.env.NODE_ENV === 'production';
 
 const APIUrl = process.env.API_URL ?? 'http://localhost:' + (isProduction ? '4001' : '3001');
@@ -30,25 +26,6 @@ var customResolver1 = function (host, url, req) {
     addClientIpHeader(req);
 
     if (/^\/api\//.test(url)) {
-        try {
-            const cookies = parseCookies(req);
-            const session = JSON.parse(cookies['session'] ?? '{}')
-            const query = new URLSearchParams(url.split('?')[1]);
-            const __env = query.get('__env');
-            const requestedSession = __env? __env : session.environment
-
-            if(requestedSession) {
-                const enviro = SiteConfig.environments.find(e => e.name == requestedSession)
-                if(enviro) {
-                    //console.log('Forcing environment from cookie: ', requestedSession)
-                    return enviro.api
-                } else {
-                    console.error("Invalid environment found in cookie, ignoring: ", requestedSession)
-                }
-            }
-        } catch(e) {
-            console.error("Error reading cookies for routing...")
-        }
         return APIUrl;
     }
 };
@@ -59,24 +36,6 @@ var customResolver2 = function (host, url, req) {
     addClientIpHeader(req);
 
     if (/^\/adminapi\//.test(url) || url === '/websocket') {
-        try {
-            const cookies = parseCookies(req);
-            const session = JSON.parse(cookies['session'] ?? '{}')
-            const query = new URLSearchParams(url.split('?')[1]);
-            const __env = query.get('__env');
-            const requestedSession = __env? __env : session.environment
-            if(requestedSession) {
-                const enviro = SiteConfig.environments.find(e => e.name == requestedSession)
-                if(enviro) {
-                    // console.log('Forcing environment from cookie: ', requestedSession)
-                    return enviro.adminApi
-                } else {
-                    console.error("Invalid environment found in cookie, ignoring: ", requestedSession)
-                }
-            }
-        } catch(e) {
-            console.error("Error reading cookies for routing...")
-        }
         return AdminAPIUrl;
     }
 };
--- packages/protolib/components/AdminPanel.tsx
+++ packages/protolib/components/AdminPanel.tsx
@@ -1,11 +1,7 @@
-import { Theme, XStack } from 'tamagui'
+import { XStack } from 'tamagui'
 import { PanelLayout } from 'app/layout/PanelLayout'
 import { SelectList, useWorkspaces, useUserSettings, useSession, PanelMenu} from 'protolib'
 import Workspaces from 'app/bundles/workspaces'
-import { SiteConfig } from 'app/conf'
-import {devMode} from 'protolib/base/env'
-import {getApiUrl} from 'protolib/base'
-import { useUpdateEffect } from 'usehooks-ts'
 
 const menuData = {}
 
@@ -24,46 +20,16 @@ const WorkspaceSelector = () => {
     />:null
 }
 
-const EnvironmentSelector = ({environments}) => {
-    const [settings, setSettings] = useUserSettings()
-    const [session, setSession] = useSession()
-    
-    useUpdateEffect(() => {
-        document.location.reload()
-        //@ts-ignore
-    }, [session.environment])
-    return settings.workspace?<SelectList 
-        triggerProps={{o:0.8, bc:"transparent", bw: 0}}
-        valueProps={{o:0.8}}
-        f={1} 
-        title={"Environments"}
-        value={session.environment ?? environments[0].name}
-        elements={environments.map(e=>e.name)}
-        setValue={(v) => {setSession({...session, environment:v})}}
-    />:null
-}
-
-
-
 export const AdminPanel = ({children }) => {
     const [settings] = useUserSettings()
     const userSpaces = useWorkspaces()
     const [session, setSession] = useSession()
     
     const currentWorkspace = settings && settings.workspace? settings.workspace : userSpaces[0]
-    const environments = [{
-        name: devMode?'development':'production',
-        address: getApiUrl()
-    }, ...(SiteConfig.environments ?? [])].filter((obj, index, self) => {
-        return index === self.findIndex((t) => (
-            t.name === obj.name
-        ));
-    });
     
     // console.log('userSpaces: ', userSpaces, 'current Workspace: ', currentWorkspace)
     return (Workspaces[currentWorkspace] ? <PanelLayout topBar={<>
         <XStack $lg={{display:"none"}}>
-            <XStack>{environments.length > 1 && <EnvironmentSelector environments={environments} />}</XStack>
             <XStack>{userSpaces.length > 1 && <WorkspaceSelector />}</XStack>
         </XStack>
     </>} menuContent={<PanelMenu workspace={Workspaces[currentWorkspace]} />}>
