3b6eb8091ca66f6c4f217afbb2c70b765bf57209
noreplydev <contact.cristiansanchez@gmail.com>
2023-12-11 11:24:01 +0000

    Changed devices schema

--- packages/protolib/bundles/devices/devices/devicesSchemas.ts
+++ packages/protolib/bundles/devices/devices/devicesSchemas.ts
@@ -3,8 +3,9 @@ import { Schema } from 'protolib/base'
 import { AutoModel } from 'protolib/base'
 
 export const DevicesSchema = Schema.object({
-  name: z.string().hint("Device name").static().regex(/^[a-z]+$/, "Only lower case chars").id(),
-  deviceDefinition: z.string().hidden(),
+  name: z.string().hint("Device name").static().regex(/^[a-z0-9A-Z]+$/, "Only lower case chars or numbers").id().search(),
+  deviceDefinition: z.string(),
+  substitutions: z.record(z.string().optional(), z.any().optional()).optional(),
   subsystem: z.record(z.string(), z.any()).optional(),
   data: z.array(z.record(z.string(), z.any())).optional()
 })
