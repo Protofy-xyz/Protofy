8a09891d3b7f871d9552bab2980bc4fb63c317d1
Noemí Blázquez <ap0calipse@gmail.com>
2023-12-15 04:52:50 +0100

    Work in slides component

--- packages/protolib/components/Slides.tsx
+++ packages/protolib/components/Slides.tsx
@@ -1,6 +1,4 @@
-import { InteractiveIcon } from 'protolib'
-import { XStack, YStack, Text, Stack } from '@my/ui'
-import { ChevronLeft, ChevronRight } from '@tamagui/lucide-icons'
+import { XStack, YStack, Text, Stack, Button } from '@my/ui'
 import { useState } from 'react'
 import { Tinted } from './Tinted'
 
@@ -13,42 +11,50 @@ export const Slides = ({ slides }) => {
 
     const titlesUpToCurrentStep = slides
         .filter((_, index) => index <= step)
-        .map(slide => slide.title)
+        .map(slide => slide.name)
         .join(" / ");
 
     return (
-        <YStack w="40vw" h="70vh" minWidth={"800px"} p="$3" f={1}>
-            <Tinted>
-                <XStack mt="$4" justifyContent="space-between" width="100%">
-                    <Stack flex={1}>
-                        <Text fontWeight={"600"} fontSize={16} color="$gray9">{titlesUpToCurrentStep}</Text>
-                    </Stack>
-                    <Stack flex={1} alignItems="flex-end">
-                        <Text fontWeight={"600"} fontSize={16} color="$gray9">[{step+1}/{totalSlides}]</Text>
-                    </Stack>
-                </XStack>
+        <YStack w={800} h={700} p="$3" f={1}>
 
-                <InteractiveIcon
-                    Icon={ChevronRight}
-                    onPress={(e) => {
+            <XStack mt="$4" justifyContent="space-between" width="100%">
+                <Stack flex={1}>
+                    <Text fontWeight={"600"} fontSize={16} color="$gray9">{titlesUpToCurrentStep}</Text>
+                </Stack>
+                <Stack flex={1} alignItems="flex-end">
+                    <Text fontWeight={"600"} fontSize={16} color="$gray9">[{step + 1}/{totalSlides}]</Text>
+                </Stack>
+            </XStack>
+
+            <Tinted>
+                <Stack>
+                    <Text fontWeight={"600"} fontSize={34} color="$color9">{slides[step].title}</Text>
+                </Stack>
+            </Tinted>
+
+            <Stack mt={"$6"}>
+                {slides[step].component}
+            </Stack>
+
+            <XStack gap={40} jc='center' mb={"$5"}  flex={1} alignItems="flex-end">
+            {step !== 0?<Button w={250} onPress={(e) => {
+                    e.stopPropagation();
+                    if (step > 0) {
+                        setStep(prev_step)
+                    }
+                }} >Back
+                </Button>:<></>}
+                <Tinted>
+                    <Button w={250} onPress={(e) => {
                         e.stopPropagation();
                         if (post_step) {
                             setStep(post_step)
                         }
-                    }}
-                    ml={"$3"}
-                    disabled={post_step ? false : true} />
-                <InteractiveIcon
-                    Icon={ChevronLeft}
-                    onPress={(e) => {
-                        e.stopPropagation();
-                        if (step > 0) {
-                            setStep(prev_step)
-                        }
+                    }} >{totalSlides=== step+1?"Create":"Next"}
+                    </Button>
+                </Tinted>
+            </XStack>
 
-                    }} ml={"$3"}
-                    disabled={step === 0} />
-            </Tinted>
         </YStack>
     );
 }
