f57ca8c1de9c7c13971129fc0b4cdcc13e30b0d1
jcarlosn <jcarlos.norte@gmail.com>
2023-12-15 22:47:32 +0100

    Run with bare express when compiled

--- apps/electron/index.js
+++ apps/electron/index.js
@@ -1,33 +1,40 @@
 const { app, BrowserWindow } = require('electron')
-const next = require('next')
 const express = require('express');
 
-//TODO: copy @types/react y @types/node with a postbuild hook in electron-bulder
 process.chdir(__dirname);
 
 const dev = process.env.NODE_ENV === 'development';
-const nextApp = next({ dev })
-const handle = nextApp.getRequestHandler()
+let handle;
+let prepare;
+let server;
 const PORT = 7777;
+
+if(dev) {
+  const next = require('next')
+  const nextApp = next({ dev })
+  handle = nextApp.getRequestHandler()
+  prepare = () => nextApp.prepare()
+  server = express()
+  server.all('*', (req, res) => {
+    return handle(req, res);
+  });
+} else {
+  prepare = async () => true
+  server = express()
+  server.use(express.static('./.next/server/pages'));
+}
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
   })
-  win.loadURL(`http://localhost:${PORT}/screen`)
+  win.loadURL(`http://localhost:${PORT}/screen`+(dev?'':'.html'))
   //win.webContents.openDevTools()
 }
 
 app.whenReady().then(() => {
-  nextApp.prepare().then(() => {
-    const server = express();
-  
-    server.all('*', (req, res) => {
-      return handle(req, res);
-    });
-  
-
+  prepare().then(() => {
     server.listen(PORT, (err) => {
       if (err) throw err;
       console.log(`> Ready on http://localhost:${PORT}`);
