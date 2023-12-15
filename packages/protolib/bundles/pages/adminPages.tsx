98eff9c5fe60494a6e319c2776bc00ab070c0637
Noemí Blázquez <ap0calipse@gmail.com>
2023-12-15 03:55:20 +0100

    Prepares pages adminpage to use a custom form

--- packages/protolib/bundles/pages/adminPages.tsx
+++ packages/protolib/bundles/pages/adminPages.tsx
@@ -1,18 +1,19 @@
-
 import { PageModel } from '.'
 import { DataView } from 'protolib'
 import { DataTable2, Chip, API, InteractiveIcon, AdminPage, PaginatedDataSSR, NextLink } from 'protolib'
 import { z } from 'protolib/base'
 import { Paragraph, XStack, YStack, useThemeName } from '@my/ui'
-import { ExternalLink, Pencil, Eye } from '@tamagui/lucide-icons'
+import { ExternalLink, Pencil } from '@tamagui/lucide-icons'
 import { usePageParams } from '../../next';
 import { getURLWithToken } from '../../lib/Session'
 import { useState } from 'react'
 import { getPendingResult } from '../../base'
 import { usePendingEffect } from '../../lib/usePendingEffect'
 import { Tinted } from '../../components/Tinted'
-import { Button, Image } from 'tamagui'
+import { Button, Image, ScrollView } from 'tamagui'
 import { useRouter } from 'next/router'
+import { AlertDialog } from '../../components/AlertDialog'
+import { Slides } from '../../components/Slides'
 
 const environments = require('../../../app/bundles/environments')
 
@@ -21,53 +22,107 @@ const sourceUrl = '/adminapi/v1/pages'
 const objectsSourceUrl = '/adminapi/v1/objects?all=1'
 const templates = ["blank", "default", "admin"]
 
+const slides = [
+    {
+        name: "Create new page",
+        title: "Select your Template",
+        component: <h1>step 1</h1>
+    },
+    {
+        name: "Configure your page",
+        title: "Configure your page",
+        component: <h1>step 2</h1>
+    },
+    {
+        name: "lil",
+        title: "step 3",
+        component: <h1>step 3</h1>
+    }
+]
+
+const TemplatePreview = ({ template, isSelected, onPress, theme }) => {
+    const [previewVisible, setPreviewVisible] = useState(false);
+    const templateUrl = `https://raw.githubusercontent.com/Protofy-xyz/Protofy/assets/templates/${template}-${theme}.png`
+    return (
+        <YStack onPress={onPress} onHoverIn={() => setPreviewVisible(true)} onHoverOut={() => setPreviewVisible(false)} overflow='hidden' borderWidth={isSelected ? "$1" : "$0.5"} borderColor={isSelected ? "$color7" : "$gray8"} f={1} cursor='pointer' borderRadius={"$3"}>
+            <Image
+                source={{ height: 120, width: 238, uri: templateUrl }}
+            />
+            <YStack
+                display={previewVisible ? 'block' : 'none'}
+                zi={10000}
+                position='absolute'
+                right={"$2"}
+                top={"$2"}
+            >
+                <NextLink target="_blank" href={templateUrl}>
+                    <Button
+                        backgroundColor={"$color7"}
+                        size="$1.5" borderRadius={"$1"}
+                        px="$2" textProps={{ size: "$1" }}
+                        onPress={(e) => {
+                            e.stopPropagation()
+                        }}
+                    // iconAfter={<Eye size="$1" color="$color7" />}
+                    >preview</Button>
+                </NextLink>
+            </YStack>
+            <XStack jc='space-between' borderTopWidth={"$0.5"} borderColor={"$gray8"} backgroundColor={"$gray3"} py="$1" px="$2">
+                <Paragraph>{template}</Paragraph>
+            </XStack>
+        </YStack>
+    )
+}
+
+const SelectGrid = ({ children }) => {
+    return <Tinted>
+        <div
+            style={{
+                gap: 20,
+                display: 'grid',
+                gridTemplateColumns: `repeat( auto-fill, minmax(230px, 1fr) )`,
+            }}
+        >
+            {children}
+        </div>
+    </Tinted>
+}
+
 export default {
     'admin/pages': {
         component: ({ pageState, initialItems, pageSession, extraData }: any) => {
             const router = useRouter();
             const { replace } = usePageParams(pageState)
             const [objects, setObjects] = useState(extraData?.objects ?? getPendingResult('pending'))
+            const [addOpen, setAddOpen] = useState(false)
             usePendingEffect((s) => { API.get({ url: objectsSourceUrl }, s) }, setObjects, extraData?.objects)
+
 
             const getUrl = (route) => {
                 const environment = environments[document.location.hostname.split('.')[0]]
                 return environment && environment.baseUrl ? environment.baseUrl + route : route
             }
-            const TemplatePreview = ({ template, isSelected, onPress, theme }) => {
-                const [previewVisible, setPreviewVisible] = useState(false);
-                const templateUrl = `https://raw.githubusercontent.com/Protofy-xyz/Protofy/assets/templates/${template}-${theme}.png`
-                return (
-                    <YStack onPress={onPress} onHoverIn={() => setPreviewVisible(true)} onHoverOut={() => setPreviewVisible(false)} overflow='hidden' borderWidth={isSelected ? "$1" : "$0.5"} borderColor={isSelected ? "$color7" : "$gray8"} f={1} cursor='pointer' borderRadius={"$3"}>
-                        <Image
-                            source={{ height: 120, width: 238, uri: templateUrl }}
-                        />
-                        <YStack
-                            display={previewVisible ? 'block' : 'none'}
-                            zi={10000}
-                            position='absolute'
-                            right={"$2"}
-                            top={"$2"}
-                        >
-                            <NextLink target="_blank" href={templateUrl}>
-                                <Button
-                                    backgroundColor={"$color7"}
-                                    size="$1.5" borderRadius={"$1"}
-                                    px="$2" textProps={{ size: "$1" }}
-                                    onPress={(e) => {
-                                        e.stopPropagation()
-                                    }}
-                                // iconAfter={<Eye size="$1" color="$color7" />}
-                                >preview</Button>
-                            </NextLink>
-                        </YStack>
-                        <XStack jc='space-between' borderTopWidth={"$0.5"} borderColor={"$gray8"} backgroundColor={"$gray3"} py="$1" px="$2">
-                            <Paragraph>{template}</Paragraph>
-                        </XStack>
-                    </YStack>
-                )
-            }
 
             return (<AdminPage title="Pages" pageSession={pageSession}>
+                <AlertDialog
+                    integratedChat
+                    p={"$2"}
+                    pt="$5"
+                    pl="$5"
+                    setOpen={setAddOpen}
+                    open={addOpen}
+                    hideAccept={true}
+                    description={""}
+                >
+                    <YStack f={1} jc="center" ai="center">
+                        <ScrollView maxHeight={"90vh"}>
+                            <XStack mr="$5">
+                                <Slides slides={slides}></Slides>
+                            </XStack>
+                        </ScrollView>
+                    </YStack>
+                </AlertDialog>
+
                 <DataView
                     integratedChat
                     sourceUrl={sourceUrl}
@@ -90,6 +145,7 @@ export default {
                         template: z.union(templates?.map((t: string) => z.literal(t))).after("route").size(2),
                         object: z.union([z.literal("without object"), ...(objects && objects.data ? objects.data?.items.filter(o => o.features && o.features['AutoAPI']).map(o => z.literal(o.name)) : [])] as any).after('route'),
                     }}
+                    // onAddButton={() => { setAddOpen(true) }}
                     extraMenuActions={[
                         {
                             text: "Edit Page file",
@@ -103,19 +159,11 @@ export default {
                             component: (path, data, setData) => {
                                 const themeName = useThemeName();
                                 return (
-                                    <Tinted>
-                                        <div
-                                            style={{
-                                                gap: 20,
-                                                display: 'grid',
-                                                gridTemplateColumns: `repeat( auto-fill, minmax(230px, 1fr) )`,
-                                            }}
-                                        >
-                                            {
-                                                templates.map((template) => <TemplatePreview theme={themeName} template={template} isSelected={data == template} onPress={() => setData(template)} />)
-                                            }
-                                        </div>
-                                    </Tinted>
+                                    <SelectGrid>
+                                        {
+                                            templates.map((template) => <TemplatePreview theme={themeName} template={template} isSelected={data == template} onPress={() => setData(template)} />)
+                                        }
+                                    </SelectGrid>
                                 )
                             }
                         }
--- packages/protolib/components/Slides.tsx
+++ packages/protolib/components/Slides.tsx
@@ -1,6 +1,6 @@
 import { InteractiveIcon } from 'protolib'
-import { XStack, YStack } from '@my/ui'
-import { ChevronLeft, ChevronRight  } from '@tamagui/lucide-icons'
+import { XStack, YStack, Text, Stack } from '@my/ui'
+import { ChevronLeft, ChevronRight } from '@tamagui/lucide-icons'
 import { useState } from 'react'
 import { Tinted } from './Tinted'
 
@@ -11,35 +11,44 @@ export const Slides = ({ slides }) => {
     const prev_step = step > 1 ? step - 1 : 0
     const post_step = step < totalSlides - 1 ? step + 1 : null
 
+    const titlesUpToCurrentStep = slides
+        .filter((_, index) => index <= step)
+        .map(slide => slide.title)
+        .join(" / ");
+
     return (
-        <XStack ai="center" w="40vw" h="70vh" minWidth={"800px"}>
+        <YStack w="40vw" h="70vh" minWidth={"800px"} p="$3" f={1}>
             <Tinted>
-                <InteractiveIcon
-                    Icon={ChevronLeft}
-                    onPress={(e) => {
-                        e.stopPropagation();
-                        if(step>0){
-                            setStep(prev_step)
-                        }
-
-                    }} ml={"$3"}
-                    disabled={step === 0} />
-
-                <YStack>
-                    <h1>{slides[step].title}</h1>
-                </YStack>
+                <XStack mt="$4" justifyContent="space-between" width="100%">
+                    <Stack flex={1}>
+                        <Text fontWeight={"600"} fontSize={16} color="$gray9">{titlesUpToCurrentStep}</Text>
+                    </Stack>
+                    <Stack flex={1} alignItems="flex-end">
+                        <Text fontWeight={"600"} fontSize={16} color="$gray9">[{step+1}/{totalSlides}]</Text>
+                    </Stack>
+                </XStack>
 
                 <InteractiveIcon
                     Icon={ChevronRight}
                     onPress={(e) => {
                         e.stopPropagation();
-                        if(post_step) {
+                        if (post_step) {
                             setStep(post_step)
                         }
                     }}
                     ml={"$3"}
-                    disabled={post_step?false:true} />
+                    disabled={post_step ? false : true} />
+                <InteractiveIcon
+                    Icon={ChevronLeft}
+                    onPress={(e) => {
+                        e.stopPropagation();
+                        if (step > 0) {
+                            setStep(prev_step)
+                        }
+
+                    }} ml={"$3"}
+                    disabled={step === 0} />
             </Tinted>
-        </XStack>
+        </YStack>
     );
 }
