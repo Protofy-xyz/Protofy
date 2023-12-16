b605f7dcc5f38a8d46999bb5fe9e3819f73c2524
jcarlosn <jcarlos.norte@gmail.com>
2023-12-16 02:14:03 +0100

    Remove getserversideprops in electron pages

--- apps/electron/pages/screen.tsx
+++ apps/electron/pages/screen.tsx
@@ -5,5 +5,3 @@ export default function IndexPage(props:any) {
   useSession(props.pageSession)
   return <Feature.component {...props} />
 }
-
-export const getServerSideProps = Feature.getServerSideProps
