a0d4468a8bf93de4791f34f88a31a81a750d410c
jcarlosn <jcarlos.norte@gmail.com>
2023-12-16 02:12:40 +0100

    hide menubar in electron

--- apps/electron/index.js
+++ apps/electron/index.js
@@ -32,6 +32,8 @@ function createWindow() {
     width: 800,
     height: 600,
   })
+  win.setMenuBarVisibility(false);
+  
   if(dev) {
     win.loadURL(`http://localhost:${PORT}/screen`)
   } else {
