7a1199b14a61063a214b1704567eea63b4b7b0b7
noreplydev <contact.cristiansanchez@gmail.com>
2023-12-18 16:38:58 +0000

    Updated loading component in Flows

--- packages/protolib/adminpanel/features/components/Flows.tsx
+++ packages/protolib/adminpanel/features/components/Flows.tsx
@@ -1,9 +1,10 @@
 import dynamic from 'next/dynamic'
+import { XStack } from 'tamagui'
 
 const FlowsWidget = dynamic(() => import('protolib/adminpanel/features/components/FlowsWidget'), {
-  loading: () => <>
+  loading: () => <XStack jc='center' ai='center' height="100%" width="100%">
     Loading
-  </>,
+  </XStack>,
   ssr: false
 })
 
