4e000be92dfe78ccdd760d2b55d89a20a814cf0b
jcarlosn <jcarlos.norte@gmail.com>
2023-12-21 05:31:45 +0100

    Start admin panel in compiled mode

--- apps/admin/entrypoint.js
+++ apps/admin/entrypoint.js
@@ -1,20 +1,48 @@
 const next = require('next');
+var express = require('express');
+const fs = require('fs')
+const path = require('path')
 
+const isFullDev = process.env.FULLDEV
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const PORT = dev ? 3003 : 4003;
 
-app.prepare().then(() => {
-  const express = require('express');
-  const server = express();
-
-  server.all('*', (req, res) => {
-    return handle(req, res);
+if (!isFullDev) {
+  var app = express();
+  app.use((req, res, next) => {
+    const parts = req.path.split('/').filter(x => x).filter(x => x != '..').filter((x, i) => !(x=='admin'&&!i))
+    var file = path.join('.', 'out', parts.join('/'));
+    console.log('file: ', file)
+    if (fs.existsSync(file)) {
+      res.sendFile(file, { root: '.' });
+    } else {
+      if(fs.existsSync(file+'.html')) {
+        res.sendFile(file+'.html', { root: '.' });
+      } else {
+        res.sendFile('404.html', { root: './out' });
+      }
+    }
   });
 
-  const PORT = dev?3003:4003;
-  server.listen(PORT, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${PORT}`);
+  app.listen(PORT, () => {
+    console.log(`> Static server ready on http://localhost:${PORT}`);
   });
-});
+} else {
+  const app = next({ dev });
+  const handle = app.getRequestHandler();
+
+  app.prepare().then(() => {
+    const express = require('express');
+    const server = express();
+
+    server.all('*', (req, res) => {
+      return handle(req, res);
+    });
+
+    const PORT = dev ? 3003 : 4003;
+    server.listen(PORT, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${PORT}`);
+    });
+  });
+}
