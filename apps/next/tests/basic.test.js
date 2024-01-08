e258d0a3cbdb9c89ae475c4b900286ea80351fbf
gdomenech98 <gerard@protofy.xyz>
2024-01-08 09:45:19 +0000

    [TEST] testing test sample pass

--- apps/next/tests/basic.test.js
+++ apps/next/tests/basic.test.js
@@ -80,31 +80,31 @@ describe("Test entities autocreation", () => {
     }, 10000)
 
     beforeEach(async () => {
-        try {
-            driver = await new Builder()
-            .forBrowser('chrome')
-            .usingServer('http://localhost:4444/wd/hub') // URL to Selenium Hub
-            .setChromeOptions(new chrome.Options().headless().addArguments("--no-sandbox", "--disable-dev-shm-usage"))
-            .build();
-            await driver.get(HOST_URL);
-            await driver.manage().window().setRect({ width: 1920, height: 1080 });
-            await navigateToLogin(driver);
-            await signInSubmit(driver, USER_IDENTIFIER, USER_PASSWORD);
-            await navigateToWorkspace(driver);
-        }catch(e) {
-            console.error(e)
-        }
+        // try {
+        //     driver = await new Builder()
+        //     .forBrowser('chrome')
+        //     .usingServer('http://localhost:4444/wd/hub') // URL to Selenium Hub
+        //     .setChromeOptions(new chrome.Options().headless().addArguments("--no-sandbox", "--disable-dev-shm-usage"))
+        //     .build();
+        //     await driver.get(HOST_URL);
+        //     await driver.manage().window().setRect({ width: 1920, height: 1080 });
+        //     await navigateToLogin(driver);
+        //     await signInSubmit(driver, USER_IDENTIFIER, USER_PASSWORD);
+        //     await navigateToWorkspace(driver);
+        // }catch(e) {
+        //     console.error(e)
+        // }
     }, 30000)
     
 
     afterEach(async () => {
-        if (driver) {
-            try {
-               await driver.quit()
-            }catch(e){
-                console.error(e)
-            }
-        }
+        // if (driver) {
+        //     try {
+        //        await driver.quit()
+        //     }catch(e){
+        //         console.error(e)
+        //     }
+        // }
     })
 
     describe("sample", () => {
