359e058bbe7dc9459a9bcb375fb4fec7ae65194d
noreplydev <contact.cristiansanchez@gmail.com>
2024-01-17 19:12:42 +0100

    [FIX] device definition schema to show custom component on Dataview

--- packages/protolib/bundles/devices/devices/devicesSchemas.ts
+++ packages/protolib/bundles/devices/devices/devicesSchemas.ts
@@ -4,7 +4,7 @@ import { AutoModel } from 'protolib/base'
 
 export const DevicesSchema = Schema.object({
   name: z.string().hint("Device name").static().regex(/^[a-z0-9A-Z]+$/, "Only lower case chars or numbers").id().search(),
-  deviceDefinition: z.string(),
+  deviceDefinition: z.string().hidden(),
   substitutions: z.record(z.string().optional(), z.any().optional()).optional(),
   subsystem: z.record(z.string(), z.any()).optional(),
   data: z.array(z.record(z.string(), z.any())).optional()
