738a087981d0778d76b4821a9c34f19416529cee
gdomenech98 <gerard@protofy.xyz>
2024-01-17 11:53:35 +0000

    [ADD] jest now uses ts instead js. Creeated ProtoBrowser class, a helper class to work with selenium and perform some operations over Protofy web

---
+++ apps/next/tests/ProtoBrowser.ts
@@ -0,0 +1,135 @@
+import * as path from 'path';
+import * as fs from 'fs';
+
+const { Builder, By, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const { v4: uuidv4 } = require('uuid');
+
+const HOST_URL = 'http://host.docker.internal:8080/'
+
+export class ProtoBrowser {
+    driver: any;
+    constructor(driver: any) {
+        this.driver = driver
+    }
+
+    static async __newInstance__(): Promise<ProtoBrowser> {
+        let driver = await new Builder()
+            .forBrowser('chrome')
+            .usingServer('http://localhost:4444/wd/hub') // URL to Selenium Hub
+            .setChromeOptions(new chrome.Options().headless().addArguments("--no-sandbox", "--disable-dev-shm-usage"))
+            .build();
+        await driver.get(HOST_URL);
+        await driver.manage().window().setRect({ width: 1920, height: 1080 });
+        return new ProtoBrowser(driver)
+    }
+
+    async close() {
+        await this.driver.quit()
+    }
+
+    getDriver() {
+        return this.driver;
+    }
+
+    async navigateToLogin() {
+        await this.driver.wait(until.elementLocated(By.id('header-login-link')));
+        await this.driver.executeScript("document.querySelector('#header-login-link > p').click();");
+        await this.driver.wait(until.elementLocated(By.id('sign-in-btn')));
+    }
+
+    async navigateToRegister() {
+        await this.navigateToLogin()
+        await this.driver.wait(until.elementLocated(By.id('sign-up-link')));
+        const signUpLinkElem = await this.driver.findElement(By.id('sign-up-link'))
+        await signUpLinkElem.click()
+        await this.driver.wait(until.elementLocated(By.id('sign-up-btn')));
+    }
+
+    async navigateToWorkspace() {
+        await this.driver.wait(until.elementLocated(By.id('header-session-user-id')));
+        await this.driver.executeScript("document.querySelector('#layout-menu-btn').click();");
+        await this.driver.wait(until.elementLocated(By.id("pop-over-workspace-link")));
+        await this.driver.executeScript("document.querySelector('#pop-over-workspace-link').click();");
+        await this.driver.wait(async () => {
+            const currentUrl = await this.driver.getCurrentUrl();
+            return currentUrl.includes('/admin');
+        });
+    }
+
+    async signInSubmit(email: string, password: string) {
+        // Fill sign-in form
+        const inputFieldEmail = await this.driver.findElement(By.id('sign-in-email-input'));
+        await inputFieldEmail.sendKeys(email);
+        const inputFieldPassword = await this.driver.findElement(By.id('sign-in-password-input'));
+        await inputFieldPassword.sendKeys(password);
+        const signInButton = await this.driver.findElement(By.id('sign-in-btn'));
+        await signInButton.click()
+    }
+
+    async signUpFlow(email: string, password: string) {
+        // Fill sign-up form
+        const inputFieldEmail = await this.driver.findElement(By.id('sign-up-email-input'));
+        await inputFieldEmail.sendKeys(email);
+        const inputFieldPassword = await this.driver.findElement(By.id('sign-up-password-input'));
+        await inputFieldPassword.sendKeys(password);
+        const inputFieldRePassword = await this.driver.findElement(By.id('sign-up-repassword-input'));
+        await inputFieldRePassword.sendKeys(password);
+        const signUpButton = await this.driver.findElement(By.id('sign-up-btn'));
+        await signUpButton.click()
+        await this.driver.wait(async () => { // Wait to load session and be redirected to '/'
+            return (
+                (new URL(await this.driver.getCurrentUrl()).pathname === '/')
+                && until.elementLocated(By.id('header-session-user-id'))
+                && until.elementIsVisible(By.id('home-page'))
+            )
+        });
+    }
+    async navigateToWorkspaceSection(entity: string) {
+        await this.driver.get(HOST_URL + `admin/${entity}`);
+    }
+
+    async getEditableObjectCreate() {
+        /*open create dialog */
+        await this.driver.wait(until.elementLocated(By.id('admin-dataview-add-btn')));
+        await this.driver.executeScript("document.querySelector('#admin-dataview-add-btn').click();");
+        await this.driver.wait(until.elementLocated(By.id('admin-dataview-create-dlg')))
+        await this.driver.wait(until.elementLocated(By.id('admin-eo')))
+    }
+
+    async fillEditableObjectInput(field: string, value: string, debounce = undefined) {
+        /*fill input */
+        const nameInput = await this.driver.findElement(By.id(`editable-object-input-${field}`))
+        await nameInput.clear() // Clear previous values
+        const sendSlowly = debounce ?? false
+        if (sendSlowly) {
+            for (let character of value) { // Send keys slowly 
+                await nameInput.sendKeys(character);
+                await this.driver.sleep(debounce ?? 10); // Adjust the sleep time as necessary
+            }
+        } else {
+            await nameInput.sendKeys(value);
+        }
+    }
+
+    async fillEditableObjectSelect(field: string, option: string | number) {
+        /*open selectable */
+        // await driver.executeScript(`document.querySelector("#eo-select-list-${field}").parentElement.querySelector("span>li").click()`);
+        const selectListElem = await this.driver.findElement(By.xpath(`//*[@id='eo-select-list-${field}']/../span/li`))
+        await selectListElem.click()
+        /*click selectable option */
+        const selectListOptionElem = await this.driver.findElement(By.xpath(`//*[@id='eo-select-list-${field}-item-${option}']/..`))
+        await selectListOptionElem.click();
+        // await driver.executeScript(`document.querySelector("#eo-select-list-${field}-item-${option}").parentElement.click()`)
+    }
+
+    async submitEditableObject() {
+        await this.driver.executeScript(`document.querySelector("#admin-eo>div>div>div>span>span>span>button").click()`)
+    }
+
+    async takeScreenshot(name: string) {
+        const img = await this.driver.takeScreenshot(this.driver);
+        fs.writeFileSync(path.join(__dirname, `${name}.png`), img, 'base64')
+    }
+
+}
---
+++ apps/next/tests/basic.test.ts
@@ -0,0 +1,195 @@
+const { execSync } = require('child_process');
+const path = require('path')
+const { Builder, By, until } = require('selenium-webdriver');
+const chrome = require('selenium-webdriver/chrome');
+const { v4: uuidv4 } = require('uuid');
+const HOST_URL = 'http://host.docker.internal:8080/'
+import { ProtoBrowser } from './ProtoBrowser'
+
+describe("Basic tests", () => {
+    let protoBrowser: any;
+    beforeEach(async () => {
+        protoBrowser = await ProtoBrowser.__newInstance__()
+    }, 30000)
+
+    afterEach(async () => {
+        await protoBrowser.close()
+    })
+    it("should have a public sign in authentication interface", async () => {
+        await protoBrowser.navigateToLogin()
+        let driver = protoBrowser.getDriver()
+        const path = new URL(await driver.getCurrentUrl()).pathname; // TODO: create a method that returns current URL
+        expect(path).toBe('/auth/login');
+        await driver.wait(until.elementLocated(By.id('sign-in-email-input')));
+        const inputFieldEmail = await driver.findElement(By.id('sign-in-email-input'));
+        expect(inputFieldEmail).toBeTruthy();
+        const inputFieldPassword = await driver.findElement(By.id('sign-in-password-input'));
+        expect(inputFieldPassword).toBeTruthy();
+        const signInButton = await driver.findElement(By.id('sign-in-btn'));
+        expect(signInButton).toBeTruthy();
+        const signUpLink = await driver.findElement(By.id('sign-up-link'));
+        expect(signUpLink).toBeTruthy();
+    }, 30000)
+
+    it("should have a public sign up authentication interface", async () => {
+        await protoBrowser.navigateToRegister()
+        let driver = protoBrowser.getDriver()
+        const path = new URL(await driver.getCurrentUrl()).pathname;
+        expect(path).toBe('/auth/register');
+        await driver.wait(until.elementLocated(By.id('sign-up-email-input')));
+        const inputFieldEmail = await driver.findElement(By.id('sign-up-email-input'));
+        expect(inputFieldEmail).toBeTruthy();
+        const inputFieldPassword = await driver.findElement(By.id('sign-up-password-input'));
+        expect(inputFieldPassword).toBeTruthy();
+        const inputFieldRePassword = await driver.findElement(By.id('sign-up-repassword-input'));
+        expect(inputFieldRePassword).toBeTruthy();
+        const signInButton = await driver.findElement(By.id('sign-up-btn'));
+        expect(signInButton).toBeTruthy();
+        const signUpLink = await driver.findElement(By.id('sign-in-link'));
+        expect(signUpLink).toBeTruthy();
+    }, 30000)
+
+    it("should be able to register and retrieve a session using sign up interface", async () => {
+        await protoBrowser.navigateToRegister()
+        let driver = protoBrowser.getDriver()
+        const email = `randomuser-${uuidv4()}@noreply.com`
+        const password = 'changeme4321'
+        // console.log('Registering user (email): ', email)
+        await driver.wait(until.elementLocated(By.id('sign-up-email-input')));
+        await protoBrowser.signUpFlow(email, password);
+        expect(new URL(await driver.getCurrentUrl()).pathname).toBe('/')
+        // expect(until.elementLocated(By.id('header-session-user-id'))).toBe()
+    }, 30000)
+})
+
+describe("Test entities autocreation", () => {
+    const USER_IDENTIFIER = 'user@user.user'
+    const USER_PASSWORD = 'user1234'
+    let protoBrowser: any;
+
+    beforeAll(async () => { // Create a user with admin role
+        try {
+            const output = execSync(`cd ${path.join(__dirname, '..', '..', '..')} && yarn add-user ${USER_IDENTIFIER} ${USER_PASSWORD} admin`, { encoding: 'utf-8', stdio: 'inherit' })
+            expect(output.includes('Done')).toBeTruthy();
+        } catch (e) { } // Prevent crash when user already exist
+        // Login and navigate to workspace
+        protoBrowser = await ProtoBrowser.__newInstance__()
+        await protoBrowser.navigateToLogin();
+        await protoBrowser.signInSubmit(USER_IDENTIFIER, USER_PASSWORD);
+        await protoBrowser.navigateToWorkspace();
+    }, 60000)
+
+    afterAll(async () => {
+        await protoBrowser.close()
+    }, 20000)
+
+    describe("test api creations", () => {
+        const TEMPLATES = {
+            Automatic_CRUD: 0,
+            Automatic_CRUD_Custom_Storage: 1,
+            IOT_Router: 2,
+            Empty: 3
+        }
+
+        const OBJECTS = {
+            Without_Object: 0
+        }
+        it("should be able to create an empty api", async () => {
+            await protoBrowser.navigateToWorkspaceSection('apis')
+            await protoBrowser.getEditableObjectCreate()
+            const apiName = 'testapi'
+            await protoBrowser.fillEditableObjectInput('name', apiName)
+            await protoBrowser.fillEditableObjectSelect('template', TEMPLATES.Empty)
+            await protoBrowser.fillEditableObjectSelect('object', OBJECTS.Without_Object)
+            await protoBrowser.submitEditableObject()
+            let driver = await protoBrowser.getDriver()
+            driver.wait(until.elementLocated(By.id(`apis-datatable-${apiName}`)))
+            const dt_api_name = await driver.findElement(By.id(`apis-datatable-${apiName}`)).getText()
+            expect(dt_api_name).toBe(apiName);
+        }, 30000)
+    })
+
+    describe("test object creation", () => {
+        beforeEach(async () => {
+            await protoBrowser.navigateToWorkspaceSection('objects')
+            await protoBrowser.getEditableObjectCreate()
+        }, 30000)
+
+        it("should be able to create a simple object", async () => {
+            const objectName = 'testObject'
+            await protoBrowser.fillEditableObjectInput('name', objectName)
+            // Open editable form
+            let driver = protoBrowser.getDriver();
+            await driver.executeScript('document.querySelector("#eo-formgroup>span>div>div>span>button").click()')
+            await driver.executeScript('document.querySelector("#eo-obj-comp-btn").click()')
+            // Fill object key
+            const keyName = "myKey"
+            const locator = By.xpath("//*[@id='eo-add-field-input']/span/input")
+            await driver.wait(until.elementLocated(locator))
+            const keyInput = await driver.findElement(locator)
+            await keyInput.clear() // Clear previous values
+            for (let character of keyName) { // Send keys slowly 
+                await keyInput.sendKeys(character)
+                await driver.sleep(10); // Adjust the sleep time as necessary
+            }
+            await driver.executeScript('document.querySelector("#alert-dlg-accept").click()')
+            await protoBrowser.submitEditableObject()
+            await driver.wait(until.elementLocated(By.id(`objects-datatable-${objectName}`)))
+            const dt_object_name = await driver.findElement(By.id(`objects-datatable-${objectName}`)).getText()
+            expect(dt_object_name).toBe(objectName);
+        }, 30000)
+    })
+
+    describe("test page entity", () => {
+        const PAGE_TEMPLATES = {
+            BLANK: "blank",
+            DEFAULT: "default",
+            ADMIN: "admin",
+            LANDING: "landing",
+            ECOMERCE: "ecomerce",
+            NEWSFEED: "newsfeed",
+        }
+
+        describe("test page creation", () => {
+            beforeEach(async () => {
+                await protoBrowser.navigateToWorkspaceSection('pages')
+                await protoBrowser.getEditableObjectCreate()
+            }, 30000)
+            it("should be able to create a blank page", async () => {
+                const pageName = 'testpage'
+                const pageRoute = 'testpage'
+                // Select template
+                let driver = protoBrowser.getDriver()
+                const pageTemplateElem = await driver.findElement(By.id(`pages-template-${PAGE_TEMPLATES.BLANK}`))
+                await pageTemplateElem.click()
+                const addPageButtonElem1 = await driver.findElement(By.id(`admin-pages-add-btn`))
+                await addPageButtonElem1.click()
+                // Configure page
+                await protoBrowser.fillEditableObjectInput('name', pageName, 10)
+                await protoBrowser.fillEditableObjectInput('route', pageRoute, 10)
+                const addPageButtonElem2 = await driver.findElement(By.id(`admin-pages-add-btn`))
+                await addPageButtonElem2.click()
+                await driver.wait(until.elementLocated(By.id(`pages-datatable-${pageName}`)))
+                const dt_page_name = await driver.findElement(By.id(`pages-datatable-${pageName}`)).getText()
+                expect(dt_page_name).toBe(pageName);
+            }, 30000)
+        })
+
+        describe("test edit page", () => {
+            beforeEach(async () => {
+                await protoBrowser.navigateToWorkspaceSection('pages')
+            }, 30000)
+            it("should be able to edit the page", async () => {
+                let driver = protoBrowser.getDriver()
+                await driver.wait(until.elementLocated(By.id('admin-dataview-add-btn')));
+                const pageOptionsBtn = await driver.findElement(By.id(`more-btn-home`))
+                await pageOptionsBtn.click()
+                await driver.wait(until.elementLocated(By.id("more-btn-home-option-1")));
+                const pageOptionsBtnEdit = await driver.findElement(By.id(`more-btn-home-option-1`))
+                await pageOptionsBtnEdit.click()
+                await driver.wait(until.elementLocated(By.id('file-widget-home')));
+            }, 10000)
+        })
+
+    })
+})
