74808a8b9e0d4e0ba0e8e96861c193f0c6898f2c
gdomenech98 <gerard@protofy.xyz>
2024-01-17 15:27:24 +0000

    [TESTCOMMIT-MAYBE BRAKE] decrease test timeout

--- apps/next/tests/basic.test.ts
+++ apps/next/tests/basic.test.ts
@@ -28,7 +28,7 @@ describe("Basic tests", () => {
     let protoBrowser: ProtoBrowser;
     beforeEach(async () => {
         protoBrowser = await ProtoBrowser.__newInstance__()
-    }, 30000)
+    }, 15000)
 
     afterEach(async () => {
         await protoBrowser.close()
@@ -40,7 +40,7 @@ describe("Basic tests", () => {
         expect(await protoBrowser.waitForElement(By.id('sign-in-password-input'))).toBeTruthy();
         expect(await protoBrowser.waitForElement(By.id('sign-in-btn'))).toBeTruthy();
         expect(await protoBrowser.waitForElement(By.id('sign-up-link'))).toBeTruthy();
-    }, 30000)
+    }, 10000)
 
     it("should have a public sign up authentication interface", async () => {
         await protoBrowser.navigateToRegister()
@@ -50,14 +50,14 @@ describe("Basic tests", () => {
         expect(await protoBrowser.waitForElement(By.id('sign-up-repassword-input'))).toBeTruthy();
         expect(await protoBrowser.waitForElement(By.id('sign-up-btn'))).toBeTruthy();
         expect(await protoBrowser.waitForElement(By.id('sign-in-link'))).toBeTruthy();
-    }, 30000)
+    }, 10000)
 
     it("should be able to register and retrieve a session using sign up interface", async () => {
         await protoBrowser.navigateToRegister()
         await protoBrowser.waitForElement(By.id('sign-up-email-input'));
         await protoBrowser.signUpFlow(`randomuser-${uuidv4()}@noreply.com`, 'changeme4321');
         expect(await protoBrowser.getUrlPath()).toBe('/')
-    }, 30000)
+    }, 10000)
 })
 
 describe("Test entities autocreation", () => {
