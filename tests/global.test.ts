62ca921bddf73c7d7ce36e46329e17cf0ec35aec
noreplydev <contact.cristiansanchez@gmail.com>
2024-01-23 10:27:04 +0100

    [FT] Added global tests folder to ensure project setup integrity

---
+++ tests/global.test.ts
@@ -0,0 +1,26 @@
+const path = require('path')
+const fs = require('fs')
+
+const INVALID_DEPENDENCIES = ["bindings", "deasync", "node-etcd"]
+
+describe("Protofy setup tests", () => {
+    it("Should have a yarn.lock file and avoid usage of OS dependent packages", () => {
+        const validDependencies = () => {
+            const yarnLockContent = fs.readFileSync(path.join(__dirname, '../yarn.lock'), 'utf-8');
+            if (!yarnLockContent) return false
+            const dependencies = yarnLockContent.split('\n\n')
+
+            for (let i = 0; i < dependencies.length; i++) {
+                if (dependencies[i].startsWith('#') || dependencies[i].startsWith('__metadata')) continue
+
+                const dependencyName = dependencies[i].split(':')[0] || "";
+                const matches = INVALID_DEPENDENCIES.filter((inv) => dependencyName.includes(inv))
+                if (matches.length > 0) return false
+            }
+
+            return true
+        }
+
+        expect(validDependencies()).toBe(true);
+    })
+})
