115cfba873f13ee664c55bbb5ebfb106263b8fc6
Noemí Blázquez <ap0calipse@gmail.com>
2024-01-25 23:29:58 +0100

    Changes log message to simplify ouput

--- packages/protolib/api/lib/mqtt.ts
+++ packages/protolib/api/lib/mqtt.ts
@@ -24,7 +24,7 @@ export const getMQTTClient = () => {
         });
 
         mqttClient.on('error', function (error) {
-            logger.error({ error }, `MQTT Error: ${error.message}`);
+            logger.error({ error }, "MQTT Error");
         });
     }
     return mqttClient
