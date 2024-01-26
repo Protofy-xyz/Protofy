7af71227c0c4dab3d648b98c39cae837d654d730
jcarlosn <jcarlos.norte@gmail.com>
2024-01-26 01:21:56 +0100

    Refactors api and admin-api. Removes modules in favor of bundles and adds initialization functions to improve debugging

--- apps/api/src/api.ts
+++ apps/api/src/api.ts
@@ -3,84 +3,64 @@ import * as path from 'path';
 import * as fs from 'fs';
 import BundleAPI from 'app/bundles/apis'
 import httpLogger from "pino-http";
-
 import {getLogger } from 'protolib/base';
-
-const modulesDir = path.join(__dirname, 'modules');
 const logger = getLogger()
 
-const mqtt = getMQTTClient()
+export default async () => {
+    const mqtt = getMQTTClient()
 
-const topicSub = (topic, cb) => {
-    mqtt.subscribe(topic)
-    mqtt.on("message", (topic, message) => {
-        const parsedMessage = message.toString();
-        cb(parsedMessage, topic)
-    });
-};
-
-const topicPub = (topic, data) => {
-    mqtt.publish(topic, data)
-}
-
-const deviceSub = (deviceName, component, componentName, cb) => {
-    return topicSub(deviceName + '/' + component + '/' + componentName + '/state', cb)
-}
-
-const devicePub = async (deviceName, component, componentName, command) => {
-    if (typeof command == "string") {
-        topicPub(deviceName + '/' + component + '/' + componentName + '/command', command)
-    } else {
-        topicPub(deviceName + '/' + component + '/' + componentName + '/command', JSON.stringify(command))
+    const topicSub = (topic, cb) => {
+        mqtt.subscribe(topic)
+        mqtt.on("message", (topic, message) => {
+            const parsedMessage = message.toString();
+            cb(parsedMessage, topic)
+        });
+    };
+    
+    const topicPub = (topic, data) => {
+        mqtt.publish(topic, data)
     }
-}
-
-fs.readdir(modulesDir, (error, files) => {
-
-    if (error) {
-        logger.error({ error }, "Error reading modules directory")
-        return
+    
+    const deviceSub = (deviceName, component, componentName, cb) => {
+        return topicSub(deviceName + '/' + component + '/' + componentName + '/state', cb)
     }
-
-    files.forEach((file) => {
-        if (path.extname(file) === '.ts') {
-            require(path.join(modulesDir, file));
-            logger.debug(`API Module loaded: ${file.substr(0, file.length - 3)}`)
+    
+    const devicePub = async (deviceName, component, componentName, command) => {
+        if (typeof command == "string") {
+            topicPub(deviceName + '/' + component + '/' + componentName + '/command', command)
+        } else {
+            topicPub(deviceName + '/' + component + '/' + componentName + '/command', JSON.stringify(command))
         }
-    })
-})
-
-
-export const logRequest = httpLogger({
-    serializers: {
-        req: (req) => {
-          if (process.env.NODE_ENV === "development") {
-            return {
-              method: req.method,
-              url: req.url,
-            };
-          } else {
-            return req;
-          }
-        },
-        res: (res) => {
-            if (process.env.NODE_ENV === "development") {
-              return {
-                code: res.statusCode,
-              };
-            } else {
-              return res;
-            }
-        },
-    },
-    level: "info",
-    transport: {
-        target: 'pino-pretty'
     }
-});
-
-app.use(logRequest);
-
-BundleAPI(app, { mqtt, devicePub, deviceSub })
-
-export default app
+    
+    app.use(httpLogger({
+        serializers: {
+            req: (req) => {
+              if (process.env.NODE_ENV === "development") {
+                return {
+                  method: req.method,
+                  url: req.url,
+                };
+              } else {
+                return req;
+              }
+            },
+            res: (res) => {
+                if (process.env.NODE_ENV === "development") {
+                  return {
+                    code: res.statusCode,
+                  };
+                } else {
+                  return res;
+                }
+            },
+        },
+        level: "info",
+        transport: {
+            target: 'pino-pretty'
+        }
+    }));
+    
+    BundleAPI(app, { mqtt, devicePub, deviceSub })
+    return app
+}
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,32 +1,35 @@
 import dotenv from 'dotenv'
-import {setLoggerConfig, getLogger } from 'protolib/base/logger';
-setLoggerConfig({name: "api"})
+import { setLoggerConfig, getLogger } from 'protolib/base/logger';
+setLoggerConfig({ name: "api" })
 require('events').EventEmitter.defaultMaxListeners = 100;
-
 const logger = getLogger()
 // get config vars
 dotenv.config({ path: '../../.env' });
-
 import http from 'http';
 import app from './api'
-import {generateEvent} from 'app/bundles/library'
+import { generateEvent } from 'app/bundles/library'
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-const server = http.createServer(app);
-const PORT = isProduction?4001:3001
+const start = async () => {
+  const server = http.createServer(app);
+  const PORT = isProduction ? 4001 : 3001
 
-server.listen(PORT, () => {
-  logger.info({service:{protocol: "http", port: PORT}}, "Service started: HTTP")
-});
+  server.listen(PORT, () => {
+    logger.info({ service: { protocol: "http", port: PORT } }, "Service started: HTTP")
+  });
 
-// generateEvent({
-//   path: 'services/start/api', //event type: / separated event category: files/create/file, files/create/dir, devices/device/online
-//   from: 'api', // system entity where the event was generated (next, api, cmd...)
-//   user: 'system', // the original user that generates the action, 'system' if the event originated in the system itself
-//   payload: {}, // event payload, event-specific data
-// })
+  // generateEvent({
+  //   path: 'services/start/api', //event type: / separated event category: files/create/file, files/create/dir, devices/device/online
+  //   from: 'api', // system entity where the event was generated (next, api, cmd...)
+  //   user: 'system', // the original user that generates the action, 'system' if the event originated in the system itself
+  //   payload: {}, // event payload, event-specific data
+  // })
+}
+
+start()
 
 
 
-  
+
+
