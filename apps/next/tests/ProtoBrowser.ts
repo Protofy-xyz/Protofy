dca08a79d521aaebfeb4b80f6a0c114cdbf1b0d1
MiquelCos <miquel@protofy.xyz>
2024-02-02 09:00:54 +0100

    Increase timeout on navigate workspace method

--- apps/next/tests/ProtoBrowser.ts
+++ apps/next/tests/ProtoBrowser.ts
@@ -117,7 +117,7 @@ export class ProtoBrowser {
     async navigateToWorkspace() {
         await this.waitForElement('#header-session-user-id');
         await this.clickElement('#layout-menu-btn')
-        await this.clickElement('#pop-over-workspace-link', 60000)
+        await this.clickElement('#pop-over-workspace-link', 80000)
         await this.getPage().waitForURL('**/admin/*', { timeout: 60000 });
     }
 
