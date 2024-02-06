3e2808fdfe772a1928856f9a0512d09eff0356c1
jcarlosn <jcarlos.norte@gmail.com>
2024-02-06 02:34:49 +0100

    Makes scroll transparent

--- packages/protolib/components/AdminPanel.tsx
+++ packages/protolib/components/AdminPanel.tsx
@@ -135,7 +135,7 @@ export const AdminPanel = ({ children }) => {
 
 
   // console.log('userSpaces: ', userSpaces, 'current Workspace: ', currentWorkspace)
-  return rightPanelSize && <MainPanel borderLess={true} rightPanelSize={rightPanelSize} setRightPanelSize={setRightPanelSize} rightPanelStyle={{ marginRight: '20px', height: 'calc(100vh - 85px)', marginTop: '68px' }} rightPanelVisible={appState.logsPanelOpened} rightPanelResizable={true} centerPanelContent={Workspaces[currentWorkspace]
+  return rightPanelSize && <MainPanel borderLess={true} rightPanelSize={rightPanelSize} setRightPanelSize={setRightPanelSize} rightPanelStyle={{ marginRight: '20px', height: 'calc(100vh - 85px)', marginTop: '68px', backgroundColor: 'transparent' }} rightPanelVisible={appState.logsPanelOpened} rightPanelResizable={true} centerPanelContent={Workspaces[currentWorkspace]
     ? <PanelLayout
       topBar={
         <>
