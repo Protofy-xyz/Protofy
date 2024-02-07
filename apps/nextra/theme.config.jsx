64b983a8b8861786cd40c62fb3fd596eff3e7a07
Noemí Blázquez <ap0calipse@gmail.com>
2024-02-07 01:18:25 +0100

    Customize nextra header bar

--- apps/nextra/theme.config.jsx
+++ apps/nextra/theme.config.jsx
@@ -1,9 +1,18 @@
+
+import Image from 'next/image'
+
+
 export default {
-    logo: <span>Protofy documentation</span>,
-    project: {
-      link: 'https://github.com/Protofy-xyz/Protofy'
-    },
-    docsRepositoryBase: 'https://github.com/Protofy-xyz/Protofy/tree/main/apps/nextra',
-    footer: false
-    // ... other theme options
-  }
+  logo: <div style={{ display: 'flex', alignItems: 'center' }}>
+    <span style={{ marginRight: '10px' }}>
+      <img src="/logo.png" alt="Protofy" width="40" height="40" />
+    </span>
+    <span style={{ fontWeight: 'bold', fontSize: '18px'}}>Protofy</span>
+  </div>,
+  project: {
+    link: 'https://github.com/Protofy-xyz/Protofy'
+  },
+  docsRepositoryBase: 'https://github.com/Protofy-xyz/Protofy/tree/main/apps/nextra',
+  footer: false
+  // ... other theme options
+}
