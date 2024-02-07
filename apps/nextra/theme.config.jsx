51e10b83731051c5fe65dde80437769f66841869
Noemí Blázquez <ap0calipse@gmail.com>
2024-02-07 01:33:06 +0100

    change protofy title nextra opacity

--- apps/nextra/theme.config.jsx
+++ apps/nextra/theme.config.jsx
@@ -7,7 +7,7 @@ export default {
     <span style={{ marginRight: '10px' }}>
       <img src="/logo.png" alt="Protofy" width="40" height="40" />
     </span>
-    <span style={{ fontWeight: 'bold', fontSize: '18px'}}>Protofy</span>
+    <span style={{ fontWeight: 'bold', fontSize: '18px', opacity:0.8}}>Protofy</span>
   </div>,
   project: {
     link: 'https://github.com/Protofy-xyz/Protofy'
