c98adfce2e966141a95c100bd80f58d64e2e20c7
Noemí Blázquez <ap0calipse@gmail.com>
2024-02-09 14:24:28 +0100

    Genetare token with permissions

--- packages/protolib/adminapi/auth.ts
+++ packages/protolib/adminapi/auth.ts
@@ -33,13 +33,23 @@ app.post('/adminapi/v1/auth/login', handler(async (req: any, res: any) => {
             //update lastLogin
             await getDB(dbPath).put(storedUser.username, JSON.stringify({ ...storedUser, lastLogin: moment().toISOString() }))
             const group = JSON.parse(await getDB(groupDBPath).get(storedUser.type))
-            res.send({ session: genNewSession({ id: storedUser.username, type: storedUser.type, admin: group.admin ? true : false }), context: await getSessionContext(storedUser.type) })
+            const newSession = {
+                id: storedUser.username,
+                type: storedUser.type,
+                admin: group.admin ? true : false,
+                permissions: [...(group.admin ? ["*"] : []), ...(group.permissions ?? []), ...(storedUser.permissions ?? [])]
+            }
+            res.send({
+                session: genNewSession(newSession),
+                context: await getSessionContext(storedUser.type)
+            })
             generateEvent({
                 path: 'auth/login/success', //event type: / separated event category: files/create/file, files/create/dir, devices/device/online
                 from: 'api', // system entity where the event was generated (next, api, cmd...)
                 user: request.username, // the original user that generates the action, 'system' if the event originated in the system itself
                 payload: { clientIp: req.get('X-Client-IP') || req.headers['x-client-ip'] } // event payload, event-specific data
             }, getServiceToken())
+            logger.info({ newSession }, "Session created: " + request.username)
             return
         }
     } catch (error) {
@@ -64,16 +74,17 @@ app.post('/adminapi/v1/auth/register', handler(async (req: any, res: any) => {
     if (await existsKey(dbPath, request.username)) {
         res.status(500).send({ fieldErrors: { 'username': 'A user with the same email already exists' } });
     } else {
-        const { rePassword, ...newUserData } = request
-        await getDB(dbPath).put(request.username, JSON.stringify({
+        const { rePassword, password, ...newUserData } = request
+        const newUser = {
             ...newUserData,
-            password: await hash(newUserData.password),
             createdAt: moment().toISOString(),
             from: 'api',
             type: 'user'
-        }))
+        }
+        await getDB(dbPath).put(request.username, JSON.stringify({...newUser, password: await hash(password)}))
         let group = {
-            admin: false
+            admin: false,
+            permissions: []
         };
         try {
             group = JSON.parse(await getDB(groupDBPath).get('user'))
@@ -87,6 +98,19 @@ app.post('/adminapi/v1/auth/register', handler(async (req: any, res: any) => {
             user: request.username, // the original user that generates the action, 'system' if the event originated in the system itself
             payload: {} // event payload, event-specific data
         }, getServiceToken())
-        res.send({ session: genNewSession({ id: request.username, type: "user", admin: group.admin ? true : false }), context: await getSessionContext('user') })
+        logger.info({ newUserData }, "User created: " + newUserData.username)
+
+
+        const newSession = {
+            id: request.username,
+            type: "user",
+            admin: group.admin ? true : false,
+            permissions: [...(group.admin ? ["*"] : []), ...(group.permissions ?? [])]
+        }
+        res.send({
+            session: genNewSession(newSession),
+            context: await getSessionContext('user')
+        })
+        logger.info({ newSession }, "Session created: " + request.username)
     }
 }));
