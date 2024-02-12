d1cc2228b2f862fd3807b2817649683d392dfda5
jcarlosn <jcarlos.norte@gmail.com>
2024-02-12 23:44:01 +0100

    Removes outdated code

--- packages/app/BaseConfig.ts
+++ packages/app/BaseConfig.ts
@@ -43,24 +43,6 @@ export const getBaseConfig = (name, process, token?, config?) => {
     mqtt: {
       auth: process.env.ENABLE_MQTT_AUTH === "true" ? true : false
     },
-    // logger: {
-    //   ...(process && process.env.NODE_ENV === 'development' && typeof window === "undefined" ?
-    //     {
-    //       transport: {
-    //         targets: [
-    //           {
-    //             target: 'pino-pretty',
-    //             level: 'info',
-    //             options: {
-    //               colorize: true
-    //             }
-    //           }
-    //         ]
-    //       }
-    //     } : {}),
-    //   name: name ?? 'default',
-    //   level: 'debug'
-    // }
     logger: {
       transport: {
         targets: [
