b053f761ceab7a8a86952b914508dfa2d79e51b9
noreplydev <contact.cristiansanchez@gmail.com>
2024-02-19 19:18:41 +0000

    [FT] Updated visualUiHooks

--- packages/visualui/src/visualUiHooks.ts
+++ packages/visualui/src/visualUiHooks.ts
@@ -1,9 +1,34 @@
-import { useEditorContext } from "@protocraft/core";
+import { useState, useEffect } from 'react'
+import { useAtom } from 'jotai'
+import { EditorStore, useEditorContext } from "@protocraft/core";
 
 export function newVisualUiContext(options: any) {
   return useEditorContext(options)
 }
 
-export function useVisualUi(atom) {
+export function useVisualUi(atom, callb, defState) {
+  const [state, setState] = useState(defState)
+  const [craftContext] = useAtom<EditorStore>(atom)
 
+  useEffect(() => {
+    if (!craftContext || !craftContext['subscribe']) return
+
+    craftContext.subscribe(
+      (_) => {
+        setState((prev: any) => {
+          const result = callb(prev, craftContext)
+          return result ?? prev
+        })
+      },
+      () => {
+        // we need to discover more about this callback
+      }
+    );
+  }, [craftContext])
+
+  return {
+    state: state,
+    visualUiData: craftContext,
+    set: 'baby'
+  }
 }
