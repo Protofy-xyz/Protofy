9734ab1450b18228aa710f0b45ddcd636210212f
MiquelCos <miquel@protofy.xyz>
2024-02-20 19:01:18 +0100

    Updates create mask button

--- packages/protoflow/src/CreateMaskButton.tsx
+++ packages/protoflow/src/CreateMaskButton.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useMemo } from 'react';
-import { Button, YStack, Dialog, Select, Input, XStack, Text } from '@my/ui'
+import { Button, YStack, Dialog, Select, XStack, Input } from '@my/ui'
 import useTheme from './diagram/Theme';
 import { Drama, X, Check, ChevronDown } from 'lucide-react';
 import { getAllTypes } from './dynamicMasks/ProtolibProps';
@@ -16,9 +16,16 @@ type BodyDataProps = {
 };
 
 export default ({ nodeData, maskType }: Props) => {
+    const initialData = Object.keys(nodeData).filter(k => k.startsWith('prop')).map(k => {
+        return {
+            label: nodeData[k].key,
+            field: nodeData[k].key,
+            fieldType: 'prop'
+        }
+    })
+    const [maskBodyData, setMaskBodyData] = useState<BodyDataProps[]>(initialData)
     const [newType, setNewType] = useState<string | undefined>(undefined)
     const [newField, setNewField] = useState<string>('')
-    const [maskBodyData, setMaskBodyData] = useState<BodyDataProps[]>([])
     const items = [undefined, ...getAllTypes()]
 
     const clearNewData = () => {
@@ -26,6 +33,7 @@ export default ({ nodeData, maskType }: Props) => {
         setNewField('')
     }
     const onAddType = () => {
+        if (!newField) return
         var newData: BodyDataProps = {
             label: newField,
             field: newField,
@@ -38,6 +46,21 @@ export default ({ nodeData, maskType }: Props) => {
         maskBodyData.push(newData)
         setMaskBodyData([...maskBodyData])
         clearNewData()
+    }
+    const onChangeType = (index, key, value) => {
+        var newBodyData = [...maskBodyData]
+
+        if (key == 'field') {
+            newBodyData[index]['label'] = value
+        }
+
+        if (key == 'type' && value == "default") {
+            delete newBodyData[index][key]
+        } else {
+            newBodyData[index][key] = value
+        }
+
+        setMaskBodyData(newBodyData)
     }
     const onDelete = (deleteField) => {
         const newMaskBody = maskBodyData.filter(b => b.field != deleteField)
@@ -64,11 +87,11 @@ export default ({ nodeData, maskType }: Props) => {
             .then(response => response.json())
             .then(data => console.log(data))
     }
+
     return <Dialog modal onOpenChange={onOpenChange}>
         <Dialog.Trigger asChild>
             <YStack justifyContent='center'>
                 <Button chromeless alignSelf="center" theme={"blue"} mb="$3">
-                    {/* create mask */}
                     <Drama fillOpacity={0} color={useTheme('interactiveColor')} />
                 </Button>
             </YStack>
@@ -91,65 +114,87 @@ export default ({ nodeData, maskType }: Props) => {
                 <Dialog.Description>
                     {'Properties: '}
                 </Dialog.Description>
-                <YStack overflow="scroll" maxHeight="$18" overflowX="hidden">
-                    {maskBodyData.map((ele) => (
-                        <XStack
-                            key={ele.field} justifyContent='space-between'
-                            alignItems='center' borderBottomColor="$gray6"
-                            borderBottomWidth="$0.25" marginBottom="$2" paddingBottom="$2"
-                        >
-                            <XStack flex={1} >
-                                <Text marginRight="$4">
-                                    {ele.field}
-                                </Text>
-                                <Text opacity={0.6}>
-                                    {ele.type ?? 'default'}
-                                </Text>
-                            </XStack>
-                            <Button size="$3" onPress={() => onDelete(ele.field)} theme='red' icon={X}></Button>
-                        </XStack>))}
+                <YStack>
+                    <YStack overflow="scroll" maxHeight="$18" overflowX="hidden" >
+                        {maskBodyData.map((ele, index) => (
+                            <PropEditor
+                                onChange={onChangeType}
+                                index={index}
+                                item={ele}
+                                selectItems={items}
+                                onDelete={onDelete}
+                            />
+                        ))}
+                    </YStack>
+                    <XStack gap="$3" alignItems='center'>
+                        <Input
+                            value={newField}
+                            onChangeText={setNewField}
+                            placeholder='field name...'
+                        />
+                        <TypeSelect value={newType} onValueChange={setNewType} items={items} />
+                        <Button size="$4" onPress={onAddType} theme='blue'>Add</Button>
+                    </XStack>
                 </YStack>
-                <XStack gap="$3" alignItems='center'>
-                    <Input value={newField} onChangeText={setNewField} placeholder='field name...' />
-                    <Select value={newType} onValueChange={setNewType} disablePreventBodyScroll>
-                        <Select.Trigger width={220} iconAfter={ChevronDown}>
-                            <Select.Value placeholder="default" />
-                        </Select.Trigger>
-                        <Select.Content zIndex={9999999999}>
-                            <Select.Viewport>
-                                <Select.Group>
-                                    <Select.Label>Types</Select.Label>
-                                    {useMemo(
-                                        () =>
-                                            items.map((item, i) => {
-                                                return (
-                                                    <Select.Item
-                                                        index={i}
-                                                        key={item}
-                                                        value={item?.toLowerCase() ?? 'default'}
-                                                    >
-                                                        <Select.ItemText>{item ?? 'default'}</Select.ItemText>
-                                                        <Select.ItemIndicator marginLeft="auto">
-                                                            <Check size={16} />
-                                                        </Select.ItemIndicator>
-                                                    </Select.Item>
-                                                )
-                                            }),
-                                        [items]
-                                    )}
-                                </Select.Group>
-                            </Select.Viewport>
-                        </Select.Content>
-                    </Select>
-                    <Button size="$3" onPress={onAddType} theme='blue' icon={Check}></Button>
-                </XStack>
                 <Dialog.Close displayWhenAdapted asChild>
                     <Button theme='blue' onPress={generateMask}>
                         Create
                     </Button>
                 </Dialog.Close>
-
             </Dialog.Content>
         </Dialog.Portal>
-    </Dialog>
+    </Dialog >
 }
+
+const PropEditor = ({ item, selectItems, onChange, index, onDelete }) => {
+    const [field, setField] = useState(item.field)
+    
+    return <XStack
+        key={item.field}
+        gap="$3"
+        marginBottom="$3"
+    >
+        <Input
+            value={field}
+            onBlur={() => onChange(index, 'field', field)}
+            onChangeText={setField}
+            placeholder='field name...'
+        />
+        <TypeSelect value={item?.type} onValueChange={(val) => onChange(index, 'type', val)} items={selectItems} />
+        <Button size="$4" onPress={() => onDelete(item.field)} icon={X}></Button>
+    </XStack>
+}
+
+const TypeSelect = ({ value, onValueChange, items }) => <Select value={value??'default'} onValueChange={onValueChange} disablePreventBodyScroll>
+    <Select.Trigger width={220} iconAfter={ChevronDown}>
+        <Select.Value placeholder="default" />
+    </Select.Trigger>
+    <Select.Content zIndex={9999999999}>
+        <Select.Viewport>
+            <Select.Group>
+                <Select.Label>Types</Select.Label>
+                {useMemo(
+                    () =>
+                        items.map((item, i) => {
+                            const itemName = item ?? 'default'
+                            return (
+                                <Select.Item
+                                    index={i}
+                                    key={item}
+                                    value={item?.toLowerCase() ?? 'default'}
+                                >
+                                    <Select.ItemText>
+                                        {itemName}
+                                    </Select.ItemText>
+                                    <Select.ItemIndicator marginLeft="auto">
+                                        <Check size={16} />
+                                    </Select.ItemIndicator>
+                                </Select.Item>
+                            )
+                        }),
+                    [items]
+                )}
+            </Select.Group>
+        </Select.Viewport>
+    </Select.Content>
+</Select>
