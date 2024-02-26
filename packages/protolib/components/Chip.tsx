802fe67b899bdc74e2ff2962d4c5c3aa83ae2b5a
jcarlosn <jcarlos.norte@gmail.com>
2024-02-26 01:46:53 +0100

    Fix chips looking bold on compiled admin panel

--- packages/protolib/components/Chip.tsx
+++ packages/protolib/components/Chip.tsx
@@ -6,7 +6,7 @@ export const Chip = ({ loading, icon, text, bold, color, ...props}:{loading?:boo
     return <XStack ai="center" jc="center" bc={color??'$color5'} p={2} px={"$3"} br={25} {...props}>
             
             {icon && React.cloneElement(icon, { size:20, strokeWidth: 0.7, color: 'var(--color)' })}
-            {text && <SizableText ml={icon?"$2":"$0"} o={0.97} fontWeight={bold?"900":"900"} size="$1" color="$color" >{text}</SizableText>}
+            {text && <SizableText ml={icon?"$2":"$0"} o={0.97} fontWeight={bold?"900":"400"} size="$1" color="$color" >{text}</SizableText>}
             {loading && <><Spacer size={9} /><Spinner color={"var(--color8)"} scale={0.7} size='small' /></>}
         </XStack>
 }
