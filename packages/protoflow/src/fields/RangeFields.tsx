561386a5f711a16177a3e337ef3f60b523b4a98b
MiquelCos <miquel@protofy.xyz>
2024-02-28 12:37:15 +0100

    Adds new range field types

--- packages/protoflow/src/fields/RangeFields.tsx
+++ packages/protoflow/src/fields/RangeFields.tsx
@@ -3,41 +3,71 @@ import useTheme from '../diagram/Theme';
 import { FlowStoreContext } from '../store/FlowsStore';
 import { CustomField } from '.';
 
-export const getRangeTypes = () => ['range-theme']
+export const getRangeTypes = () => ['range-theme', 'range-px', 'range']
 
 export default ({ nodeData = {}, item, node }) => {
     const useFlowsStore = useContext(FlowStoreContext)
     const setNodeData = useFlowsStore(state => state.setNodeData)
 
-    const { field, label, type, fieldType } = item
+    const defData = {
+        "range-theme": { // e.g. "$10"
+            pre: s => s[0] == "$" ? s.split("$")[1] : s, 
+            post: s => "$" + s,
+            max: 20,
+            min: 0,
+            kind: 'StringLiteral'
+        },
+        "range-px": { // e.g. "10px"
+            pre: s => (s[s.length - 2] == "p" && s[s.length - 1] == "x") ? s.slice(0, -2) : s,
+            post: s => s + "px",
+            max: 100,
+            min: 0,
+            kind: 'StringLiteral'
+        },
+        "default": {
+            pre: s => s,
+            post: s => s,
+            max: 100,
+            min: 0,
+            step: 1,
+            kind: 'NumericLiteral'
+        }
+    }
+
+    const { field, label, type, fieldType, data } = item
 
     const fieldKey = field.replace(fieldType + '-', '')
-    const data = nodeData[field]
-    const value = data?.value
+    const itemData = nodeData[field]
+    const value = itemData?.value
+
+    const rangeData = { ...defData[type], ...data }
+    const pre = rangeData?.pre ?? defData['default']?.pre
+    const post = rangeData?.post ?? defData['default']?.post
+    const max = rangeData?.max ?? defData['default']?.max
+    const min = rangeData?.min ?? defData['default']?.min
+    const step = rangeData?.step ?? defData['default']?.step
+    const kind = rangeData?.kind ?? defData['default']?.kind
 
     const onValueChange = (val) => {
         // current case is fieldType == "prop"
-        setNodeData(node.id, { ...nodeData, [field]: { ...data, key: fieldKey, value: val, kind: 'StringLiteral' } })
+        setNodeData(node.id, { ...nodeData, [field]: { ...itemData, key: fieldKey, value: val, kind: kind } })
     }
 
     const getInput = () => {
-        switch (type) {
-            case 'range-theme':
-                const pre = (s) => s[0] == "$" ? s.split("$")[1] : s
-                const post = (s) => "$" + s
-                const initialRangeValue = value ?? 0
-                const [tmpRangeValue, setTmpRangeValue] = React.useState(pre(initialRangeValue));
-                const max = 20
+        const initialRangeValue = value ?? 0
+        const [tmpRangeValue, setTmpRangeValue] = React.useState(pre(initialRangeValue));
 
-                return <>
-                    <div style={{ fontSize: '14px', position: 'relative', top: '3px', width: max.toString().length * 18, textAlign: 'left', color: useTheme('textColor') }}>{post(tmpRangeValue)}</div>
-                    <input type="range" style={{ width: '100%', marginTop: '6px', accentColor: useTheme('interactiveColor'), height: '5px', borderWidth: '4px solid blue', backgroundColor: useTheme("inputBackgroundColor"), borderRadius: '10px' }}
-                        step={1}
-                        onChange={(event: any) => setTmpRangeValue(event.target.value)}
-                        onMouseUp={() => onValueChange(post(tmpRangeValue))}
-                        value={tmpRangeValue} min={0} max={20} />
-                </>
-        }
+        return <>
+            <div style={{ fontSize: '14px', position: 'relative', top: '3px', width: max.toString().length * 18, textAlign: 'left', color: useTheme('textColor') }}>{post(tmpRangeValue)}</div>
+            <input type="range" style={{ width: '100%', marginTop: '6px', accentColor: useTheme('interactiveColor'), height: '5px', borderWidth: '4px solid blue', backgroundColor: useTheme("inputBackgroundColor"), borderRadius: '10px' }}
+                step={step}
+                onChange={(event: any) => setTmpRangeValue(event.target.value)}
+                onMouseUp={() => onValueChange(post(tmpRangeValue))}
+                value={tmpRangeValue}
+                min={min}
+                max={max}
+            />
+        </>
     }
 
     return <CustomField label={label} input={getInput()} />
