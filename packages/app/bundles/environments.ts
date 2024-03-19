e3a7f376427de839bc211e70e7c9fc0da2a3facd
Gerard <gerard@protofy.xyz>
2024-03-19 12:45:28 +0100

    [add] docker envs at environments when running from docker

--- packages/app/bundles/environments.ts
+++ packages/app/bundles/environments.ts
@@ -1,18 +1,20 @@
 export const environments = {
     dev: {
-        api: 'http://localhost:3001',
-        adminApi: 'http://localhost:3002',
-        frontend: 'http://localhost:3000',
+        api: process.env.API_URL ?? 'http://localhost:3001',
+        adminApi: process.env.ADMIN_API_URL ?? 'http://localhost:3002',
+        frontend: process.env.SITE_URL ?? 'http://localhost:3000',
+        // TODO add missing docker url from docker envs
         websocket: 'http://localhost:3002',
         admin: 'http://localhost:3003',
         docs: 'http://localhost:7600'
     },
     prod: {
-        api: 'http://localhost:4001',
-        adminApi: 'http://localhost:4002',
-        frontend: 'http://localhost:4000',
-        websocket: 'http://localhost:4002',
-        admin: 'http://localhost:4003',
-        docs: 'http://localhost:7700'
+        api: process.env.API_URL ?? 'http://localhost:4001/',
+        adminApi: process.env.ADMIN_API_URL ?? 'http://localhost:4002/',
+        frontend: process.env.SITE_URL ?? 'http://localhost:4000/',
+        // TODO add missing docker url from docker from docker envs
+        websocket: 'http://localhost:4002/',
+        admin: 'http://localhost:4003/',
+        docs: 'http://localhost:7700/'
     }
 }
