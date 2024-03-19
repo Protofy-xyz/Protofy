0e03f867164927e579c96094364a9e0873239621
Noemí Blázquez <ap0calipse@gmail.com>
2024-03-19 01:14:10 +0100

    Fixes navigation and logout flows in /admin/.
    Co-authored-by: jcarlosn <jcarlos.norte@gmail.com>

--- packages/protolib/components/NextLink.tsx
+++ packages/protolib/components/NextLink.tsx
@@ -1,8 +1,8 @@
 import NextInternalLink from 'next/link'
 import * as React from 'react'
 
-export const NextLink = React.forwardRef((props:any, ref:any) => {
+export const NextLink = React.forwardRef(({native, ...props}:any, ref:any) => {
   return (
-    <NextInternalLink ref={ref} {...props} className={`next-link ` + (props.className || '')} />
+    native ? <a ref={ref} {...props} className={`next-link ` + (props.className || '')}></a>:<NextInternalLink ref={ref} {...props} className={`next-link ` + (props.className || '')} />
   )
 }) as typeof NextInternalLink
