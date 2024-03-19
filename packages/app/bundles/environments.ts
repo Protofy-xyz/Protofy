65837362698e1435a5c19bee2b7f02d431fc76f2
noreplydev <contact.cristiansanchez@gmail.com>
2024-03-19 18:39:07 +0000

    minor changes

--- packages/app/bundles/environments.ts
+++ packages/app/bundles/environments.ts
@@ -3,18 +3,18 @@ export const environments = {
         api: process.env.API_URL ?? 'http://localhost:3001',
         adminApi: process.env.ADMIN_API_URL ?? 'http://localhost:3002',
         frontend: process.env.SITE_URL ?? 'http://localhost:3000',
+        admin: process.env.ADMIN_SITE_URL ?? 'http://localhost:3003',
         // TODO add missing docker url from docker envs
         websocket: 'http://localhost:3002',
-        admin: process.env.ADMIN_SITE_URL ?? 'http://localhost:3003',
         docs: 'http://localhost:7600'
     },
     prod: {
         api: process.env.API_URL ?? 'http://localhost:4001/',
         adminApi: process.env.ADMIN_API_URL ?? 'http://localhost:4002/',
         frontend: process.env.SITE_URL ?? 'http://localhost:4000/',
+        admin: process.env.ADMIN_SITE_URL ?? 'http://localhost:4003/',
         // TODO add missing docker url from docker from docker envs
         websocket: 'http://localhost:4002/',
-        admin: process.env.ADMIN_SITE_URL ?? 'http://localhost:4003/',
         docs: 'http://localhost:7700/'
     }
 }
