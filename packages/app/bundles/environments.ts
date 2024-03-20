0e76599d48c2e510e5f6b82e6f5c74f45af262c7
gdomenech98 <gerard@protofy.xyz>
2024-03-20 08:49:42 +0000

    [ADD] environments for documentation

--- packages/app/bundles/environments.ts
+++ packages/app/bundles/environments.ts
@@ -4,17 +4,17 @@ export const environments = {
         adminApi: process.env.ADMIN_API_URL ?? 'http://localhost:3002',
         frontend: process.env.SITE_URL ?? 'http://localhost:3000',
         admin: process.env.ADMIN_SITE_URL ?? 'http://localhost:3003',
+        docs: process.env.DOCS_SITE_URL ?? 'http://localhost:7600',
         // TODO add missing docker url from docker envs
-        websocket: 'http://localhost:3002',
-        docs: 'http://localhost:7600'
+        websocket: 'http://localhost:3002'
     },
     prod: {
         api: process.env.API_URL ?? 'http://localhost:4001/',
         adminApi: process.env.ADMIN_API_URL ?? 'http://localhost:4002/',
         frontend: process.env.SITE_URL ?? 'http://localhost:4000/',
         admin: process.env.ADMIN_SITE_URL ?? 'http://localhost:4003/',
+        docs: process.env.DOCS_SITE_URL ?? 'http://localhost:7700/',
         // TODO add missing docker url from docker from docker envs
-        websocket: 'http://localhost:4002/',
-        docs: 'http://localhost:7700/'
+        websocket: 'http://localhost:4002/'
     }
 }
