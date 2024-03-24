9ce5113afa0a55fa673cd39c7c74a0ab6767738d
jcarlosn <jcarlos.norte@gmail.com>
2024-03-24 03:40:43 +0100

    Fix itemsPerPage when doing filtered lists

--- packages/protolib/base/ProtoModel.ts
+++ packages/protolib/base/ProtoModel.ts
@@ -106,7 +106,7 @@ export abstract class ProtoModel<T extends ProtoModel<T>> {
 
     list(search?, session?, extraData?, params?): any {
         if(params) {
-            const {page, orderBy, orderDirection, search, ...filters} = params
+            const {itemsPerPage, page, orderBy, orderDirection, search, ...filters} = params
 
             const allFiltersMatch = Object.keys(filters).every(key => {
                 return this.data && this.data[key] == filters[key];
@@ -114,7 +114,7 @@ export abstract class ProtoModel<T extends ProtoModel<T>> {
 
             if(!allFiltersMatch) return
         }
-        
+
         if (search) {
             const { parsed, searchWithoutTags } = parseSearch(search);
 
