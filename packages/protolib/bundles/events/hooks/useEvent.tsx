16ca1e866b0a88f288e701cfca3eca60b815ad56
Noemí Blázquez <ap0calipse@gmail.com>
2024-03-25 23:11:34 +0100

    Moved event filtering logic from useEventEffect to useEvent

--- packages/protolib/bundles/events/hooks/useEvent.tsx
+++ packages/protolib/bundles/events/hooks/useEvent.tsx
@@ -1,6 +1,22 @@
 import { useSubscription } from 'mqtt-react-hooks'
 
-export const useEvent = ()=> {
-    const { message } = useSubscription('notifications/event/create/#');
+export const useEvent = (eventFilter) => {
+    const { message } = useSubscription('notifications/event/create/#')
+
+    if (message && message.message && eventFilter) {
+        try {
+            let content = JSON.parse(message.message as string)
+            if (eventFilter.path && content['path'] != eventFilter.path) {
+                return
+            }
+            if (eventFilter.from && content['from'] != eventFilter.from) {
+                return
+            }
+            return message;
+        } catch (e) {
+            console.error('Error parsing message from mqtt: ', e)
+        }
+    }
+
     return message
 }
--- packages/protolib/bundles/events/hooks/useEventEffect.tsx
+++ packages/protolib/bundles/events/hooks/useEventEffect.tsx
@@ -2,21 +2,12 @@ import { useEffect } from "react"
 import { useEvent } from "./useEvent"
 
 export const useEventEffect = (onEvent, eventFilter?: { path?: string, from?: string }) => {
-    const event = useEvent()
+    const event = useEvent(eventFilter)
+
     useEffect(() => {
-        if (event && event.message) {
-            try {
-                let content = JSON.parse(event.message as string)
-                if (eventFilter.path && content['path'] != eventFilter.path) {
-                    return
-                }
-                if (eventFilter.from && content['from'] != eventFilter.from) {
-                    return
-                }
-                onEvent(event)
-            } catch (e) {
-                console.error('Error parsing message from mqtt: ', e)
-            }
+        if (event) {
+            onEvent(event)
         }
     }, [event])
+
 }
