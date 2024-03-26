034f5d9f75005a82cf42e986fdf36be8c2d321ad
Noemí Blázquez <ap0calipse@gmail.com>
2024-03-26 02:41:40 +0100

    Implements context.onEvent to listen for events in apis

--- packages/protolib/bundles/events/api/index.ts
+++ packages/protolib/bundles/events/api/index.ts
@@ -1,3 +1,18 @@
-export const onEvent = (eventFilter?: { path?: string, from?: string }) => {
-
+export const onEvent = (context, cb, eventFilter?: { path?: string, from?: string }) => {
+    context.topicSub('notifications/event/create/#', (async (msg: string, topic: string) => {
+        try {
+            const message = JSON.parse(msg)
+            if (message && eventFilter) {
+                if (eventFilter.path && message['path'] != eventFilter.path) {
+                    return
+                }
+                if (eventFilter.from && message['from'] != eventFilter.from) {
+                    return
+                }
+            }
+            cb(message)
+        } catch (e) {
+            console.error('Error parsing message from mqtt: ', e)
+        }
+    }))
 }
