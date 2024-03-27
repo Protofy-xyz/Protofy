d12db6f4e5be27e5923f7814ffeb0446f7c85250
jcarlosn <jcarlos.norte@gmail.com>
2024-03-27 03:21:37 +0100

    Reenable memory system but protected to be SSR compatible
    Co-authored-by: Noemí Blázquez <ap0calipse@gmail.com>

--- packages/protoflow/src/lib/memory.ts
+++ packages/protoflow/src/lib/memory.ts
@@ -1,16 +1,18 @@
-//window['flowMemory'] = {}
+const isClient = typeof window !== "undefined"
+if(isClient) window['flowMemory'] = {}
 
 export const read = (instanceId, key, defaultValue) => {
-/*   if(!window['flowMemory'][instanceId]) {
+  if(!isClient) return []
+  if(!window['flowMemory'][instanceId]) {
     return defaultValue
   }
-  return window['flowMemory'][instanceId][key] */
-  return []
+  return window['flowMemory'][instanceId][key]
 }
 
 export const write = (instanceId, key, value) => {
-/*   if(!window['flowMemory'][instanceId]) {
+  if(!isClient) return
+  if(!window['flowMemory'][instanceId]) {
     window['flowMemory'][instanceId] = {}
   }
-  window['flowMemory'][instanceId][key] = value */
+  window['flowMemory'][instanceId][key] = value
 }
