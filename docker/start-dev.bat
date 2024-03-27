5a192d75c799af53e072a5051b6be6c23d42911f
gdomenech98 <gerard@protofy.xyz>
2024-03-27 08:39:06 +0000

    [refactor] simplify start-dev scripts

--- docker/start-dev.bat
+++ docker/start-dev.bat
@@ -1,7 +1,5 @@
 @echo off
 setlocal enabledelayedexpansion
-CALL .\helpers\get-envs.bat || (echo Failed to run helpers\get-envs.bat & exit /b)
 CALL .\init.bat || (echo Failed to run init.bat & exit /b)
 CALL .\build.bat || (echo Failed to run build.bat & exit /b)
-
-%DOCKER_COMPOSE_CMD% -p protofy %SERVICES_COMPOSE_FILES% up
+CALL .\start-dev-fast.bat || (echo Failed to run start-dev-fast.bat & exit /b)
