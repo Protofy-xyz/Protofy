7a363607da7d0795faafb6e121164c19096c4b8b
Noemí Blázquez <ap0calipse@gmail.com>
2024-03-27 16:39:31 +0100

    Update NextLink component to correctly handle prefetch property

--- packages/protolib/components/NextLink.tsx
+++ packages/protolib/components/NextLink.tsx
@@ -3,6 +3,6 @@ import * as React from 'react'
 
 export const NextLink = React.forwardRef(({native, ...props}:any, ref:any) => {
   return (
-    native ? <a ref={ref} {...props} className={`next-link ` + (props.className || '')}></a>:<NextInternalLink ref={ref} {...props} className={`next-link ` + (props.className || '')} />
+    native ? <a ref={ref} {...props} prefetch={props.prefetch?"true":"false"} className={`next-link ` + (props.className || '')}></a>:<NextInternalLink ref={ref} {...props} prefetch={props.prefetch} className={`next-link ` + (props.className || '')} />
   )
 }) as typeof NextInternalLink
