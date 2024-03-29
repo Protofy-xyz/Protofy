7eab2df9b7055265ff0a3eb40ee7fe9487b861c8
jcarlosn <jcarlos.norte@gmail.com>
2024-03-29 14:29:17 +0100

    Wraps pino logger to have a third parameters id

--- packages/protolib/base/logger.ts
+++ packages/protolib/base/logger.ts
@@ -8,5 +8,14 @@ export const getLogger = () => {
   if (!logger) {
     logger = pino(config.logger)
   }
-  return logger
+  
+  //wrapper for pino, allowing to have a third parameter id, to correlate messages with specific code nodes
+  return {
+    error: (data, msg?, id?) => logger.error(data, msg),
+    fatal: (data, msg?, id?) => logger.fatal(data, msg),
+    warn: (data, msg?, id?) => logger.warn(data, msg),
+    info: (data, msg?, id?) => logger.info(data, msg),
+    debug: (data, msg?, id?) => logger.debug(data, msg),
+    trace: (data, msg?, id?) => logger.trace(data, msg),
+  }
 }
