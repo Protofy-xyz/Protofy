75c84a9601c07ac98df8b72d81eef1672881ab76
jcarlosn <jcarlos.norte@gmail.com>
2024-04-02 03:33:56 +0200

    Reordered default workspace menus, files should be in cms. Devices is IoT devices

--- packages/app/bundles/custom/workspaces/admin.tsx
+++ packages/app/bundles/custom/workspaces/admin.tsx
@@ -8,21 +8,20 @@ export default {
             { "name": "Users", "icon": "users", "type": "users", "path": "/" },
             { "name": "Groups", "icon": "groups", "type": "groups", "path": "/" },
             { "name": "Events", "icon": "activity", "type": "events", "path": "/?page=0&orderBy=created&orderDirection=desc" },
-            // { "name": "Tasks", "icon": "zap", "type": "tasks", "path": "/" },
             { "name": "Messages", "icon": Inbox, "type": "messages", "path": "/"},
         ],
         "Development": [
-            { "name": "Files", "icon": "folder", "type": "files", "path": "/?path=packages/app/bundles/custom" },
             { "name": "Objects", "icon": "box", "type": "objects", "path": "/" },
             { "name": "Pages", "icon": "layout", "type": "pages", "path": "/" },
             { "name": "Automations", "icon": ToyBrick, "type": "apis", "path": "/" },
         ],
         "CMS": [
+            { "name": "Files", "icon": "folder", "type": "files", "path": "/?path=packages/app/bundles/custom" },
             { "name": "Databases", "icon": "database", "type": "databases", "path": "/"},
             { "name": "Resources", "icon": Library, "type": "resources", "path": "/"},
             { "name": "Public", "icon": "doorOpen", "type": "files", "path": "/?path=/apps/next/public" },
         ],
-        "Devices":[
+        "IoT Devices":[
             { "name": "Devices", "icon": "serverConf", "type":"devices", "path": "/"},
             { "name": "Definitions", "icon": "bookOpen", "type":"deviceDefinitions", "path": "/"}
         ]
--- packages/app/bundles/custom/workspaces/editor.tsx
+++ packages/app/bundles/custom/workspaces/editor.tsx
@@ -1,21 +1,22 @@
 import { Cog, Inbox, Library, ToyBrick } from '@tamagui/lucide-icons'
 
 export default {
-    "default": "/workspace/events",
+    "default": "/admin/pages",
     "label": "Workspace",
     "menu": {
         "System": [
             { "name": "Users", "icon": "users", "type": "users", "path": "/" },
             { "name": "Groups", "icon": "groups", "type": "groups", "path": "/" },
-            { "name": "Events", "icon": "activity", "type": "events", "path": "/" },
-            // { "name": "Tasks", "icon": "zap", "type": "tasks", "path": "/" },
-            { "name": "Public", "icon": "doorOpen", "type": "files", "path": "/?path=/apps/next/public" },
-            { "name": "Files", "icon": "folder", "type": "files", "path": "/?path=packages/app/bundles/custom" },    
+            { "name": "Events", "icon": "activity", "type": "events", "path": "/?page=0&orderBy=created&orderDirection=desc" },
             { "name": "Messages", "icon": Inbox, "type": "messages", "path": "/"},
-            { "name": "Resources", "icon": Library, "type": "resources", "path": "/"},
-            { "name": "Databases", "icon": "database", "type": "databases", "path": "/"}
         ],
-        "Devices":[
+        "CMS": [
+            { "name": "Files", "icon": "folder", "type": "files", "path": "/?path=packages/app/bundles/custom" },
+            { "name": "Databases", "icon": "database", "type": "databases", "path": "/"},
+            { "name": "Resources", "icon": Library, "type": "resources", "path": "/"},
+            { "name": "Public", "icon": "doorOpen", "type": "files", "path": "/?path=/apps/next/public" },
+        ],
+        "IoT Devices":[
             { "name": "Devices", "icon": "serverConf", "type":"devices", "path": "/"},
             { "name": "Definitions", "icon": "bookOpen", "type":"deviceDefinitions", "path": "/"}
         ]
