7fc3ef1d5d3af54a2a71953b6a286c0bc76c1049
Noemí Blázquez <ap0calipse@gmail.com>
2024-04-03 04:14:03 +0200

    Improves InputSelect integration with tamagui theme
    Co-authored-by: jcarlosn <jcarlos.norte@gmail.com>

--- packages/protolib/components/InputSelect.tsx
+++ packages/protolib/components/InputSelect.tsx
@@ -2,31 +2,66 @@ import React from 'react';
 import CreatableSelect from 'react-select/creatable';
 import { useThemeSetting } from '@tamagui/next-theme'
 import { getTokenValue } from 'tamagui';
-import {useTheme} from '@my/ui';
+import { useTheme } from '@my/ui';
 import { useTint } from '../lib/Tints';
 
-export const InputSelect = React.forwardRef(({controlStyle={}, containerStyle = {},...props}: any, ref: any) => {
+export const InputSelect = React.forwardRef(({ controlStyle = {}, containerStyle = {}, ...props }: any, ref: any) => {
   const { resolvedTheme } = useThemeSetting()
   const theme = useTheme()
   const tint = useTint()
   const darkMode = resolvedTheme == 'dark'
-  return <CreatableSelect ref={ref} {...props}
+  const DropdownIndicator = () => null;
+  const IndicatorSeparator = () => null;
+  return <CreatableSelect
+    components={{ DropdownIndicator, IndicatorSeparator }}
+    ref={ref}
+    isClearable={true}
     styles={{
       option: (provided, state) => ({
         ...provided,
-        backgroundColor: state.isFocused ? theme['color5'].val : provided.backgroundColor,
+        backgroundColor: state.isSelected ? theme['color7'].val : (state.isFocused ? theme['color5'].val : provided.backgroundColor),
         // color: state.isFocused ? 'colorDeTextoDeseado' : provided.color,
       }),
       menu: (provided) => ({
         ...provided,
         backgroundColor: theme.bgContent.val,
+        borderColor: 'red',
         zIndex: 9999999999999
       }),
-      control: (provided) => ({
+      control: (provided, state) => ({
         ...provided,
         width: '100%',
         backgroundColor: theme.bgContent.val,
+        outline: 'none',
+        boxShadow: 'none',
+        borderColor: darkMode ? '#aaa' : '#ccc',
+        borderWidth: 0,
+        borderBottomWidth: 1,
+        borderRadius: 0,
+        color: 'red',
+        "&:hover": {
+          outline: 'none',
+          boxShadow: 'none',
+          borderColor: theme.color8.val,
+          borderWidth: 0,
+          borderBottomWidth: 1
+        },
+        "&:focus": {
+          outline: 'none',
+          boxShadow: 'none',
+          borderColor: theme.color8.val,
+          borderWidth: 0,
+          borderBottomWidth: 1
+        },
         ...controlStyle
+      }),
+      input: (provided) => ({
+        ...provided,
+        color: theme.color.val
+      }),
+      singleValue: (provided) => ({
+        ...provided,
+        color: theme.color.val
       }),
       container: (provided) => ({
         ...provided,
@@ -34,6 +69,7 @@ export const InputSelect = React.forwardRef(({controlStyle={}, containerStyle = 
         ...containerStyle
       }),
     }}
+    {...props}
   >
     {props.children}
   </CreatableSelect>;
