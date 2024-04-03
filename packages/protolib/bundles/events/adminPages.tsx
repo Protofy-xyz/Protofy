e1271e76eb86443b67d991fbf62cc31b6eb59048
Noemí Blázquez <ap0calipse@gmail.com>
2024-04-03 05:15:20 +0200

    Preparing events page to support advanced filters
    Co-authored-by: jcarlosn <jcarlos.norte@gmail.com>

--- packages/protolib/bundles/events/adminPages.tsx
+++ packages/protolib/bundles/events/adminPages.tsx
@@ -1,5 +1,5 @@
 import { EventModel } from '.'
-import { DataTable2, Chip, DataView, AdminPage, PaginatedDataSSR } from 'protolib'
+import { DataTable2, Chip, DataView, AdminPage, InputSelect } from 'protolib'
 import moment from 'moment'
 import { ClipboardList } from '@tamagui/lucide-icons';
 import { JSONViewer } from '../../components/jsonui'
@@ -7,6 +7,9 @@ import { usePrompt } from '../../context/PromptAtom'
 import { useEffect, useState } from 'react';
 import { API, getPendingResult } from '../../base';
 import { usePendingEffect } from '../../lib/usePendingEffect';
+import { Paragraph, XStack } from 'tamagui';
+import { Tinted } from '../../components/Tinted';
+
 
 const format = 'HH:mm:ss DD-MM-YYYY'
 const EventIcons = {}
@@ -29,9 +32,6 @@ export default {
             const [options, setOptions] = useState(getPendingResult('pending'))
             usePendingEffect((s) => { API.get({ url: '/adminapi/v1/events/options/all' }, s) }, setOptions, options)
 
-            useEffect(() => {
-                console.log('we have options: ', options)
-            }, [options])
             return (<AdminPage title="Events" pageSession={pageSession}>
                 <DataView
                     integratedChat
@@ -44,6 +44,24 @@ export default {
                     disableViewSelector={false}
                     defaultView={'list'}
                     rowIcon={ClipboardList}
+                    // toolBarContent={<XStack justifyContent="flex-end" ai="center" width={'100%'} f={1} ml={"$5"}>
+                    //     <XStack width={"250px"}>
+                    //         <Tinted>
+                    //             <InputSelect
+                    //                 menuStyle={{
+                    //                     width: '600px'
+                    //                 }}
+                    //                 placeholder="Path..."
+                    //                 controlStyle={{
+                    //                     width: '250px'
+                    //                 }}
+                    //                 options={options.isLoaded ? options.data.paths.map(p => {
+                    //                     return { value: p, label: p }
+                    //                 }) : []} />
+                    //         </Tinted>
+                    //     </XStack>
+
+                    // </XStack>}
                     columns={DataTable2.columns(
                         DataTable2.column("path", row => row.path, true, undefined, true, '250px'),
                         DataTable2.column("user", row => row.user, true, undefined, true, '200px'),
