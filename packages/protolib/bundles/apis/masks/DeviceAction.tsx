59523cfdde8888dc1ab87811d6d4e234f81436e0
Gerard <gerard@protofy.xyz>
2024-04-04 15:38:03 +0200

    [add] device action with param4

--- packages/protolib/bundles/apis/masks/DeviceAction.tsx
+++ packages/protolib/bundles/apis/masks/DeviceAction.tsx
@@ -14,21 +14,12 @@ const DeviceAction = (node: any = {}, nodeData = {}) => {
     let deviceAction = nodeData['param3'];
 
     const [devicesData, setDevicesData] = useState<any[]>([]);
-    const [payloadVisibility, setPayloadVisibility] = useState(false);
     const color = useColorFromPalette(6)
 
-    const updatePayloadVisibility = async (devicesData) => {
-        const subsystem = devicesData.filter(device => device.name === deviceName?.replaceAll('"', ''))[0]?.subsystem
-        const actions = subsystem?.filter(subsystem => subsystem.name === deviceComponent?.replaceAll('"', ''))[0]?.actions
-        const payloadValue = actions?.filter(action => action.name === deviceAction?.replaceAll('"', ''))[0]?.payload?.value
-        setPayloadVisibility(payloadValue ? true : false)
-    }
-    
     const getDevices = async () => {
         const { data } = await deviceRepository.list()
         const { items: devices } = data;
         setDevicesData([...devices]);
-        updatePayloadVisibility(devices)
     }
 
     // Device
@@ -44,26 +35,18 @@ const DeviceAction = (node: any = {}, nodeData = {}) => {
     const selectedSubsystemModel = new SubsystemModel(selectedSubsystem)
     // Action
     const subsystemActionNames = selectedSubsystemModel.getActionsNames(true) ?? [];
-
-    const actionPayloadNodeParams: Field[] = [
-        {
-            label: 'Action payload', field: 'param4', type: 'input', static: true,
-        }
-    ] as Field[]
+    const selectedAction = selectedSubsystemModel.getActions()?.find((action) => action.name === deviceAction?.replaceAll('"', ''))
+    const actionValue = selectedAction?.payload?.value;
 
     useEffect(() => {
         getDevices()
     }, [])
-
-    useEffect(() => {
-        updatePayloadVisibility(devicesData)
-    }, [deviceAction])
-
     return (
         <Node icon={Play} node={node} isPreview={!node.id} title='Device Action' color={color} id={node.id} skipCustom={true} disableInput disableOutput>
             <NodeParams id={node.id} params={[{ label: 'Device name', field: 'param1', type: 'select', static: true, data: deviceNames }]} />
             {deviceSubsystemsNames?.length ? <NodeParams id={node.id} params={[{ label: 'Component', field: 'param2', type: 'select', static: true, data: deviceSubsystemsNames }]} /> : null}
             {subsystemActionNames?.length ? <NodeParams id={node.id} params={[{ label: 'Action', field: 'param3', type: 'select', static: true, data: subsystemActionNames }]} /> : null}
+            {!actionValue && deviceAction ?<NodeParams id={node.id} params={[{ label: 'Action payload', field: 'param4', type: 'input', static: true }]} /> : null }
         </Node>
     )
 }
@@ -80,5 +63,5 @@ export default {
     category: "ioT",
     keywords: ['action', "automation", 'esp32', 'device', 'iot'],
     getComponent: DeviceAction,
-    getInitialData: () => { return { to: 'context.deviceAction', param1: "", param2: "", param3: "", await: true } }
+    getInitialData: () => { return { to: 'context.deviceAction', param1: "", param2: "", param3: "", param4: "", await: true } }
 }
