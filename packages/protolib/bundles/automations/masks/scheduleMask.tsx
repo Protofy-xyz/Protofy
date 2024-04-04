861497190228fb7dd5a5a9775e890fcf7d2cec56
noreplydev <contact.cristiansanchez@gmail.com>
2024-04-04 21:30:16 +0200

    Make async the scheduledMask callback function

--- packages/protolib/bundles/automations/masks/scheduleMask.tsx
+++ packages/protolib/bundles/automations/masks/scheduleMask.tsx
@@ -12,7 +12,7 @@ const ScheduleMask = ({ node = {}, nodeData = {}, children }: any) => {
             <NodeParams id={node.id} params={[{ label: 'Year', field: 'param5', type: 'input' }]} />
             <div style={{ paddingBottom: '80px' }}>
                 <FlowPort id={node.id} type='input' label='Execute' style={{ top: '300px' }} handleId={'request'} />
-                <FallbackPort node={node} port={'param2'} type={"target"} fallbackPort={'request'} portType={"_"} preText="() =>" postText="" />
+                <FallbackPort node={node} port={'param2'} type={"target"} fallbackPort={'request'} portType={"_"} preText="async () =>" postText="" />
             </div>
         </Node>
     )
@@ -39,7 +39,7 @@ export default {
     getInitialData: () => { return { 
         to: 'context.createSchedule', 
         param1: '"12:30"',
-        param2: '() =>', 
+        param2: 'async () =>', 
         param3: '"5"', 
         param4: '"april"', 
         param5: '"2024"', 
@@ -47,48 +47,3 @@ export default {
     filterChildren: filterCallback(),
     restoreChildren: restoreCallback(),
 } 
-
-/*
-import { Node, Field, FlowPort, NodeParams, FallbackPort, Button } from 'protoflow';
-import { API } from 'protolib'
-import { Plug } from 'lucide-react';
-import { filterCallback, restoreCallback } from 'protoflow';
-import { useColorFromPalette } from 'protoflow/src/diagram/Theme'
-
-const Automation = (node: any = {}, nodeData = {}) => {
-    const color = useColorFromPalette(9)
-    return (
-        <Node icon={Plug} node={node} isPreview={!node?.id} title='te volviste un probelma' id={node.id} color={color} skipCustom={true}>
-            <NodeParams id={node.id} params={[{ label: 'Name', static: true, field: 'param3', type: 'input', pre: (str) => str.replace(/['"]+/g, ''), post: (str) => '"' + str + '"' }]} />
-            <div>
-                <FlowPort id={node.id} type='input' label='On run (params)' style={{ top: '130px' }} handleId={'request'} />
-                <FallbackPort node={node} port={'param2'} type={"target"} fallbackPort={'request'} portType={"_"} preText="async (params) => " postText="" />
-            </div>
-
-            <Button style={{marginTop: '60px'}} label="Run" onPress={() => {
-                API.get('/api/v1/automations/'+eval(nodeData['param3']))
-            }} />
-        </Node>
-    )
-}
-
-export default {
-    id: 'te volviste un probelma',
-    type: 'CallExpression',
-    check: (node, nodeData) => {
-        return (
-            node.type == "CallExpression"
-            && nodeData.to == 'context.automation'
-            && nodeData.param2 && nodeData.param2.startsWith
-            && (nodeData.param2?.startsWith('async (params) =>') || nodeData.param2?.startsWith('(params) =>'))
-        )
-    },
-    category: "automation",
-    keywords: ["automation", 'trigger'],
-    getComponent: Automation,
-    filterChildren: filterCallback(),
-    restoreChildren: restoreCallback(),
-    getInitialData: () => { return { to: 'context.automation', param1: 'app', param2: 'async (params) =>', param3: '""' } }
-}
-
-*/
