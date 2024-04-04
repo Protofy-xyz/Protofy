7280243c9c021be23128842c697edf470ffafaec
TonProtofy <ton.miserachs@protofy.xyz>
2024-04-04 12:06:06 +0200

    Remove mistaken commited API

--- packages/app/bundles/custom/apis/index.ts
+++ packages/app/bundles/custom/apis/index.ts
@@ -1,9 +1,6 @@
 import {Protofy} from 'protolib/base'
-import jhnApi from "./jhn";
 
-const autoApis = Protofy("apis", {
-    jhn: jhnApi
-})
+const autoApis = Protofy("apis", {})
 
 export default (app, context) => {
     Object.keys(autoApis).forEach((k) => {
