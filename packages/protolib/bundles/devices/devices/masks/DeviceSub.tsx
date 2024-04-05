3e7e3c56d5fd243270e545655e368ad3ba7cc472
Gerard <gerard@protofy.xyz>
2024-04-05 10:56:56 +0200

    [refactor] device listener, now called device subscribe

--- packages/protolib/bundles/devices/devices/masks/DeviceSub.tsx
+++ packages/protolib/bundles/devices/devices/masks/DeviceSub.tsx
@@ -1,126 +1,61 @@
-import { Node, Field, NodeParams, FlowPort, FallbackPort } from 'protoflow';
-import { API, Text } from 'protolib';
+import { Node, NodeParams, FlowPort, FallbackPort } from 'protoflow';
 import { useState, useEffect } from 'react';
-import {useColorFromPalette} from 'protoflow/src/diagram/Theme'
+import { useColorFromPalette } from 'protoflow/src/diagram/Theme'
 import { Cable } from 'lucide-react';
-
 import { filterCallback, restoreCallback } from 'protoflow';
-
-const getDeviceSubsystemsNames = (devData) => {
-    const deviceSubsystems = {}
-    devData?.forEach(device => {
-        const subsystemsNames = device?.subsystem?.filter(sub => sub?.monitors && sub?.monitors?.length).map(sub => '"' + sub.name + '"')
-        deviceSubsystems['"' + device.name + '"'] = subsystemsNames
-    });
-    return deviceSubsystems
-}
-
-const getSubsystemsActions = (devData) => {
-    const deviceSubsystemsActions = {};
-
-    devData.forEach(device => {
-        const deviceName = '"' + device.name + '"';
-        const actions = {};
-
-        device.subsystem?.forEach(subsystem => {
-            subsystem.actions?.forEach(action => {
-                actions['"' + subsystem.name + '"'] = actions['"' + subsystem.name + '"'] || [];
-                actions['"' + subsystem.name + '"'].push('"' + action.name + '"');
-            });
-        });
-
-        deviceSubsystemsActions[deviceName] = actions;
-    });
-    return deviceSubsystemsActions
-
-}
-
-const getSubsystemsMonitors = (devData) => {
-    const deviceSubsystemsMonitors = {};
-
-    devData.forEach(device => {
-        const deviceName = '"' + device.name + '"';
-        const monitors = {};
-
-        device.subsystem?.forEach(subsystem => {
-            subsystem.monitors?.forEach(monitor => {
-                monitors['"' + subsystem.name + '"'] = monitors['"' + subsystem.name + '"'] || [];
-                monitors['"' + subsystem.name + '"'].push('"' + monitor.name + '"');
-            });
-        });
-
-        deviceSubsystemsMonitors[deviceName] = monitors;
-    });
-    console.log("DeviceSubsystemsMonitors: ", deviceSubsystemsMonitors)
-    return deviceSubsystemsMonitors
-
-}
-
-const getDeviceNames = (devData) => devData?.map((device) => '"' + device.name + '"')
+import { DeviceRepository } from '../../repositories/deviceRepository';
+import { DeviceCollection, DeviceModel } from '../../models/DeviceModel';
+import { DeviceDataType, SubsystemType } from '../../models/interfaces';
+import { SubsystemCollection, SubsystemModel } from '../../models/SubsystemModel';
 
 
+const deviceRepository = new DeviceRepository()
 const DeviceSub = ({ node = {}, nodeData = {}, children }: any) => {
-    const color = useColorFromPalette(7)
-    const [devicesData, setDevicesData] = useState<any[]>([]);
-    // const [payloadVisibility, setPayloadVisibility] = useState(false);
+
     let deviceName = nodeData['param1'];
     let deviceComponent = nodeData['param2'];
-    let deviceMonitor= nodeData['param3'];
+    let deviceMonitor = nodeData['param3'];
 
-    // const updatePayloadVisibility = async (devicesData) => {
-    //     const subsystem = devicesData.filter( device => device.name === deviceName.replaceAll('"', ''))[0]?.subsystem
-    //     const actions = subsystem?.filter( subsystem => subsystem.name === deviceComponent.replaceAll('"', ''))[0]?.actions
-    //     // const payloadValue = actions?.filter( action => action.name === deviceAction.replaceAll('"', ''))[0]?.payload?.value
-    //     // setPayloadVisibility(payloadValue ? true : false)
-    // }
+    const color = useColorFromPalette(7)
+    const [devicesData, setDevicesData] = useState<any[]>([]);
+
     const getDevices = async () => {
-        const { data } = await API.get("/adminapi/v1/devices")
-        console.log("Readed deviceSub items: ",data)
-        const devices = data?.items;
+        const { data } = await deviceRepository.list()
+        const { items: devices } = data;
         setDevicesData([...devices]);
-        // updatePayloadVisibility(devices)
     }
+
+    // Device
+    const deviceCollection = new DeviceCollection(devicesData);
+    const deviceNames = deviceCollection?.getNames(true) ?? [];
+    const selectedDevice: DeviceDataType = deviceCollection.findByName(deviceName, true);
+    const selectedDeviceModel = new DeviceModel(selectedDevice)
+    // Subsystem
+    const deviceSubsystems = selectedDeviceModel.getSubsystems()
+    const subsystemsCollection = new SubsystemCollection(deviceSubsystems);
+    const deviceSubsystemsNames = selectedDeviceModel.getSubsystemNames('monitor', true) ?? [];
+    const selectedSubsystem: SubsystemType = subsystemsCollection.findByName(deviceComponent, true);
+    const selectedSubsystemModel = new SubsystemModel(selectedSubsystem)
+    // Monitor
+    const subsystemMonitorNames = selectedSubsystemModel.getMonitorsNames(true) ?? [];
+    // const selectedMonitor = selectedSubsystemModel.getActionByName(deviceMonitor?.replaceAll('"', ''))
 
     useEffect(() => {
         getDevices()
     }, [])
-    
-    // useEffect(() => {
-    //     updatePayloadVisibility(devicesData)
-    // }, [deviceAction])
 
-    const nodeParams: Field[] = [
-        {
-            label: 'Device name', field: 'param1', type: 'select', static: true,
-            selectedIndex: getDeviceNames(devicesData)?.indexOf(deviceName) ?? 0,
-            data: devicesData ? getDeviceNames(devicesData) : ['"none"'],
-        },
-        {
-            label: 'Component', field: 'param2', type: 'select', static: true,
-            selectedIndex: getDeviceSubsystemsNames(devicesData)[deviceName]?.indexOf(deviceComponent) ?? 0,
-            data: devicesData ? (getDeviceSubsystemsNames(devicesData)[deviceName] ?? []) : ['"none"'],
-        },
-        {
-            label: 'Monitor', field: 'param3', type: 'select', static: true,
-            selectedIndex: devicesData && getSubsystemsMonitors(devicesData)[deviceName] ? getSubsystemsMonitors(devicesData)[deviceName][deviceComponent]?.indexOf(deviceMonitor) ?? [] : 0,
-            data: devicesData && getSubsystemsMonitors(devicesData)[deviceName] ? getSubsystemsMonitors(devicesData)[deviceName][deviceComponent] ?? [] : ['"none"'],
-        }
-    ] as Field[]
-
-    // const actionPayloadNodeParams: Field[] = [
-    //     {
-    //         label: 'Action payload', field: 'param4', type: 'input', static: true,
-    //     }
-    // ] as Field[]
-    
     return (
-        <Node icon={Cable} node={node} isPreview={!node.id} title='Device Listener' color={color} id={node.id} skipCustom={true} disableInput disableOutput>
-            <NodeParams id={node.id} params={nodeParams} />
-            {/* {payloadVisibility ? <></> : <NodeParams id={node.id} params={actionPayloadNodeParams} />} */}
-            <div style={{ marginTop: "35px" }}>
-                <FlowPort id={node.id} type='output' label='On (message, topic)' style={{ top: '225px' }} handleId={'request'} />
-                <FallbackPort node={node} port={'param4'} type={"target"} fallbackPort={'request'} portType={"_"} preText="(message,topic) => " postText="" />
-            </div>
+        <Node icon={Cable} node={node} isPreview={!node.id} title='Device Subscribe' color={color} id={node.id} skipCustom={true} disableInput disableOutput>
+            <NodeParams id={node.id} params={[{ label: 'Device name', field: 'param1', type: 'select', static: true, data: deviceNames }]} />
+            {deviceSubsystemsNames?.length ? <NodeParams id={node.id} params={[{ label: 'Component', field: 'param2', type: 'select', static: true, data: deviceSubsystemsNames }]} /> : null}
+            {subsystemMonitorNames?.length ? <NodeParams id={node.id} params={[{ label: 'Monitor', field: 'param3', type: 'select', static: true, data: subsystemMonitorNames }]} /> : null}
+            {
+                (deviceName && deviceComponent && deviceMonitor) ?
+                    <div style={{ marginTop: "35px" }}>
+                        <FlowPort id={node.id} type='output' label='On (message, topic)' style={{ top: '225px' }} handleId={'request'} />
+                        <FallbackPort node={node} port={'param4'} type={"target"} fallbackPort={'request'} portType={"_"} preText="(message,topic) => " postText="" />
+                    </div> : null
+            }
         </Node>
     )
 }
@@ -133,5 +68,5 @@ export default {
     getComponent: (node, nodeData, children) => <DeviceSub node={node} nodeData={nodeData} children={children} />,
     filterChildren: filterCallback("4"),
     restoreChildren: restoreCallback("4"),
-    getInitialData: () => { return { to: 'context.deviceSub', param1: '"none"', param2: '"none"', param3: '"none"', param4: '(message,topic) =>' } }
+    getInitialData: () => { return { to: 'context.deviceSub', param1: '', param2: '', param3: '', param4: '(message,topic) =>' } }
 }
