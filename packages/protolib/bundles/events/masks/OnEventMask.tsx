3562d2ccafabcc90ec63cceaae6e2f3295d4139c
lluis@protofy.xyz <lluis@protofy.xyz>
2024-04-07 02:42:27 +0200

    [ADD] Support for asyncrhonous function inside callback in onEventMask

--- packages/protolib/bundles/events/masks/OnEventMask.tsx
+++ packages/protolib/bundles/events/masks/OnEventMask.tsx
@@ -1,4 +1,4 @@
-import { Node, FlowPort, NodeParams, FallbackPort, filterCallback, restoreCallback } from 'protoflow';
+import { Node, FlowPort, NodeParams, FallbackPort, filterCallback, restoreCallback, getFieldValue } from 'protoflow';
 import { useColorFromPalette } from 'protoflow/src/diagram/Theme'
 import { Cable } from 'lucide-react';
 
@@ -10,7 +10,7 @@ const OnEventMask = ({ node = {}, nodeData = {}, children }: any) => {
             <NodeParams id={node.id} params={[{ label: 'From', field: 'param-4', type: 'input' }]} />
             <div style={{ paddingBottom: "30px" }}>
                 <FlowPort id={node.id} type='input' label='On Event (event)' style={{ top: '170px' }} handleId={'request'} />
-                <FallbackPort node={node} port={'param-2'} type={"target"} fallbackPort={'request'} portType={"_"} preText="(event) => " postText="" />
+                <FallbackPort node={node} port={'param-2'} type={"target"} fallbackPort={'request'} portType={"_"} preText="async (event) => " postText="" />
             </div>
         </Node>
     )
@@ -23,9 +23,14 @@ export default {
     type: 'CallExpression',
     category: "automation",
     keywords: ["automation", 'event', 'trigger'],
-    check: (node, nodeData) => node.type == "CallExpression" && nodeData.to == 'context.onEvent' && nodeData["param-4"] != "device" && nodeData["param-4"] != "device",
+    check: (node, nodeData) => {
+        return node.type == "CallExpression" 
+        && nodeData.to == 'context.onEvent'
+        && getFieldValue('param-2', nodeData)?.startsWith('(event) =>') || getFieldValue('param-2', nodeData)?.startsWith('async (event) =>')  
+        && getFieldValue('param-4', nodeData) != "device"
+    },
     getComponent: (node, nodeData, children) => <OnEventMask node={node} nodeData={nodeData} children={children} />,
-    getInitialData: () => { return { to: 'context.onEvent', "param-1": { value: 'context', kind: "Identifier" }, "param-2": { value: '(event) =>', kind: "Identifier" }, "param-3": { value: "", kind: "StringLiteral" }, "param-4": { value: '', kind: "StringLiteral" } } },
-    filterChildren: filterCallback(),
-    restoreChildren: restoreCallback(),
+    getInitialData: () => { return { to: 'context.onEvent', "param-1": { value: 'context', kind: "Identifier" }, "param-2": { value: 'async (event) =>', kind: "Identifier" }, "param-3": { value: "", kind: "StringLiteral" }, "param-4": { value: '', kind: "StringLiteral" } } },
+    filterChildren: filterCallback('-2'),
+    restoreChildren: restoreCallback('-2'),
 }
