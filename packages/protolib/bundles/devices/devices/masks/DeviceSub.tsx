dcdeb196e041fe84b5b990890643245ad79ca105
Gerard <gerard@protofy.xyz>
2024-04-08 12:43:37 +0200

    [fix] device Publish

--- packages/protolib/bundles/devices/devices/masks/DeviceSub.tsx
+++ packages/protolib/bundles/devices/devices/masks/DeviceSub.tsx
@@ -1,4 +1,4 @@
-import { Node, NodeParams, FlowPort, FallbackPort } from 'protoflow';
+import { Node, NodeParams, FlowPort, FallbackPort, getFieldValue } from 'protoflow';
 import { useState, useEffect } from 'react';
 import { useColorFromPalette } from 'protoflow/src/diagram/Theme'
 import { Cable } from 'lucide-react';
@@ -12,12 +12,13 @@ import { SubsystemCollection, SubsystemModel } from '../../models/SubsystemModel
 const deviceRepository = new DeviceRepository()
 const DeviceSub = ({ node = {}, nodeData = {}, children }: any) => {
 
+    // const [payloadVisibility, setPayloadVisibility] = useState(false);
+    let deviceName = getFieldValue("param-1", nodeData);
+    let deviceComponent = deviceName ? getFieldValue("param-2", nodeData) : "";
+    let deviceMonitor = deviceName ? getFieldValue("param-3", nodeData) : "";
+
     const color = useColorFromPalette(7)
     const [devicesData, setDevicesData] = useState<any[]>([]);
-    // const [payloadVisibility, setPayloadVisibility] = useState(false);
-    let deviceName = nodeData['param-1'];
-    let deviceComponent = nodeData['param-2'];
-    let deviceMonitor= nodeData['param-3'];
 
     const getDevices = async () => {
         const { data } = await deviceRepository.list()
@@ -27,17 +28,17 @@ const DeviceSub = ({ node = {}, nodeData = {}, children }: any) => {
 
     // Device
     const deviceCollection = new DeviceCollection(devicesData);
-    const deviceNames = deviceCollection?.getNames(true) ?? [];
-    const selectedDevice: DeviceDataType = deviceCollection.findByName(deviceName, true);
+    const deviceNames = deviceCollection?.getNames() ?? [];
+    const selectedDevice: DeviceDataType = deviceCollection.findByName(deviceName);
     const selectedDeviceModel = new DeviceModel(selectedDevice)
     // Subsystem
     const deviceSubsystems = selectedDeviceModel.getSubsystems()
     const subsystemsCollection = new SubsystemCollection(deviceSubsystems);
-    const deviceSubsystemsNames = selectedDeviceModel.getSubsystemNames('monitor', true) ?? [];
-    const selectedSubsystem: SubsystemType = subsystemsCollection.findByName(deviceComponent, true);
+    const deviceSubsystemsNames = selectedDeviceModel.getSubsystemNames('monitor') ?? [];
+    const selectedSubsystem: SubsystemType = subsystemsCollection.findByName(deviceComponent);
     const selectedSubsystemModel = new SubsystemModel(selectedSubsystem)
     // Monitor
-    const subsystemMonitorNames = selectedSubsystemModel.getMonitorsNames(true) ?? [];
+    const subsystemMonitorNames = selectedSubsystemModel.getMonitorsNames() ?? [];
     // const selectedMonitor = selectedSubsystemModel.getActionByName(deviceMonitor?.replaceAll('"', ''))
 
     useEffect(() => {
@@ -46,14 +47,14 @@ const DeviceSub = ({ node = {}, nodeData = {}, children }: any) => {
 
     return (
         <Node icon={Cable} node={node} isPreview={!node.id} title='Device Subscribe' color={color} id={node.id} skipCustom={true} disableInput disableOutput>
-            <NodeParams id={node.id} params={[{ label: 'Device name', field: 'param1', type: 'select', static: true, data: deviceNames }]} />
-            {deviceSubsystemsNames?.length ? <NodeParams id={node.id} params={[{ label: 'Component', field: 'param2', type: 'select', static: true, data: deviceSubsystemsNames }]} /> : null}
-            {subsystemMonitorNames?.length ? <NodeParams id={node.id} params={[{ label: 'Monitor', field: 'param3', type: 'select', static: true, data: subsystemMonitorNames }]} /> : null}
+            <NodeParams id={node.id} params={[{ label: 'Device name', field: 'param-1', type: 'select', static: true, data: deviceNames }]} />
+            <NodeParams id={node.id} params={[{ label: 'Component', field: 'param-2', type: 'select', static: true, data: deviceSubsystemsNames }]} />
+            <NodeParams id={node.id} params={[{ label: 'Monitor', field: 'param-3', type: 'select', static: true, data: subsystemMonitorNames }]} />
             {
                 (deviceName && deviceComponent && deviceMonitor) ?
                     <div style={{ marginTop: "35px" }}>
                         <FlowPort id={node.id} type='output' label='On (message, topic)' style={{ top: '225px' }} handleId={'request'} />
-                        <FallbackPort node={node} port={'param4'} type={"target"} fallbackPort={'request'} portType={"_"} preText="(message,topic) => " postText="" />
+                        <FallbackPort node={node} port={'param-4'} type={"target"} fallbackPort={'request'} portType={"_"} preText="(message,topic) => " postText="" />
                     </div> : null
             }
         </Node>
@@ -68,5 +69,5 @@ export default {
     getComponent: (node, nodeData, children) => <DeviceSub node={node} nodeData={nodeData} children={children} />,
     filterChildren: filterCallback("4"),
     restoreChildren: restoreCallback("4"),
-    getInitialData: () => { return { to: 'context.deviceSub', "param-1": '', "param-2": '', "param-3": '', "param-4": '(message,topic) =>' } }
+    getInitialData: () => { return { to: 'context.deviceSub', "param-1": { value: "", kind: "StringLiteral" }, "param-2": { value: "", kind: "StringLiteral" }, "param-3": { value: "", kind: "StringLiteral" }, "param-4": '(message,topic) =>' } }
 }
