575fe83551602bfa4e114f03a3e1e38b1aee0d83
jcarlosn <jcarlos.norte@gmail.com>
2024-04-09 13:54:55 +0200

    Revert "[add] sendMail to context"
    This reverts commit 1983ce87bcac93b0ef3ac6be63c3d609ba31a422.

--- packages/protolib/bundles/apis/api/index.ts
+++ packages/protolib/bundles/apis/api/index.ts
@@ -2,4 +2,3 @@ export { automation } from './automation';
 export { fetch } from './fetch';
 export { deviceMonitor } from './deviceMonitor';
 export { deviceAction } from './deviceAction';
-export { sendMail } from './sendMail';
--- packages/protolib/bundles/apis/api/sendMail.ts
+++
@@ -1,5 +0,0 @@
-import { sendMailWithResend } from 'protolib/api/lib/mail';
-
-export const sendMail = async (from, to, subject, content)=>{
-    return await sendMailWithResend(from, to, subject, content)
-} 
