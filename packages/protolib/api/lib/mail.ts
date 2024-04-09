9934567f82d5918c6baf4366a4b31c97ec6f3715
Gerard <gerard@protofy.xyz>
2024-04-09 09:11:08 +0200

    [add] mail with resend

---
+++ packages/protolib/api/lib/mail.ts
@@ -0,0 +1,28 @@
+import { Resend } from 'resend';
+import { getLogger } from 'protolib/base';
+
+const logger = getLogger()
+
+export const sendMailWithResend = async (from, to, subject, html) => {
+    // 'html' can be content or an html string
+    /* 
+    source: https://resend.com/
+    example:
+    {
+        from: 'onboarding@resend.dev',
+        to: 'gerard@protofy.xyz',
+        subject: 'Hello World',
+        html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
+    } 
+    */
+    const RESEND_TOKEN = process.env.MAIL_RESEND_TOKEN
+    if (!RESEND_TOKEN) {
+        logger.error("Resend Api key is not provided at env 'MAIL_RESEND_TOKEN'")
+        return
+    }
+    const resend = new Resend(RESEND_TOKEN);
+    const requestPayload = { from, to, subject, html }
+    const response = await resend.emails.send(requestPayload as any);
+    logger.info(response, "Email sent using 'Resend' to: " + requestPayload.to)
+    return requestPayload
+}
