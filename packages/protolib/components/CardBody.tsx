dcb8d2a0ba50415aa2f46d44712dd10a3198e38a
jcarlosn <jcarlos.norte@gmail.com>
2024-04-13 00:06:30 +0200

    Add props to cardbody

--- packages/protolib/components/CardBody.tsx
+++ packages/protolib/components/CardBody.tsx
@@ -1,12 +1,12 @@
-import { YStack, Paragraph } from "@my/ui"
+import { YStack, Paragraph, YStackProps } from "@my/ui"
 import { Tinted } from "./Tinted"
 
 type CardBodyProps = {
     title: any,
     children: any
 }
-export const CardBody = ({ title, children }: CardBodyProps) => {
-    return <YStack px={"$2"} pb="$5" f={1}>
+export const CardBody = ({ title, children, ...props }: CardBodyProps & YStackProps) => {
+    return <YStack px={"$2"} pb="$5" f={1} {...props}>
         <Tinted>
             <Paragraph mt="20px" ml="20px" fontWeight="700" size="$7">{title}</Paragraph>
             {children}
