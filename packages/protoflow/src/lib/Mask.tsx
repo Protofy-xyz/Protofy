95d89ad41a000fd2de7086c22d81226380e1fb2c
MiquelCos <miquel@protofy.xyz>
2024-04-17 15:42:42 +0200

    Refactors filter and restore callback functions

--- packages/protoflow/src/lib/Mask.tsx
+++ packages/protoflow/src/lib/Mask.tsx
@@ -1,56 +1,46 @@
 import { getId } from './Node'
 import { connectNodes } from './Edge';
 
-export const filterCallbackProp = (propName) => {
+export const filterCallbackNodes = (paramId = "2", type = "param", handleId = "request") => {
     return (node, childScope, edges) => {
-        const callbackId = edges.find(e => e.targetHandle == getId(node) + '-prop-' + propName)?.source
+        const callbackId = edges.find(e => e.targetHandle == getId(node) + '-' + type + '-' + paramId)?.source
         const callBack = edges.find(e => e.targetHandle == callbackId + '_call')
         if (callBack) {
-            edges.push(connectNodes(callBack.source, callBack.sourceHandle, getId(node), getId(node) + "_prop-" + propName))
+            edges.push(connectNodes(callBack.source, callBack.sourceHandle, getId(node), getId(node) + "_" + handleId))
         }
         return childScope.filter(child => child?.id != callbackId)
     }
 }
 
 export const filterCallback = (numParam = "2", handleId = "request") => {
-    return (node, childScope, edges) => {
-        const callbackId = edges.find(e => e.targetHandle == getId(node) + '-param-' + numParam)?.source
-        const callBack = edges.find(e => e.targetHandle == callbackId + '_call')
-        if (callBack) {
-            // console.log('new edge: ', connectNodes(getId(node), getId(node) + '_request', callBack.target, callBack.targetHandle))
-            edges.push(connectNodes(callBack.source, callBack.sourceHandle, getId(node), getId(node) + '_' + handleId))
+    return filterCallbackNodes(numParam, 'param', handleId)
+}
+
+export const filterCallbackProp = (paramId = "onPress") => {
+    var type = 'prop'
+    return filterCallbackNodes(paramId, type, type + "-" + paramId)
+}
+
+export const restoreCallbackNodes = (paramId, type) => {
+    return (node, nodes, originalNodes, edges, originalEdges) => {
+        const arrowEdge = originalEdges.find(e => e.targetHandle == node.id + '-' + type + '-' + paramId)
+        let recoveredEdges = []
+        let recoveredNodes: any = []
+        if (arrowEdge) {
+            const arrowNode = originalNodes?.find(n => n.id == arrowEdge.source)
+            if (arrowNode) {
+                recoveredNodes.push(arrowNode)
+            }
+            recoveredEdges = originalEdges.filter((e) => e.source == arrowNode?.id || e.target == arrowNode?.id)
         }
-        return childScope.filter(child => child?.id != callbackId)
+        return { nodes: [...recoveredNodes, ...nodes], edges: [...recoveredEdges, ...edges] }
     }
 }
 
 export const restoreCallback = (numParam = "2") => {
-    return (node, nodes, originalNodes, edges, originalEdges) => {
-        const arrowEdge = originalEdges.find(e => e.targetHandle == node.id + '-param-' + numParam)
-        let recoveredEdges = []
-        let recoveredNodes: any = []
-        if (arrowEdge) {
-            const arrowNode = originalNodes?.find(n => n.id == arrowEdge.source)
-            if (arrowNode) {
-                recoveredNodes.push(arrowNode)
-            }
-            recoveredEdges = originalEdges.filter((e) => e.source == arrowNode?.id || e.target == arrowNode?.id)
-        }
-        return { nodes: [...recoveredNodes, ...nodes], edges: [...recoveredEdges, ...edges] }
-    }
+    return restoreCallbackNodes(numParam, 'param')
 }
-export const restoreCallbackProp = (propName) => {
-    return (node, nodes, originalNodes, edges, originalEdges) => {
-        const arrowEdge = originalEdges.find(e => e.targetHandle == node.id + '-prop-' + propName)
-        let recoveredEdges = []
-        let recoveredNodes: any = []
-        if (arrowEdge) {
-            const arrowNode = originalNodes?.find(n => n.id == arrowEdge.source)
-            if (arrowNode) {
-                recoveredNodes.push(arrowNode)
-            }
-            recoveredEdges = originalEdges.filter((e) => e.source == arrowNode?.id || e.target == arrowNode?.id)
-        }
-        return { nodes: [...recoveredNodes, ...nodes], edges: [...recoveredEdges, ...edges] }
-    }
+
+export const restoreCallbackProp = (paramId = "onPress") => {
+    return restoreCallbackNodes(paramId, 'prop')
 }
