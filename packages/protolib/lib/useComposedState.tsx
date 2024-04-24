4fb42b2f916805d11d75441dc8c7a875b8c533b8
MiquelCos <miquel@protofy.xyz>
2024-04-24 10:46:15 +0200

    Refactors use composed state, now composedState use proxy object

--- packages/protolib/lib/useComposedState.tsx
+++ packages/protolib/lib/useComposedState.tsx
@@ -1,28 +1,30 @@
 import { useState } from "react";
 
 export const useComposedState = (initialStates = {}) => {
-    const [statesObj, setStatesObj] = useState(initialStates);
-  
-    const updateState = (key, value) => {
+  const [statesObj, setStatesObj] = useState(initialStates);
+
+  const updateState = (key, value) => {
       setStatesObj(prevStates => ({
-        ...prevStates,
-        [key]: { value, set: newValue => updateState(key, newValue) }
+          ...prevStates,
+          [key]: { value, set: newValue => updateState(key, newValue) }
       }));
-    };
-  
-    const composedState = (key) => {
-      if (statesObj[key]) {
-        return statesObj[key];
-      } else {
-        const newState = { value: '', set: (val) => updateState(key, val) };
-        updateState(key, newState.value);
-        return newState;
-      }
-    };
-  
-    var states: any = Object.keys(statesObj).reduce((acc, key) => {
-      return { ...acc, [key]: statesObj[key].value };
-    }, {});
-  
-    return { composedState, states };
   };
+
+  const composedState : any = new Proxy({}, {
+      get: function(target, prop) {
+          if (statesObj[prop]) {
+              return statesObj[prop];
+          } else {
+              const newState = { value: '', set: (val) => updateState(prop, val) };
+              updateState(prop, newState.value);
+              return newState;
+          }
+      }
+  });
+
+  var states : any = Object.keys(statesObj).reduce((acc, key) => {
+      return { ...acc, [key]: statesObj[key].value };
+  }, {});
+
+  return { composedState, states };
+};
