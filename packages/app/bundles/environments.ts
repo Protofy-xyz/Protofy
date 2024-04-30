f7baba87b5faa299726690ae0555aa8f22714dfb
jcarlosn <jcarlos.norte@gmail.com>
2024-04-30 14:10:07 +0200

    adds proxy to environments

--- packages/app/bundles/environments.ts
+++ packages/app/bundles/environments.ts
@@ -4,13 +4,15 @@ export const environments = {
         adminApi: process.env.ADMIN_API_URL ?? 'http://localhost:3002',
         frontend: process.env.SITE_URL ?? 'http://localhost:3000',
         admin: process.env.ADMIN_SITE_URL ?? 'http://localhost:3003',
-        docs: process.env.DOCS_SITE_URL ?? 'http://localhost:7600'
+        docs: process.env.DOCS_SITE_URL ?? 'http://localhost:7600',
+        proxy: process.env.PROXY_URL ?? 'http://localhost:8080'
     },
     prod: {
         api: process.env.API_URL ?? 'http://localhost:4001/',
         adminApi: process.env.ADMIN_API_URL ?? 'http://localhost:4002/',
         frontend: process.env.SITE_URL ?? 'http://localhost:4000/',
         admin: process.env.ADMIN_SITE_URL ?? 'http://localhost:4003/',
-        docs: process.env.DOCS_SITE_URL ?? 'http://localhost:7700/'
+        docs: process.env.DOCS_SITE_URL ?? 'http://localhost:7700/',
+        proxy: process.env.PROXY_URL ?? 'http://localhost:8000'
     }
 }
