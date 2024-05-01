929bc40a3f8242b1aea772ac382e5e9dc078f69f
TonProtofy <ton.miserachs@protofy.xyz>
2024-05-01 09:35:14 +0200

    [REFACTOR] Add new sdks to cores and config for each of those

--- packages/protolib/bundles/devices/devicecores/devicecoresAPI.ts
+++ packages/protolib/bundles/devices/devicecores/devicecoresAPI.ts
@@ -1,12 +1,57 @@
 import { DeviceCoreModel } from ".";
 import { AutoAPI } from '../../../api'
+import { API } from '../../../base'
 
 const initialData = {
     "esp32": {
         "id": "1",
         "name": "esp32",
-        "sdks": ["esphome"],
-        "config": {}
+        "sdks": ["esphome-arduino", "esphome-idf"],
+        "config": {
+            "esphome-arduino":{
+                "esphome":{},
+                "esp32":{
+                    "variant": "esp32",
+                    "framework": { 
+                        "type": "arduino"
+                    }
+                }
+            },
+            "esphome-idf":{
+                "esphome":{},
+                "esp32":{
+                    "variant": "esp32", 
+                    "framework": { 
+                        "type": "esp-idf"
+                    }
+                }
+            }
+        }
+    },
+    "esp32s3": {
+        "id": "2",
+        "name": "esp32s3",
+        "sdks": ["esphome-arduino", "esphome-idf"],
+        "config": {
+            "esphome-arduino":{
+                "esphome":{},
+                "esp32":{
+                    "variant": "esp32s3",
+                    "framework": { 
+                        "type": "arduino"
+                    }
+                }
+            },
+            "esphome-idf":{
+                "esphome":{},
+                "esp32":{
+                    "variant": "esp32s3", 
+                    "framework": { 
+                        "type": "esp-idf"
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -15,5 +60,19 @@ export const DeviceCoresAPI = AutoAPI({
     modelType: DeviceCoreModel,
     initialData,
     skipDatabaseIndexes: true,
-    prefix: '/adminapi/v1/'
+    prefix: '/adminapi/v1/',
+    transformers: {
+        getConfig: async (field, e, data) => {
+            const sdksData = await API.get("/adminapi/v1/devicesdks")
+            data.sdks.forEach(async (sdk) => {
+                const sdkData = sdksData.data.items.find((item) => item.name === sdk)
+                if (sdkData !== undefined) {
+                    if(!data.config) data.config = {}
+                    data.config[sdk] = sdkData?.config
+                }  
+            })
+            return data
+        }
+    }
+
 })
--- packages/protolib/bundles/devices/devicecores/devicecoresSchemas.ts
+++ packages/protolib/bundles/devices/devicecores/devicecoresSchemas.ts
@@ -3,7 +3,7 @@ import { AutoModel, Schema, z } from '../../../base'
 export const DeviceCoreSchema = Schema.object({
   name: z.string().hint("ESP32, AT-MEGA2560  ARMv7, Protofy, ...").static().id(),
   sdks: z.array(z.string()).hint("esphome, platformio, wled, javascript, ...").hidden(),
-  config: z.record(z.string(), z.string()).generate(() => { return {} })
+  config: z.record(z.string(), z.any()).optional().onCreate('getConfig').onUpdate('getConfig'),
 })
 export type DeviceCoreType = z.infer<typeof DeviceCoreSchema>;
 export const DeviceCoreModel = AutoModel.createDerived<DeviceCoreType>("DeviceCoreModel", DeviceCoreSchema);
