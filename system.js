c1cd230e084aafd41e55de47eb09a7ad7bb0f68a
jcarlosn <jcarlos.norte@gmail.com>
2024-05-01 15:05:41 +0200

    Fix service endpoints

--- system.js
+++ system.js
@@ -5,29 +5,28 @@ const config = {
         {
             "name": "admin",
             "description": "Admin panel to extend, customize and manage protofy",
-            "route": (url) => url.startsWith('/admin/'),
+            "route": (url) => url.startsWith('/admin/') || url == '/admin',
             "endpoint": process.env.ADMIN_SITE_URL ?? 'http://localhost:'+(isProduction ? 4003 : 3003),
             "priority": 100
         },
         {
             "name": "admin-api",
             "description": "Administration API services for protofy admin panel",
-            "route": (url) => url.startsWith('/adminapi/') || url == '/websocket',
+            "route": (url) => url.startsWith('/adminapi/') || url == '/websocket' || url == '/adminapi',
             "endpoint": process.env.ADMIN_API_URL ?? 'http://localhost:'+(isProduction ? 4002 : 3002),
             "priority": 100
         },
         {
             "name": "api",
             "description": "API services for protofy",
-            "route": (url) => url.startsWith('/api/'),
+            "route": (url) => url.startsWith('/api/') || url == '/api',
             "endpoint": process.env.API_URL ?? 'http://localhost:'+(isProduction ? 4001 : 3001),
             "priority": 100
         },
         {
             "name": "nextra",
-            "disabled": true,
             "description": "Documentation services for protofy, providing the documentation based on nextra",
-            "route": (url) => url.startsWith('/documentation/'),
+            "route": (url) => url.startsWith('/documentation/' || url == '/documentation'),
             "endpoint": process.env.DOCS_SITE_URL ?? 'http://localhost:'+(isProduction ? 7700 : 7600),
             "priority": 100
         },
