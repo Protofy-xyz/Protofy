19ffdcdc6bc4f10f4c26b40cb6dcf63d8361b8fc
MiquelCos <miquel@protofy.xyz>
2024-05-02 19:16:31 +0200

    [minor changes] updates subscribe molecule

--- packages/protolib/visualui/molecules/Subscribe.tsx
+++ packages/protolib/visualui/molecules/Subscribe.tsx
@@ -1,7 +1,7 @@
 import { Element } from "@protocraft/core";
 
 const Subscribe = (atoms) => {
-    return <Element is={atoms.basic.Container} alignItems="center" justifyContent="center" gap="$8">
+    return <Element is={atoms.basic.Container} alignItems="center" justifyContent="center" gap="$8" padding="$6">
         <Element is={atoms.text.Head3} fontSize="50px">
             Subscribe
         </Element>
@@ -9,7 +9,7 @@ const Subscribe = (atoms) => {
             Enter your email and be the first to recieve latest news
         </Element>
         <Element is={atoms.layout.HStack} alignItems="center" gap="$2">
-            <Element is={atoms.basic.Input} placeholder="Type your email" minWidth={350} f={3}></Element>
+            <Element is={atoms.basic.Input} state="cs.email" placeholder="Type your email" minWidth={350} f={3}></Element>
             <Element is={atoms.basic.ButtonSimple} f={1} size="$4">
                 subscribe
             </Element>
