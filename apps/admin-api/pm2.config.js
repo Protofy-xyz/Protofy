2db266cf8534ba8f4633054fb48f98d8596035bb
Noemí Blázquez <ap0calipse@gmail.com>
2024-05-07 00:42:11 +0200

    Change admin-api service name when running in full dev mode, to have some feedback
    Co-authored-by: jcarlosn <jcarlos.norte@gmail.com>

--- apps/admin-api/pm2.config.js
+++ apps/admin-api/pm2.config.js
@@ -1,13 +1,12 @@
 const isProduction = process.env.NODE_ENV === 'production';
 const isFullDev = process.env.DEV_ADMIN_API === '1';
-
 const path = require('path');
 const currentDir = path.dirname(__filename);
 
 module.exports = {
     apps: [
-        isFullDev ?{
-            name: 'admin-api-dev',
+        isFullDev ? {
+            name: 'admin-api-dev-fulldev',
             script: path.join(currentDir, '../../node_modules/ts-node/dist/bin.js'),
             args: '--files --project tsconfig.json src/index.ts',
             watch: false,
