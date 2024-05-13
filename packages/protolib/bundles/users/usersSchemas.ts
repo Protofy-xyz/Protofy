2b551bbe5b2b5655232ca23a129b1d80baf7d784
Noemí Blázquez <ap0calipse@gmail.com>
2024-05-13 18:05:26 +0200

    Fixes https://github.com/Protofy-xyz/Protofy/issues/87
    Co-authored-by: jcarlosn <jcarlos.norte@gmail.com>

--- packages/protolib/bundles/users/usersSchemas.ts
+++ packages/protolib/bundles/users/usersSchemas.ts
@@ -3,7 +3,7 @@ import moment from "moment";
 
 export const UserSchema = Schema.object({
     username: z.string().email().label('email').hint('user@example.com').static().id().search(),
-    type: z.string().min(1).label("group").hint('user, admin, ...').search().help("The type refers to a group name. Groups contains privileges (admin true/false) and workspaces.").indexed(),
+    type: z.string().min(1).label("group").hint('user, admin, ...').search().generate('user').help("The type refers to a group name. Groups contains privileges (admin true/false) and workspaces.").indexed(),
     password: z.string().min(6).hint('**********').secret().onCreate('cypher').onUpdate('update').onRead('clearPassword').onList('clearPassword').help("Salted hashed password using bcrypt."),
     permissions: z.array(z.string()).optional().label("additional permissions"),
     createdAt: z.string().min(1).generate((obj) => moment().toISOString()).search().hidden().indexed(),
