6c47ee59e0045d6db62f153d25106aee5f3e5e93
jcarlosn <jcarlos.norte@gmail.com>
2024-06-06 16:37:57 +0200

    Fix rainbowtext size (landing page broken)

--- packages/protolib/components/RainbowText.tsx
+++ packages/protolib/components/RainbowText.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { SizableText, Text, TextProps } from "tamagui";
 
 export default React.forwardRef(({rainbowType, ...props}: TextProps & {rainbowType?: string}, ref:any) => (
-    <SizableText ref={ref} {...props}>
-        <div className={(rainbowType??"rainbow")+" clip-text"} style={{display: 'inline-block'}}>{props.children}</div>
-    </SizableText>
+    <Text ref={ref} {...props}>
+        <div className={(rainbowType??"soft")+" clip-text"} style={{display: 'inline-block'}}>{props.children}</div>
+    </Text>
 ))
