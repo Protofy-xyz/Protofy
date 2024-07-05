b14793304c8a377f22765113670ab735dae9c262
jcarlosn <jcarlos.norte@gmail.com>
2024-07-05 12:20:40 +0200

    Refactor imports

--- packages/app/features/profile.tsx
+++ packages/app/features/profile.tsx
@@ -1,6 +1,7 @@
 import { DefaultLayout } from '../layout/DefaultLayout'
 import { H2, YStack } from 'tamagui'
-import { useSession, Page} from 'protolib'
+import { useSession} from 'protolib/lib/Session'
+import {Page} from 'protolib/components/Page'
 
 export function Profile({pageSession}) {
   const [session] = useSession(pageSession)
