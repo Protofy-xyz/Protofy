14fa942aa766371e2e39f342d248a6ef55f36f84
jcarlosn <jcarlos.norte@gmail.com>
2024-07-11 01:41:00 +0200

    Fix logger problems because of logger utilites extracted into a separated and precompiled package, combined with module-level variable acting as a logger cache.

--- packages/protobase/src/logger.ts
+++ packages/protobase/src/logger.ts
@@ -1,21 +1,17 @@
 import pino from 'pino';
 import { getConfig } from './Config';
 
-var loggers = {};
-
 export const getLogger = (logId='default', userConfig?) => {
   const config = userConfig ?? getConfig()
-  if (!loggers[logId]) {
-    loggers[logId] = pino(config.logger)
-  }
+  const logger  = pino(config.logger)
   
   //wrapper for pino, allowing to have a third parameter id, to correlate messages with specific code nodes
   return {
-    error: (data, msg?, id?) => loggers[logId].error(data, msg),
-    fatal: (data, msg?, id?) => loggers[logId].fatal(data, msg),
-    warn: (data, msg?, id?) => loggers[logId].warn(data, msg),
-    info: (data, msg?, id?) => loggers[logId].info(data, msg),
-    debug: (data, msg?, id?) => loggers[logId].debug(data, msg),
-    trace: (data, msg?, id?) => loggers[logId].trace(data, msg),
+    error: (data, msg?, id?) => logger.error(data, msg),
+    fatal: (data, msg?, id?) => logger.fatal(data, msg),
+    warn: (data, msg?, id?) => logger.warn(data, msg),
+    info: (data, msg?, id?) => logger.info(data, msg),
+    debug: (data, msg?, id?) => logger.debug(data, msg),
+    trace: (data, msg?, id?) => logger.trace(data, msg),
   }
 }
