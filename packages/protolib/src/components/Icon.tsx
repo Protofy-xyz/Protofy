b8b4f84de4ac2bfb0e5a1f359b7568391821101d
jcarlosn <jcarlos.norte@gmail.com>
2024-07-16 03:13:15 +0200

    Disabled complete import from icon library. This creates a HUGE bundle with all the icons, and it heavily increases compile times (from 40s to 90s in my computer)
    Components should never import entire icon libraries. If this is needed, we need to find a better way for this.

--- packages/protolib/src/components/Icon.tsx
+++ packages/protolib/src/components/Icon.tsx
@@ -1,7 +1,9 @@
-import { icons } from '@tamagui/lucide-icons';
+// import { icons } from '@tamagui/lucide-icons';
 import { forwardRef } from 'react';
 
 export const Icon = forwardRef(({ name, color, size }: {name: string, color?: string, size?: number | string}, ref) => {
-    const LucideIcon = icons[name] ?? icons['Component'];
-    return <LucideIcon ref={ref} color={color} size={size} />;
+    // const LucideIcon = icons[name] ?? icons['Component'];
+    // return <LucideIcon ref={ref} color={color} size={size} />;
+
+    return <></>
 });
