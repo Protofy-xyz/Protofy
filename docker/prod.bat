d9024451a3e01b979008d90e2b03248cb03680e7
gdomenech98 <gerard@protofy.xyz>
2024-07-23 10:55:58 +0000

    adjusted prod scripts to run with "--service" flag and run as service

--- docker/prod.bat
+++ docker/prod.bat
@@ -1,3 +1,25 @@
 setlocal enabledelayedexpansion
 CALL .\helpers\get-envs.bat --prod || (echo Failed to run helpers\get-envs.bat & exit /b)
-%DOCKER_COMPOSE_CMD% -p protofy %SERVICES_COMPOSE_FILES% up
+set SERVICE=false
+
+:loop
+if "%~1"=="" goto afterloop
+if "%~1"=="--service" (
+    set SERVICE=true
+)
+shift
+goto loop
+:afterloop
+
+
+if "%SERVICE%"=="true" (
+    set COMMAND=%DOCKER_COMPOSE_CMD% -p protofy %SERVICES_COMPOSE_FILES% up -d && call logs.bat
+) else (
+    set COMMAND=%DOCKER_COMPOSE_CMD% -p protofy %SERVICES_COMPOSE_FILES% up
+)
+
+echo Running as service: %SERVICE%
+echo Executing command: %COMMAND%
+
+CALL %COMMAND%
+
