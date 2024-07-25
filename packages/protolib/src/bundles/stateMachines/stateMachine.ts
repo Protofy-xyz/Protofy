01a9ed0926661af03cfa5b71f8585182e7b036a1
noreplydev <contact.cristiansanchez@gmail.com>
2024-07-25 19:26:23 +0200

    Add message broadcast and machine instance inspection to the state machines api

--- packages/protolib/src/bundles/stateMachines/stateMachine.ts
+++ packages/protolib/src/bundles/stateMachines/stateMachine.ts
@@ -60,4 +60,26 @@ export class StateMachine {
         this.running = false
         return true
     }
+
+    emit(emitType: string, data: any) {
+        if (!this.actor) {
+            console.log("Cannot change machine instance state while it is stopped.")
+            return false
+        }
+
+        this.actor.send({
+            type: emitType,
+            ...data
+        })
+        return true
+    }
+
+    inspect() {
+        if (!this.actor) {
+            console.log("Cannot change machine instance state while it is stopped.")
+            return null
+        }
+
+        return this.actor.getSnapshot()
+    }
 }
--- packages/protolib/src/bundles/stateMachines/stateMachinesApi.ts
+++ packages/protolib/src/bundles/stateMachines/stateMachinesApi.ts
@@ -54,6 +54,44 @@ export const StateMachinesAPI = (app, context) => {
     }
   })
 
+  // change instance state
+  app.get("/adminapi/v1/statemachines/:instanceName/emit", checkMachineInstanceName, async (req, res) => {
+    const instanceName = req.params.instanceName
+    const { emitType, payload } = req.body ?? {}
+
+    if (!emitType) {
+      return res.status(400).json({ status: "Bad request. Required emitType." })
+    }
+
+    try {
+      if (runtimeMachines[instanceName].emit(emitType, payload ?? {})) {
+        return res.status(200).json({ status: "Machine message emitted", [instanceName]: runtimeMachines[instanceName] })
+      } else {
+        return res.status(400).json({ status: "Machine instance is not started", [instanceName]: runtimeMachines[instanceName] })
+      }
+    } catch (e) {
+      console.error("Cannot emit message to machine instance")
+      return res.status(500).json({ status: "Cannot emit message to machine instance" })
+    }
+  })
+
+  // debugging options
+  app.get("/adminapi/v1/statemachines/:instanceName/inspect", checkMachineInstanceName, async (req, res) => {
+    const instanceName = req.params.instanceName
+
+    try {
+      const machineInspectionData = runtimeMachines[instanceName].inspect()
+      if (machineInspectionData) {
+        return res.status(200).json({ status: "Ok", [instanceName]: machineInspectionData })
+      } else {
+        return res.status(400).json({ status: "Machine instance is not started", [instanceName]: runtimeMachines[instanceName] })
+      }
+    } catch (e) {
+      console.error("Cannot inspect machine instance")
+      return res.status(500).json({ status: "Cannot inspect machine instance" })
+    }
+  })
+
   // generate machine instance from definition
   app.get("/adminapi/v1/statemachines/:definitionName/:instanceName", async (req, res) => {
     const { definitionName, instanceName } = req.params
