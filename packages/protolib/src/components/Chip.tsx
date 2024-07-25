2c9fb699d361f62d00c5ae6f7746d034d910d04c
MiquelCos <miquel@protofy.xyz>
2024-07-25 17:32:34 +0200

    Adds support to remove single filter

--- packages/protolib/src/components/Chip.tsx
+++ packages/protolib/src/components/Chip.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {XStack, SizableText, ColorProp, Spinner, Spacer, XStackProps} from 'tamagui'
+import {XStack, SizableText, ColorProp, Spinner, Spacer, XStackProps, TextProps} from 'tamagui'
 
-export const Chip = ({ loading, icon, text, bold, color, children, ...props}:{loading?:boolean, icon?:any, text?: string, bold?: boolean, color?: ColorProp} & XStackProps) => {
+export const Chip = ({ loading, icon, text, bold, color, children, textProps, ...props}:{loading?:boolean, icon?:any, text?: string, bold?: boolean, color?: ColorProp, textProps: TextProps} & XStackProps) => {
     return <XStack ai="center" jc="center" bc={color??'$color5'} p={2} px={"$3"} br={25} {...props}>
             {icon && React.cloneElement(icon, { size:20, strokeWidth: 0.7, color: 'var(--color)' })}
             {children}
-            {text && <SizableText ml={icon?"$2":"$0"} o={0.97} fontWeight={bold?"900":"400"} size="$1" color="$color" >{text}</SizableText>}
+            {text && <SizableText ml={icon?"$2":"$0"} o={0.97} fontWeight={bold?"900":"400"} size="$1" color="$color" {...textProps}>{text}</SizableText>}
             {loading && <><Spacer size={9} /><Spinner color={"var(--color8)"} scale={0.7} size='small' /></>}
         </XStack>
 }
