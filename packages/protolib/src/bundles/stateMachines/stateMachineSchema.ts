9c9ba21dfcb0709548df64d131d0c508c629655a
noreplydev <contact.cristiansanchez@gmail.com>
2024-07-29 15:52:00 +0200

    Add composition between StateMachine and StateMachineDefinition models

--- packages/protolib/src/bundles/stateMachines/stateMachineSchema.ts
+++ packages/protolib/src/bundles/stateMachines/stateMachineSchema.ts
@@ -1,7 +1,9 @@
 import { AutoModel, Schema, z } from 'protobase'
+import { StateMachineDefinitionModel } from './stateMachineDefinitions/stateMachineDefinitionSchema';
 
 export const StateMachineSchema = Schema.object({
     name: Schema.string().id().static(),
+    definition: StateMachineDefinitionModel.linkTo((data) => data.name).hint("Select your machine definition"),
 })
 
 export type StateMachineType = z.infer<typeof StateMachineSchema>;
