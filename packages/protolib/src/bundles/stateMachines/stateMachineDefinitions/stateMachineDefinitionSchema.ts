1bb41430fcdf6443f88f205454173032f0cf9cf5
noreplydev <contact.cristiansanchez@gmail.com>
2024-07-29 19:52:06 +0200

    Update StateMachineDefinition domain object schema

--- packages/protolib/src/bundles/stateMachines/stateMachineDefinitions/stateMachineDefinitionSchema.ts
+++ packages/protolib/src/bundles/stateMachines/stateMachineDefinitions/stateMachineDefinitionSchema.ts
@@ -4,7 +4,6 @@ export const StateMachineDefinitionSchema = Schema.object({
     name: z.string().id().static().search(),
     context: Schema.record(z.string(), z.any()),
     states: Schema.array(z.string()),
-    transitions: Schema.record(z.string(), z.record(z.string(), z.string())),
 })
 
 export type StateMachineDefinitionType = z.infer<typeof StateMachineDefinitionSchema>;
