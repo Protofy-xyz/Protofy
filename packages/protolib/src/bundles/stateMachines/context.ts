cb3afcc468363fad7a025a5e708ea49e9985d7f3
noreplydev <contact.cristiansanchez@gmail.com>
2024-07-30 17:29:21 +0200

    Refactor stateMachines apiContext functions

--- packages/protolib/src/bundles/stateMachines/context.ts
+++ packages/protolib/src/bundles/stateMachines/context.ts
@@ -1,32 +1,49 @@
 import { getServiceToken } from "../apis/context"
 import { API } from 'protobase'
 
-const spawnStateMachine = async (definitionName, instaceName, errorCb?) => {
+
+const spawnStateMachine = async (options: {
+    definitionName: string, 
+    instanceName: string, 
+    doneCb?: (result) => void, 
+    errorCb?: (err) => void
+}) => {
+    const {definitionName, instanceName, doneCb, errorCb} = options
+
     const url = `/adminapi/v1/statemachines?token=${getServiceToken()}`
     let result = await API.post(url, {
-        name: instaceName,
+        name: instanceName,
         definition: {
             name: definitionName
         }
     })
     if (result.isError) {
-        errorCb && errorCb()
+        errorCb && errorCb(result.error)
         throw result.error
     }
 
     console.log('State machine created: ', result.data)
+    doneCb && await doneCb(result.data)
     return result.data
 }
 
-const emitToStateMachine = async (instaceName, emitType, payload = {}, errorCb?) => {
-    const url = `/adminapi/v1/statemachines/${instaceName}/emit?token=${getServiceToken()}`
+const emitToStateMachine = async (options: {
+    instanceName: string,
+    emitType: string, 
+    payload?: object,
+    doneCb?: (result) => void
+    errorCb?: (err) => void
+}) => {
+    const {instanceName, emitType, payload, doneCb, errorCb} = options
+    const url = `/adminapi/v1/statemachines/${instanceName}/emit?token=${getServiceToken()}`
     let result = await API.post(url, { emitType, payload })
     if (result.isError) {
-        errorCb && errorCb()
+        errorCb && errorCb(result.error)
         throw result.error
     }
 
-    console.log("Emited to '" + instaceName + "' state machine: ", result.data)
+    console.log("Emited to '" + instanceName + "' state machine: ", result.data)
+    doneCb && await doneCb(result.data)
     return result.data
 }
 
