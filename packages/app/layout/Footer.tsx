ec2e34ee7d7f60fb7f2f8b158b3fb31d96676118
MiquelCos <miquel@protofy.xyz>
2024-08-01 13:01:40 +0200

    Updates app footer

--- packages/app/layout/Footer.tsx
+++ packages/app/layout/Footer.tsx
@@ -1,41 +1,91 @@
 import { NextLink } from 'protolib/components/NextLink'
 import { Logo } from 'protolib/components/Logo'
 import { Footer as ProtoFooter, FooterElement } from 'protolib/components/layout/Footer'
+import { DiscordIcon } from 'protolib/components/icons/DiscordIcon'
+import { GithubIcon } from 'protolib/components/icons/GithubIcon'
 import React from 'react'
-import { Paragraph } from 'tamagui'
+import { Instagram, Linkedin } from 'lucide-react'
+import { XStack, Separator, Text, TextProps } from '@my/ui'
 
-export const Footer = () => <ProtoFooter>
+const FooterText = (props: TextProps) => <Text fontWeight={200} {...props} />
+export const Footer = () => <ProtoFooter
+  separator={true}
+  bottom={<>
+    <Separator pt="$2"></Separator>
+    <XStack gap="$4" flexWrap="wrap" f={1} py="$2" px="$4" jc='space-between' $sm={{ jc: 'center' }} ai="center">
+      <NextLink href="/cookies" aria-label="Cookie policy">
+        <FooterText $sm={{ ta: 'center' }} width="300px" color="$color10">
+          Cookie policy
+        </FooterText>
+      </NextLink>
+      <FooterText width="300px" ta="right" $sm={{ ta: 'center' }}>
+        made with love
+      </FooterText>
+    </XStack>
+  </>}
+>
   <FooterElement
-    flex={2}
-    mt="$-1"
-    mb="$2"
-    space="$4"
+    gap="5px"
+    width="300px"
   >
-    <NextLink href="/" aria-label="Homepage">
-      <Logo text='Protofy' />
-    </NextLink>
-    <Paragraph mt="$2" size="$3">
-      Made with love
-    </Paragraph>
+    <FooterText>
+      hello@protofy.xyz
+    </FooterText>
+    <FooterText>
+      +34 935 16 51 44
+    </FooterText>
+    <FooterText>
+      C/Ramón Turró 100, 1º 1ª
+    </FooterText>
+    <FooterText>
+      Barcelona, Spain
+    </FooterText>
   </FooterElement>
-
-  <FooterElement title="Overview" links={[
-    { href: '#', caption: 'Introduction' },
-    { href: '#', caption: 'Design Principles' },
-    { href: '#', caption: 'Architecture' }
-  ]} />
-
-  <FooterElement title="Links" links={[
-    { href: 'https://github.com/Protofy-xyz/Protofy/blob/main/README.md', caption: 'Installation' },
-    { href: 'https://github.com/Protofy-xyz/Protofy/issues', caption: 'Issues' },
-    { href: 'https://github.com/Protofy-xyz/Protofy/pulls', caption: 'Pull requests' }
-  ]} />
-
-  <FooterElement title="Community" links={[
-    { href: '/community', caption: 'Community' },
-    { href: '/', caption: 'Site' },
-    { href: 'https://github.com/Protofy-xyz/Protofy', caption: 'GitHub' },
-    { href: 'https://twitter.com/protofy_xyz', caption: 'Twitter' },
-    { href: 'https://discord.gg/VpeZxMFfYW', caption: 'Discord' }
-  ]} />
+  <FooterElement
+    gap="5px"
+    width="300px"
+    alignItems='center'
+  >
+    <NextLink href="https://www.protofy.xyz/" aria-label="Platform">
+      <FooterText fontWeight={400}>
+        Platform
+      </FooterText>
+    </NextLink>
+    <NextLink href="https://www.protofy.xyz/join" aria-label="Cloud">
+      <FooterText fontWeight={400}>
+        Cloud
+      </FooterText>
+    </NextLink>
+    <NextLink href="https://projects.protofy.xyz/" aria-label="Projects">
+      <FooterText fontWeight={400}>
+        Projects
+      </FooterText>
+    </NextLink>
+  </FooterElement>
+  <FooterElement
+    gap="5px"
+    alignItems='flex-end'
+    width="300px"
+  >
+    <FooterText>
+      © 2024 | All rights reserved
+    </FooterText>
+    <FooterText>
+      Protofy
+    </FooterText>
+    <XStack mt="$4" gap="$4" ai="center">
+      <NextLink href="https://github.com/Protofy-xyz/Protofy" aria-label="Github">
+        <GithubIcon width={24} />
+      </NextLink>
+      <NextLink href="https://discord.gg/VpeZxMFfYW" aria-label="Discord">
+        <DiscordIcon width={24} />
+      </NextLink>
+      <NextLink href="https://www.instagram.com/protofy.xyz/" aria-label="Instagram">
+        <Instagram color={"var(--color)"} size={24} />
+      </NextLink>
+      <NextLink href="https://es.linkedin.com/company/protofy-xyz" aria-label="Linkedin">
+        <Linkedin color={"var(--color)"} size={24} />
+      </NextLink>
+    </XStack>
+  </FooterElement>
 </ProtoFooter>
