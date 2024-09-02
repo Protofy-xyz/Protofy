aa45d1c3bd3302a05c8254679af0e7bb02f70d7a
Noemí Blázquez <ap0calipse@gmail.com>
2024-09-02 20:31:41 +0200

    Support array and boolean type in simpledatatable

--- packages/protolib/src/components/SimpleDataTable.tsx
+++ packages/protolib/src/components/SimpleDataTable.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { YStack, Text, XStack } from '@my/ui';
+import { Chip } from './Chip';
+import { CheckCircle, XCircle } from '@tamagui/lucide-icons';
 
 interface SimpleDataTableProps {
     columns: Array<{ name: string; selector: (row: any) => React.ReactNode }>;
     data: any[];
+    maxHeight?: number;
 }
 
-export const SimpleDataTable: React.FC<SimpleDataTableProps> = ({ columns, data }) => {
+export const SimpleDataTable: React.FC<SimpleDataTableProps> = ({ columns, data, maxHeight }) => {
     return (
         <YStack>
-            <XStack paddingBottom={5} borderBottomWidth={1} borderBottomColor="$borderColor">
+            <XStack padding={10} borderBottomWidth={1} borderBottomColor="$borderColor">
                 {columns.map((col, index) => (
                     <Text 
                         key={index} 
@@ -22,22 +25,47 @@ export const SimpleDataTable: React.FC<SimpleDataTableProps> = ({ columns, data 
                 ))}
             </XStack>
 
-            {data.map((row, rowIndex) => (
-                <XStack 
-                    key={rowIndex} 
-                    padding={10}
-                >
-                    {columns.map((col, colIndex) => (
-                        <Text 
-                            key={colIndex} 
-                            color="$primary"
-                            style={{ textAlign: 'left', flex: 1 }}
-                        >
-                            {col.selector(row)}
-                        </Text>
-                    ))}
-                </XStack>
-            ))}
+            <YStack 
+                style={{ 
+                    maxHeight: maxHeight || 300, 
+                    overflowY: 'auto'
+                }}
+            >
+                {data.map((row, rowIndex) => (
+                    <XStack 
+                        key={rowIndex} 
+                        padding={10} 
+                    >
+                        {columns.map((col, colIndex) => (
+                            <Text 
+                                key={colIndex} 
+                                color="$primary"
+                                style={{ textAlign: 'left', flex: 1 }}
+                            >
+                                {Array.isArray(col.selector(row)) ? (
+                                    <XStack space={5}>
+                                        {col.selector(row).map((item, chipIndex) => (
+                                            <Chip key={chipIndex} color="$color5">
+                                                {item}
+                                            </Chip>
+                                        ))}
+                                    </XStack>
+                                ) : (
+                                    typeof col.selector(row) === 'boolean' ? (
+                                        col.selector(row) ? (
+                                            <CheckCircle color="$color9" size={20} />
+                                        ) : (
+                                            <XCircle color="red" size={20} />
+                                        )
+                                    ) : (
+                                        col.selector(row)
+                                    )
+                                )}
+                            </Text>
+                        ))}
+                    </XStack>
+                ))}
+            </YStack>
         </YStack>
     );
 };
