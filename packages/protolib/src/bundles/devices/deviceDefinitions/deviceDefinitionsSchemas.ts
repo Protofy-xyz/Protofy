9cbdfaacb1671d6b32f28c73a6374006b6ba9885
lluis@protofy.xyz <lluis@protofy.xyz>
2024-09-03 12:48:28 +0200

    Add optional to device Config in order to be resilient if the device doesn't have configuration

--- packages/protolib/src/bundles/devices/deviceDefinitions/deviceDefinitionsSchemas.ts
+++ packages/protolib/src/bundles/devices/deviceDefinitions/deviceDefinitionsSchemas.ts
@@ -7,7 +7,7 @@ export const DeviceDefinitionSchema = Schema.object({
   sdk: z.string().hidden(),
   subsystems: z.record(z.string(), z.any()).optional().hidden(),
   environment: z.string().optional().help("The environment where the definition was created").hidden().groupIndex("env"),
-  config: z.record(z.string(), z.any()).onCreate('getConfig').onUpdate('getConfig'),
+  config: z.record(z.string(), z.any()).onCreate('getConfig').onUpdate('getConfig').optional(),
 })
 export type DeviceDefinitionType = z.infer<typeof DeviceDefinitionSchema>;
 export const DeviceDefinitionModel = AutoModel.createDerived<DeviceDefinitionType>("DeviceDefinitionModel", DeviceDefinitionSchema);
