ddaffcb9db17b1ea2c22d887d443afca408f1448
Noemí Blázquez <ap0calipse@gmail.com>
2024-09-17 20:51:46 +0200

    Split PanelLayout and added support to toggle bottom panel via prop

--- packages/protolib/src/components/layout/PanelLayout.tsx
+++ packages/protolib/src/components/layout/PanelLayout.tsx
@@ -1,7 +1,8 @@
 import { YStack, ScrollView, XStack, getTokens } from 'tamagui'
 import { AppBar } from '../AppBar'
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 
-export const PanelLayout = ({ panelBgColor = undefined, menuContent, children, SideMenu, Layout, headerContents, HeaderMenu }) => {
+export const PanelLayout = ({ panelBgColor = undefined, menuContent, children, SideMenu, Layout, headerContents, HeaderMenu, panelBottom = true }) => {
   const appBarHeight = 55
   const bgPanels = '$bgPanel'
   const _panelBgColor = '$bgContent'
@@ -30,19 +31,35 @@ export const PanelLayout = ({ panelBgColor = undefined, menuContent, children, S
       }>
       {/* <Theme name={tint as any}> */}
       <XStack f={1} p="$3" pl={0} bc={bgPanels}>
-        <XStack bc={bgPanels} elevation={3} br={"$6"} mt={appBarHeight} f={1} height={'calc(100vh - ' + (appBarHeight + 30) + 'px)'}>
-          <ScrollView $sm={{ br: "$0" }} height={'calc(100vh - ' + (appBarHeight + 30) + 'px)'}>
-            {/* <Tinted> */}
-            <YStack br={"$6"} bc={panelBgColor ?? _panelBgColor} f={1} minHeight={'calc(100vh - ' + (appBarHeight + 30) + 'px)'} flex={1}>
-              {/* <Theme reset> */}
-              {children}
-              {/* </Theme> */}
-
-            </YStack>
-            {/* </Tinted> */}
-          </ScrollView>
-        </XStack>
-        {/* </Theme> */}
+        <PanelGroup direction="vertical" style={{ height: '100%', width: '100%' }}>
+          <Panel>
+            <div style={{ display: 'flex', flexDirection: 'column', flex: 1, height: '100%', overflow: 'auto' }}>
+              <XStack bc={"bgPanels"} elevation={3} br={"$6"} mt={appBarHeight} f={1} paddingBottom={panelBottom ? '20px' : '0px'} style={{ flex: 1 }}>
+                <ScrollView f={1} $sm={{ br: "$0" }} style={{
+                  flexGrow: 1,
+                  overflowY: 'auto',
+                  display: 'flex',
+                  flexDirection: 'column'
+                }}
+                  contentContainerStyle={{ flexGrow: 1 }}
+                >
+                  <YStack f={1} br={"$6"} bc={panelBgColor ?? _panelBgColor} style={{ flex: 1, overflowY: 'auto' }}>
+                    {children}
+                  </YStack>
+                </ScrollView>
+              </XStack>
+            </div>
+          </Panel>
+          {panelBottom && <><PanelResizeHandle>
+            <div style={{ backgroundColor: '#ccc', width: '100%', height: '4px', cursor: 'row-resize' }}></div>
+          </PanelResizeHandle>
+            <Panel>
+              <div style={{ display: 'flex', flex: 1, height: '100%', backgroundColor: 'grey' }}>
+                <p>Contenido del panel inferior</p>
+              </div>
+            </Panel>
+          </>}
+        </PanelGroup>
       </XStack>
     </Layout>
   )
