5bdd2f6d2a8eb0509d42bfef374860dbea1fc4d3
TonProtofy <ton.miserachs@protofy.xyz>
2024-09-27 12:55:26 +0200

    Flush stdout after protopy print statements

--- packages/protopy/events.py
+++ packages/protopy/events.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import json
+import sys
 
 def pubEvent(host, token, event, secure_connection):
     protocol = "http" if secure_connection == False else "https"
@@ -12,6 +13,7 @@ def pubEvent(host, token, event, secure_connection):
     body = json.dumps(event)
     body = re.sub(r'(?<!")(\w+)(?!"):', r'"\1":', body)
     print("[EVENT] info: ", body)
+    sys.stdout.flush()
 
     try:
         # response = requests.post(url, json=body, headers=headers)
@@ -20,9 +22,13 @@ def pubEvent(host, token, event, secure_connection):
         if (response.status_code != 200):
             print("[EVENT] Cannot post event to Protofy")
             print("  '- ", response.text)
+            sys.stdout.flush()
             return
 
         print("[EVENT] Succesfully sent event to Protofy")
+        sys.stdout.flush()
+
         return response.json()
     except Exception as e:
         print("[EVENT] Cannot send event to Protofy: ", e)
+        sys.stdout.flush()
