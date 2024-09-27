bae5a494b108883387f29a7a801e49719d810790
Noemi Blazquez <ap0calipse@gmail.com>
2024-09-27 16:28:25 +0200

    Change ChatWidget UI

--- packages/protolib/src/components/ChatWidget.tsx
+++ packages/protolib/src/components/ChatWidget.tsx
@@ -1,34 +1,29 @@
-import { useState } from "react";
-import {Sparkles, X } from '@tamagui/lucide-icons';
-import { YStack, Button, Dialog, DialogTrigger, DialogContent, XStack } from 'tamagui';
+import { useState } from "react"
+import { Sparkles, X } from '@tamagui/lucide-icons'
+import { YStack, Button, Dialog, DialogContent } from 'tamagui'
 
 export const ChatWidget = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false)
 
   const toggleChat = () => {
-    setIsOpen(!isOpen);
-  };
+    setIsOpen(!isOpen)
+  }
 
   return (
     <YStack>
       <Button
         position="fixed"
-        bottom={20}
-        right={20}
-        size="$5"
+        bottom={50}
+        right={50}
+        size="$4"
         circular
-        icon={<Sparkles size={"$2"} />}
+        icon={isOpen ? <X size={"$2"} /> : <Sparkles size={"$2"} />}
         onPress={toggleChat}
         zIndex={1000}
         elevation="$5"
-      >
-      </Button>
-
+      />
       {isOpen && (
         <Dialog open={isOpen} onOpenChange={setIsOpen}>
-          <DialogTrigger asChild>
-            <Button>Open Chat</Button>
-          </DialogTrigger>
           <DialogContent
             width="500px"
             height="600px"
@@ -39,10 +34,6 @@ export const ChatWidget = () => {
             borderRadius="$4"
             elevation="$6"
           >
-            <XStack justifyContent="flex-end">
-              <Button size="$2" onPress={toggleChat}>Close</Button>
-            </XStack>
-            
             <iframe
               src="/chatbot/"
               width="100%"
@@ -54,7 +45,7 @@ export const ChatWidget = () => {
         </Dialog>
       )}
     </YStack>
-  );
+  )
 }
 
 export default ChatWidget
