3deb09b6278af2b5383c8421d85d1850c93fc2a0
MiquelCos <miquel@protofy.xyz>
2024-10-02 18:28:09 +0200

    Adds get last event

--- packages/protolib/src/bundles/events/api/index.ts
+++ packages/protolib/src/bundles/events/api/index.ts
@@ -1,5 +1,6 @@
 import { getServiceToken } from 'protonode'
 import { generateEvent } from "../eventsLibrary"
+import { API } from "protobase";
 
 export const onEvent = (mqtt, context, cb, path?, from?) => {
     context.topicSub(mqtt, 'notifications/event/create/#', (async (message: string, topic: string) => {
@@ -27,4 +28,26 @@ export const emitEvent = (path, from, user, payload, ephemeral = false) => {
         payload,
         ephemeral
     }, getServiceToken())
+}
+
+export const getLastEvent = async (eventFilter?: { path?: string, from?: string, user?: string }) => {
+    const token = getServiceToken()
+
+    const userUrl = eventFilter.user ? `&filter[user]=${eventFilter.user}` : ""
+    const pathUrl = eventFilter.path ? `&filter[path]=${eventFilter.path}` : ""
+    const from = eventFilter.from ? `&filter[from]=${eventFilter.from}` : ""
+    const env = process.env.NODE_ENV == 'development' ? 'dev' : 'prod'
+    const urlLastEvent = `/adminapi/v1/events?env=${env}${from}${userUrl}${pathUrl}&itemsPerPage=1&token=${token}&orderBy=created&orderDirection=desc`
+
+    let result = await API.get(urlLastEvent)
+
+    if (result.isError) {
+        console.error(result.error)
+        return
+    }
+
+    const event = result.data?.items[0]
+
+    if (!event) return
+    return event
 }
