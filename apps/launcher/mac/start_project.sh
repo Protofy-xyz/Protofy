579cfb09ee3913e8dad878ec6ac853aecf2d61fd
MiquelCos <miquel@protofy.xyz>
2024-10-02 12:19:15 +0200

    [WIP] removes launcher app

--- apps/launcher/mac/start_project.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Change to project directory
-cd /Users/miquelcos/Protofy
-
-echo "Running yarn start..."
-/usr/local/bin/yarn start   # Make sure yarn is in the correct path
-echo "yarn start finished"
