001a97ab88940b5397bc8b9eb44fecd6828bd0d5
noreplydev <contact.cristiansanchez@gmail.com>
2024-10-23 11:56:09 +0200

    Add actions handling method to the js agent client

--- packages/agents/js/index.ts
+++ packages/agents/js/index.ts
@@ -65,57 +65,56 @@ export class ProtoMqttAgent {
         this.__consumerCallbacksChecker('monitor_pub', value);
     }
 
-    // handle(subsystemName, actionName, handler) {
-    //     const subsystem = this.subsystems.find(s => s.name === subsystemName);
-    //     if (!subsystem) {
-    //         throw new Error(`Subsystem '${subsystemName}' not found.`);
-    //     }
+    handle(subsystemName, actionName, handler) {
+        const subsystem = this.subsystems.find(s => s.name === subsystemName);
+        if (!subsystem) {
+            throw new Error(`Subsystem '${subsystemName}' not found.`);
+        }
 
-    //     const action = subsystem.actions.find(a => a.name === actionName);
-    //     if (!action) {
-    //         throw new Error(`Action '${actionName}' not found in subsystem '${subsystemName}'.`);
-    //     }
+        const action = subsystem.actions.find(a => a.name === actionName);
+        if (!action) {
+            throw new Error(`Action '${actionName}' not found in subsystem '${subsystemName}'.`);
+        }
 
-    //     if (!this.subsystemsHandlers[subsystemName]) {
-    //         this.subsystemsHandlers[subsystemName] = {};
-    //     }
+        if (!this.subsystemsHandlers[subsystemName]) {
+            this.subsystemsHandlers[subsystemName] = {};
+        }
 
-    //     this.subsystemsHandlers[subsystemName][actionName] = handler;
+        this.subsystemsHandlers[subsystemName][actionName] = handler;
 
-    //     // Register action subscriber
-    //     const topic = genActionEndpoint(this.name, subsystemName, actionName);
-    //     this.client.subscribe(topic, (err) => {
-    //         if (err) {
-    //             console.error(`Failed to subscribe to topic: ${topic}`, err);
-    //         } else {
-    //             console.log(`Subscribed to topic: ${topic}`);
-    //         }
-    //     });
+        // Register action subscriber
+        const topic = genActionEndpoint(this.name, subsystemName, actionName);
+        this.client.subscribe(topic, (err) => {
+            if (err) {
+                console.error(`Failed to subscribe to topic: ${topic}`, err);
+            } else {
+                console.log(`Subscribed to topic: ${topic}`);
+            }
+        });
 
-    //     // Register the message handler
-    //     this.__onMessage();
+        // Register the message handler
+        this.__onMessage();
+        console.log(`Handler assigned for action '${actionName}' in subsystem '${subsystemName}'`);
+    }
 
-    //     console.log(`Handler assigned for action '${actionName}' in subsystem '${subsystemName}'`);
-    // }
+    __onMessage() {
+        this.client.on('message', (topic, message) => {
+            const payload = message.toString();
+            const handlers = this.subsystemsHandlers;
 
-    // __onMessage() {
-    //     this.client.on('message', (topic, message) => {
-    //         const payload = message.toString();
-    //         const handlers = this.subsystemsHandlers;
+            for (const [subsystemName, actions] of Object.entries(handlers)) {
+                for (const [actionName, handler] of Object.entries(actions)) {
+                    const expectedTopic = genActionEndpoint(this.name, subsystemName, actionName);
+                    if (topic === expectedTopic) {
+                        handler(payload);
+                        return;
+                    }
+                }
+            }
 
-    //         for (const [subsystemName, actions] of Object.entries(handlers)) {
-    //             for (const [actionName, handler] of Object.entries(actions)) {
-    //                 const expectedTopic = genActionEndpoint(this.name, subsystemName, actionName);
-    //                 if (topic === expectedTopic) {
-    //                     handler(payload);
-    //                     return;
-    //                 }
-    //             }
-    //         }
-
-    //         console.log(`No handler found for topic: ${topic}`);
-    //     });
-    // }
+            console.log(`No handler found for topic: ${topic}`);
+        });
+    }
 
     __consumerCallbacksChecker(name, value = null) {
         if (typeof this.listeners[name] === 'function') {
