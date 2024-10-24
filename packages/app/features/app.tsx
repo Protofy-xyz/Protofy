3ba3f690a6440a09492eb4af521d6dcaed9326ab
jcarlosn <jcarlos.norte@gmail.com>
2024-10-24 16:07:42 +0200

    Removes _app.tsx duplication

---
+++ packages/app/features/app.tsx
@@ -0,0 +1,117 @@
+import '@tamagui/core/reset.css'
+import '@tamagui/font-inter/css/400.css'
+import '@tamagui/font-inter/css/700.css'
+import "mapbox-gl/dist/mapbox-gl.css"
+import 'raf/polyfill'
+import 'reactflow/dist/style.css'
+import 'protoflow/src/styles.css'
+import 'protoflow/src/diagram/menu.module.css'
+import 'react-sliding-side-panel/lib/index.css'
+import 'protolib/styles/datatable.css';
+import 'protolib/styles/styles.css';
+import 'protolib/styles/chat.css';
+import 'protolib/styles/chonky.css';
+import 'protolib/styles/blueprint.css';
+import 'protolib/styles/dashboard.css';
+import 'protolib/styles/map.css';
+import 'app/styles/app.css';
+import "@blueprintjs/table/lib/css/table.css";
+import 'react-dropzone-uploader/dist/styles.css'
+import 'react-chat-widget/lib/styles.css';
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
+import { NextThemeProvider, useRootTheme } from '@tamagui/next-theme'
+import { Provider } from 'app/provider'
+import Head from 'next/head'
+import React from 'react'
+import type { SolitoAppProps } from 'solito'
+import { Provider as JotaiProvider } from 'jotai'
+import { useSession } from 'protolib/lib/Session'
+import { AppConfContext } from 'protolib/providers/AppConf'
+import { getBrokerUrl } from 'protolib/lib/Broker'
+import { Connector } from 'protolib/lib/mqtt'
+import { Toast, YStack } from '@my/ui'
+import { SiteConfig } from 'app/conf'
+import Workspaces from 'app/bundles/workspaces'
+import { PanelLayout } from 'app/layout/PanelLayout'
+import { useRouter } from 'next/router';
+
+if (process.env.NODE_ENV === 'production') {
+  require('../public/tamagui.css')
+}
+
+const getApp = (AppConfig) => {
+    return function MyApp({ Component, pageProps }: SolitoAppProps) {
+        //@ts-ignore
+        const [session] = useSession(pageProps['pageSession'])
+        const projectName = SiteConfig.projectName
+        const brokerUrl = getBrokerUrl()
+        return (
+          <>
+            <Head>
+              <title>{projectName + " - AI Supercharged LowCode Platform CMS and Framework"}</title>
+              <meta name="description" content="Next Generation Development Platform for web, mobile and IoT. Based on proven tech: React, ChatGPT, ESPHome, Express, Next, Node, Tamagui, Zod, LevelDB an much more." />
+              {/* <link rel="icon" href="/favicon.ico" /> */}
+            </Head>
+            <JotaiProvider>
+              <Connector brokerUrl={brokerUrl} options={{ username: session?.user?.id, password: session?.token }}>
+                <ThemeProvider>
+                  <AppConfContext.Provider value={{
+                    ...AppConfig,
+                    bundles: {
+                      workspaces: Workspaces,
+                    },
+                    layout: {
+                      PanelLayout
+                    }
+                  }}>
+                    <Component {...pageProps} />
+                  </AppConfContext.Provider>
+                </ThemeProvider>
+              </Connector>
+            </JotaiProvider>
+          </>
+        )
+      }
+}
+
+
+function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const [theme, setTheme] = useRootTheme()
+  const forcedTheme = SiteConfig.ui.forcedTheme
+  const currentUrl = router.asPath;
+  const containsChatbot = currentUrl.includes('/chatbot');
+  const isDev = process.env.NODE_ENV === 'development'
+
+  return (
+    <NextThemeProvider
+      forcedTheme={forcedTheme}
+      onChangeTheme={(next) => {
+        setTheme(next as any)
+      }}
+    >
+      <Provider disableRootThemeClass defaultTheme={theme}>
+        {children}
+
+        {(isDev && !containsChatbot) && <Toast
+          viewportName="warnings"
+          enterStyle={{ opacity: 0, scale: 0.5, y: -25 }}
+          exitStyle={{ opacity: 0, scale: 1, y: -20 }}
+          y={0}
+          opacity={1}
+          scale={1}
+          duration={9999999999}
+          animation="100ms"
+        >
+          <YStack>
+            <Toast.Title>Preview Mode</Toast.Title>
+            <Toast.Description>This page is in preview/development mode. This may affect your user experience and negatively impact the performance.</Toast.Description>
+          </YStack>
+        </Toast>}
+      </Provider>
+    </NextThemeProvider>
+  )
+}
+
+export default getApp
