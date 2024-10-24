ae735d41a83ccda1b915c9716453e765bfde8fc8
jcarlosn <jcarlos.norte@gmail.com>
2024-10-24 13:56:12 +0200

    Adds packages/app/apis/** to watched api paths

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -54,6 +54,7 @@ if (process.env.NODE_ENV != 'production') {
   const pathsToWatch = [
     'src/**',
     '../../packages/app/bundles/**',
+    '../../packages/app/apis/**',
     '../../packages/protolib/dist/**',
     '../../system.js',
     '../../packages/app/conf.ts',
