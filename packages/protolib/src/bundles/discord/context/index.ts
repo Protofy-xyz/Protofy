35ad726841f8d55ed29dbc7b5c8171b7084cc20d
MiquelCos <miquel@protofy.xyz>
2024-11-12 15:03:33 +0100

    Adds support to discord send to accept channel id
    Co-authored-by: Gerard <gerard@protofy.xyz>

--- packages/protolib/src/bundles/discord/context/index.ts
+++ packages/protolib/src/bundles/discord/context/index.ts
@@ -3,7 +3,7 @@ import { getKey } from '../../keys/context';
 import { Client, GatewayIntentBits } from 'discord.js'
 
 const getToken = async (apiKey?): Promise<string> => {
-    if(apiKey) return apiKey
+    if (apiKey) return apiKey
     try {
         const token = await getKey({ key: "DISCORD_APP_TOKEN", token: getServiceToken() })
         return token
@@ -33,35 +33,40 @@ type connectProps = {
     apiKey?: string
 }
 
-const send = async ({channel, message, onSend}:{channel: any, message: string, onSend?: Function}) => {
-    channel.send(message)
-    if(onSend) onSend()
-}
-
 export const discord = {
-    connect : async ({onMessage, onConnect, onDisconnect, onError, apiKey}:connectProps) => {
+    connect: async ({ onMessage, onConnect, onDisconnect, onError, apiKey }: connectProps) => {
         const key = await getToken(apiKey)
         try {
             await client.login(key)
             client.on('disconnect', () => {
-                if(onDisconnect) onDisconnect(client)
+                if (onDisconnect) onDisconnect(client)
             })
 
-            if(onConnect) onConnect(client)
+            if (onConnect) onConnect(client)
             client.on('messageCreate', (message) => {
-                if(onMessage) onMessage(message)
+                if (onMessage) onMessage(message)
             })
-    
+
         } catch (err) {
-            if(onError) onError(err)
+            if (onError) onError(err)
             console.error("Bot initialization error", err)
         }
     },
-    send,
-    response: async ({message, response, onSend}:{message: any, response: string, onSend?: Function}) => {
-        return send({channel: message.channel, message: response, onSend})
+    send: async ({ channel, message, onSend }: { channel: any, message: string, onSend?: Function }) => {
+        if (channel && typeof channel === 'string') { // Checks that provided channel is channelId instead of channel object
+            try {
+                channel = await client.channels.fetch(channel); // Fetch channel object given channelId ('channel')
+            } catch (err) {
+                console.error("Error fetching channel: ", err);
+            }
+        }
+        channel.send(message)
+        if (onSend) onSend()
     },
-    readMessages: async ({channelId, limit = 0, onMessagesRead}: {channelId: string, limit?: number, onMessagesRead: Function}) => {
+    response: async ({ message, response, onSend }: { message: any, response: string, onSend?: Function }) => {
+        return send({ channel: message.channel, message: response, onSend })
+    },
+    readMessages: async ({ channelId, limit = 0, onMessagesRead }: { channelId: string, limit?: number, onMessagesRead: Function }) => {
         console.log("Reading messages from channel", channelId, "with limit:", limit);
         try {
             const channel = await client.channels.fetch(channelId);
@@ -77,11 +82,11 @@ export const discord = {
             do {
                 // If limit is 0 (fetch all), request 100, otherwise fetch the minimum of 100 or the remaining messages
                 const fetchLimit = limit === 0 ? 100 : Math.min(remainingMessages, 100);
-                
+
                 fetchedMessages = await channel.messages.fetch({ limit: fetchLimit, before: lastMessageId });
                 messages.push(...fetchedMessages.values());
                 lastMessageId = fetchedMessages.last()?.id;
-                
+
                 // Update remaining messages count, if we are limiting
                 if (limit !== 0) {
                     remainingMessages -= fetchedMessages.size;
