95638f2bd15d58f0f748f0845d01543e2755c758
noreplydev <contact.cristiansanchez@gmail.com>
2024-11-13 19:28:18 +0100

    Remove functionCall chatbot from packages/app/chatbots

--- packages/app/chatbots/index.ts
+++ packages/app/chatbots/index.ts
@@ -1,10 +1,8 @@
 import { Protofy } from 'protobase'
 import assistantChatbot from "./assistant";
-import functionsCallsChatbot from "./functionsCallsChatbot";
 
 const chatbots = Protofy("chatbots", {
     assistant: assistantChatbot,
-    functionsCallsChatbot: functionsCallsChatbot
 })
 
 export default (app, context) => {
