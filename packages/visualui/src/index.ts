da66045bcfb9e2863f39028bd77425248dc8b0b3
MiquelCos <miquel@protofy.xyz>
2024-11-18 10:18:39 +0100

    Adds content editable atom to visual ui

--- packages/visualui/src/index.ts
+++ packages/visualui/src/index.ts
@@ -2,3 +2,4 @@ import App from "./App";
 
 export default App;
 export { newVisualUiContext, useVisualUi, useVisualUiAtom, useVisualUiComms } from "./visualUiHooks"
+export * from "./components/ContentEditable"
