c0eb4cbadc718a75e3caf43b7747d043dcc3e470
lluis@protofy.xyz <lluis@protofy.xyz>
2024-12-05 09:28:21 +0100

    bugFix on event deletion

--- packages/protolib/src/bundles/events/eventsAPI.ts
+++ packages/protolib/src/bundles/events/eventsAPI.ts
@@ -1,12 +1,27 @@
 import { EventModel } from ".";
-import { AutoAPI, getDBOptions } from 'protonode'
+import { AutoAPI, getDBOptions, getServiceToken } from 'protonode'
 import { connectDB, getDB } from '@my/config/dist/storageProviders';
+import { API } from 'protobase'
 
 export const EventsAPI = async (app, context) => {
     const EventAPI = AutoAPI({
         modelName: 'events',
         modelType: EventModel,
         prefix: '/api/core/v1/',
+        onAfterCreate: async (data, session, req) => {
+            const result = await API.get('/api/core/v1/events?itemsPerPage=1&orderBy=created&orderDirection=asc&token='+getServiceToken())
+            // console.log("result", result)
+            const maxEvents = process.env.MAX_EVENTS || 100000
+            if(result.data){
+                if(result.data.total >= maxEvents ){
+                    const element = result.data.items[0]
+                    // console.log("element", element)
+                    const result2 = await API.get(`/api/core/v1/events/${element.id}/delete?token=${getServiceToken()}`)
+                    // console.log("result2", result2)
+                }
+            }
+            return data
+        },
         skipStorage: async(data,session?,req?) => {
             if(data.ephemeral){
                 return true
