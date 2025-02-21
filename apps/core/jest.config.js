1d4c202c49456d7d280e18837fddaf216d5e7c0e
jcarlosn <jcarlos.norte@gmail.com>
2025-02-22 00:15:55 +0100

    Adds yarn test to apps/core to add event testing

--- apps/core/jest.config.js
+++ apps/core/jest.config.js
@@ -1,10 +1,11 @@
 const baseJestConfig = require('./jest/jest.config.base.js');
 module.exports = {
     ...baseJestConfig,
+    setupFiles: ['module-alias/register'],
     transform: {
         '^.+\\.test.ts?$': [
             'ts-jest',
-            { tsconfig: './tests/tsconfig.json' },
+            { tsconfig: './tsconfig.json' },
         ],
     }
 };
---
+++ apps/core/test.js
@@ -0,0 +1,23 @@
+// runTests.js
+const { startCore } = require('./dist/apps/core/src'); // Ajusta la ruta según corresponda
+const { spawn, exec } = require('child_process');
+
+
+startCore((port) => {
+  console.log(`Core started at ${port}. Running tests...`);
+  const testProcess = spawn('yarn', ['jest'],  { stdio: 'inherit', shell: true });
+  
+  testProcess.on('close', (code) => {
+    console.log(`Tests done, exit code: ${code}`);
+    if(process.platform === 'win32') {
+        exec(`taskkill /PID ${process.pid} /T /F`, (err, stdout, stderr) => {
+            if (err) {
+              console.error('Error killing process tree:', err);
+            }
+            process.exit(code);
+          });
+    } else {
+        process.kill(process.pid, 'SIGTERM');
+    }
+  });
+});
