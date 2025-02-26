81322d2364cd4de446f5a3113fc4dbcea4ede951
lluis-protofy-xyz <lluis@protofy.xyz>
2025-02-26 15:03:50 +0100

    change deviceDefinitions from database to local storage in data/deviceDefinitons folder

--- packages/protolib/src/bundles/devices/deviceDefinitions/deviceDefinitionsAPI.ts
+++ packages/protolib/src/bundles/devices/deviceDefinitions/deviceDefinitionsAPI.ts
@@ -1,8 +1,12 @@
-import { DeviceDefinitionModel } from ".";
-import { AutoAPI } from 'protonode'
+import { DeviceDefinitionModel} from ".";
+import { AutoAPI, getRoot } from 'protonode'
 import { API } from 'protobase'
 import { DevicesModel } from "../devices";
+import { promises as fs } from 'fs';
+import * as fsSync from 'fs';
+import * as fspath from 'path';
 
+const deviceDefinitionsDir = (root) => fspath.join(root, "/data/deviceDefinitions/")
 const initialData = {}
 
 const onAfterCreate = async (data, session?, req?) => {
@@ -18,12 +22,79 @@ const onAfterCreate = async (data, session?, req?) => {
 }
 
 
+const getDB = (path, req, session) => {
+    const db = {
+        async *iterator() {
+            // console.log("Iterator")
+            //check if deviceDefinitions folder exists
+            try {
+                await fs.access(deviceDefinitionsDir(getRoot(req)), fs.constants.F_OK)
+            } catch (error) {
+                console.log("Creating deviceDefinitions folder")
+                await fs.mkdir(deviceDefinitionsDir(getRoot(req)))
+            }
+            //list all .json files in the deviceDefinitions folder
+            const files = (await fs.readdir(deviceDefinitionsDir(getRoot(req)))).filter(f => {
+                const filenameSegments = f.split('.')
+                return !fsSync.lstatSync(fspath.join(deviceDefinitionsDir(getRoot(req)), f)).isDirectory() && (filenameSegments[filenameSegments.length - 1] === "json")
+            })
+            // console.log("Files: ", files)
+            for (const file of files) {
+                //read file content
+                const fileContent = await fs.readFile(deviceDefinitionsDir(getRoot(req)) + file, 'utf8')
+                yield [file.name, fileContent];
+            }
+        },
+
+        async del(key, value) {
+            // delete deviceDefinitions[key]
+            // try to delete the deviceDefinition file from the deviceDefinitions folder
+            console.log("Deleting device definition: ", JSON.stringify({key,value}))
+            const filePath = deviceDefinitionsDir(getRoot(req)) + key + ".json"
+            try {
+                await fs.unlink(filePath)
+            } catch (error) {
+                console.log("Error deleting file: " + filePath)
+            }
+        },
+
+        async put(key, value) {
+            // try to create the deviceDefinition file in the deviceDefinitions folder
+            // console.log("Creating device definition: ", JSON.stringify({key,value}))
+            const filePath = deviceDefinitionsDir(getRoot(req)) + key + ".json"
+            try{
+                await fs.writeFile(filePath, value)
+            }catch(error){
+                console.error("Error creating file: " + filePath, error)
+            }
+        },
+
+        async get(key) {
+            // try to get the deviceDefinition file from the deviceDefinitions folder
+            // console.log("Get function: ",key)
+            const filePath = deviceDefinitionsDir(getRoot(req)) + key + ".json"
+            try{
+                const fileContent = await fs.readFile(filePath, 'utf8')
+                // console.log("fileContent: ", fileContent)
+                // console.log("filePath: ", filePath)
+                return fileContent
+            }catch(error){
+                // console.log("Error reading file: " + filePath)
+                throw new Error("File not found")
+            }                   
+        }
+    };
+
+    return db;
+}
+
 export const DeviceDefinitionsAPI = AutoAPI({
     modelName: 'devicedefinitions',
     modelType: DeviceDefinitionModel,
     initialData,
     onAfterCreate: onAfterCreate,
     skipDatabaseIndexes: true,
+    getDB: getDB,
     prefix: '/api/core/v1/',
     transformers: {
         getConfig: async (field, e, data) => {
