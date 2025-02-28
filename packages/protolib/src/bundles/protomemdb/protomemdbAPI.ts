02c0704c0ad177d6e3a3f80eae0ffe130c6ef68e
lluis@protofy.xyz <lluis@protofy.xyz>
2025-02-28 18:42:33 +0100

    Extends protomemdbAPI

--- packages/protolib/src/bundles/protomemdb/protomemdbAPI.ts
+++ packages/protolib/src/bundles/protomemdb/protomemdbAPI.ts
@@ -2,7 +2,9 @@ import { ProtoMemDB } from 'protobase';
 
 export const ProtoMemDBAPI = (app, context) => {
     app.get('/api/v1/protomemdb', (req, res) => {
-        
         res.status(200).json({ tags:ProtoMemDB.getTags() })
+    })
+    app.get('/api/v1/protomemdb/:tag', (req, res) => {
+        res.status(200).json(ProtoMemDB.getByTag(req.params.tag))
     })
 }
