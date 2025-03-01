f9f96499eeb87fd9f4a296ac90e5f51110b0e66c
Noemi Blazquez <ap0calipse@gmail.com>
2025-03-01 23:22:23 +0100

    Adds API to support to getStatesFromProtoMemDB

--- packages/protolib/src/bundles/protomemdb/context/getStatesFromProtoMemDB.ts
+++ packages/protolib/src/bundles/protomemdb/context/getStatesFromProtoMemDB.ts
@@ -1,8 +1,15 @@
-import { StateElement, StateGroup } from "protobase";
+import { API, StateElement, StateGroup } from "protobase";
 import { ProtoMemDB } from "protobase";
 
-export const getStatesFromProtoMemDB = async (tag): Promise<StateGroup> => {
-    const states = await ProtoMemDB.getByTag(tag);
+export const getStatesFromProtoMemDB = async (tag, useRemoteAPI?): Promise<StateGroup> => {
+    let states
+    if(useRemoteAPI) {
+        states = (await API.get("/api/v1/protomemdb/" + tag)).data
+    } else {
+        states = await ProtoMemDB.getByTag(tag);
+    }
+    
+    
     const stateGroup = new StateGroup(Object.entries(states).map(([key, value]) => {
         return new StateElement(key, value);
     }), 'states');
