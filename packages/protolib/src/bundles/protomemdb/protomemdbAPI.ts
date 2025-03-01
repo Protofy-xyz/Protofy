476d7c19d31eb24fd57e0d374596972b0c1b8a69
lluis@protofy.xyz <lluis@protofy.xyz>
2025-03-01 16:07:41 +0100

    Add auth in protomemdb and set state endpoint to API

--- packages/protolib/src/bundles/protomemdb/protomemdbAPI.ts
+++ packages/protolib/src/bundles/protomemdb/protomemdbAPI.ts
@@ -1,10 +1,29 @@
 import { ProtoMemDB } from 'protobase';
+import {handler} from 'protonode';
 
 export const ProtoMemDBAPI = (app, context) => {
-    app.get('/api/v1/protomemdb', (req, res) => {
+    app.get('/api/v1/protomemdb', handler(async (req, res, session) => {
+        if(!session || !session.user.admin) {
+            res.status(401).send({error: "Unauthorized"})
+            return
+        }
         res.status(200).json({ tags:ProtoMemDB.getTags() })
-    })
-    app.get('/api/v1/protomemdb/:tag', (req, res) => {
+    }))
+
+    app.get('/api/v1/protomemdb/:tag', handler(async (req, res, session) => {
+        if(!session || !session.user.admin) {
+            res.status(401).send({error: "Unauthorized"})
+            return
+        }
         res.status(200).json(ProtoMemDB.getByTag(req.params.tag))
-    })
+    }))
+    
+    app.post('/api/v1/protomemdb/:tag/:name', handler(async (req, res, session) => {
+        if(!session || !session.user.admin) {
+            res.status(401).send({error: "Unauthorized"})
+            return
+        }
+        ProtoMemDB.set(req.params.tag, req.params.name, req.body.value)
+        res.status(200).json({ success: true })
+    }))
 }
