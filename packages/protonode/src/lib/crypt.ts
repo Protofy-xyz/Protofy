44a9c01836c26c652c5ddb19f229c0b358af4f52
mvrcPR <marc@protofy.xyz>
2025-03-10 14:10:28 +0100

    fix, avoid using jwt.sign on genToken when no token secret available on process.env

--- packages/protonode/src/lib/crypt.ts
+++ packages/protonode/src/lib/crypt.ts
@@ -5,7 +5,8 @@ export const hash = async (password: string) => bcrypt.hash(password, 10)
 export const checkPassword = async (password: string, hash: string) => bcrypt.compare(password, hash)
 
 export const genToken = (data:any,options:any={ expiresIn: '3600000s' }) => {
-    return jwt.sign(data, process.env.TOKEN_SECRET ?? '', options);
+    if (!process.env.TOKEN_SECRET) return
+    return jwt.sign(data, process.env.TOKEN_SECRET, options);
 }
 
 export const verifyToken = (token: string) => { 
