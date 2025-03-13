3bd80668e6d860807f1fd6fac1d8ab9edc1a533f
Noemi Blazquez <ap0calipse@gmail.com>
2025-03-13 19:46:13 +0100

    Extract Rules component from rulesCard for rule management

--- packages/protolib/src/components/autopilot/AutopilotEditor.tsx
+++ packages/protolib/src/components/autopilot/AutopilotEditor.tsx
@@ -3,17 +3,21 @@ import { YStack, ScrollView } from "@my/ui";
 import { Tinted } from "../../components/Tinted";
 import JSONViewer from "../jsonui/JSONViewer";
 import CustomPanelResizeHandle from "../MainPanel/CustomPanelResizeHandle";
+import { Rules } from "./Rules";
+import { useEffect, useState } from "react";
 
-export const AutopilotEditor = ({ data }) => {
+
+export const AutopilotEditor = ({ data, rules, value  }) => {
+
     return (
         <PanelGroup direction="horizontal">
             <Panel defaultSize={33} minSize={20} maxSize={50}>
-                <YStack 
-                    flex={1} 
-                    height="100%" 
-                    borderRadius="$3" 
-                    p="$3" 
-                    backgroundColor="$gray2" 
+                <YStack
+                    flex={1}
+                    height="100%"
+                    borderRadius="$3"
+                    p="$3"
+                    backgroundColor="$gray2"
                     overflow="hidden"
                 >
                     <ScrollView flex={1} width="100%" height="100%">
@@ -27,6 +31,7 @@ export const AutopilotEditor = ({ data }) => {
             <CustomPanelResizeHandle direction="vertical" />
 
             <Panel defaultSize={67} minSize={50}>
+
                 <YStack
                     flex={1}
                     height="100%"
@@ -36,7 +41,15 @@ export const AutopilotEditor = ({ data }) => {
                     borderRadius="$3"
                     p="$3"
                 >
-                    {/* Content rigth panel */}
+                    <Tinted>
+                    <Rules
+                        rules={rules.rules}
+                        onAddRule={rules.onAddRule}
+                        onDeleteRule={rules.onDeleteRule}
+                        loadingIndex={rules.loadingIndex}
+                    />
+                    </Tinted>
+
                 </YStack>
             </Panel>
         </PanelGroup>
