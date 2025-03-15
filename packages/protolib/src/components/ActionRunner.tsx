c5bca0144b2b06b23111a2328b297086b956c3bf
jcarlosn <jcarlos.norte@gmail.com>
2025-03-16 00:24:52 +0100

    Improves action runner button color

--- packages/protolib/src/components/ActionRunner.tsx
+++ packages/protolib/src/components/ActionRunner.tsx
@@ -29,7 +29,7 @@ export const ActionRunner = ({ name, caption = "Run", description = "", actionPa
                             width: "48px",
                             height: "48px",
                             marginBottom: '15px',
-                            backgroundColor: 'var(--color)',
+                            backgroundColor: color,
                             maskImage: `url(${icon})`,
                             WebkitMaskImage: `url(${icon})`,
                             maskRepeat: "no-repeat",
@@ -39,7 +39,7 @@ export const ActionRunner = ({ name, caption = "Run", description = "", actionPa
                             maskPosition: "center",
                             WebkitMaskPosition: "center"
                         }} />}
-                        <Button backgroundColor={color} mt={"$3"} width="100%" className="no-drag" onPress={() => {
+                        <Button pressStyle={{filter: "brightness(0.95)", backgroundColor: color}} hoverStyle={{backgroundColor:color, filter: "brightness(1.1)"}} backgroundColor={color} mt={"$3"} width="100%" className="no-drag" onPress={() => {
                             //actionData.automationParams is a key value object where the key is the name of the parameter and the value is the description
                             //if there are parameters, they should be included in the query parameters of the request
                             //if a parameter is missing, remove it from the query parameters
