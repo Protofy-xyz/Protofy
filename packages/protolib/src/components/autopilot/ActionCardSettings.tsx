26f908d0711ebcd37411951763fc93ab4c727656
Noemi Blazquez <ap0calipse@gmail.com>
2025-03-17 00:31:00 +0100

    Add ParamsEditor component and enhance ActionCardSettings with tab functionality

--- packages/protolib/src/components/autopilot/ActionCardSettings.tsx
+++ packages/protolib/src/components/autopilot/ActionCardSettings.tsx
@@ -1,83 +1,94 @@
 import { BookOpenText, ExternalLink, Cog, Palette, Type } from '@tamagui/lucide-icons'
-import { YStack, XStack, Input, Label } from '@my/ui'
+import { YStack, XStack, Input, Label, ToggleGroup } from '@my/ui'
 import { useEffect, useState } from 'react'
 import { Tinted } from '../Tinted'
 import { InputColor } from '../InputColor'
-import { RuleEditor } from '../autopilot/RuleEditor'
+import { RuleEditor } from './RuleEditor'
 import { InteractiveIcon } from '../InteractiveIcon'
 import { IconSelect } from '../IconSelect'
+import { ParamsEditor } from './ParamsEditor'
 
 export const ActionCardSettings = ({ actions, states, card, icons, onEdit = (data) => { } }) => {
-    const [cardData, setCardData] = useState(card);
-    useEffect(() => {
-      onEdit(cardData);
-    }, [cardData, onEdit]);
-  
-    return (
-      <YStack space="$4" padding="$4">
-        <Tinted>
-          <XStack alignItems="center" space="$8" width="100%">
-            <YStack flex={1}>
-              <Label size={"$5"}> <Type color={"$color8"} mr="$2" />Name</Label>
-              <Input
-                value={cardData.name}
-                onChange={(e) =>
+  const [cardData, setCardData] = useState(card);
+  const [tab, setTab] = useState("rules");
+  useEffect(() => {
+    onEdit(cardData);
+  }, [cardData, onEdit]);
+
+  return (
+    <YStack space="$4" padding="$4">
+      <Tinted>
+        <XStack alignItems="center" space="$8" width="100%">
+          <YStack flex={1}>
+            <Label size={"$5"}> <Type color={"$color8"} mr="$2" />Name</Label>
+            <Input
+              value={cardData.name}
+              onChange={(e) =>
+                setCardData({
+                  ...cardData,
+                  name: e.target.value,
+                })
+              }
+            />
+          </YStack>
+          <YStack flex={1}>
+            <XStack alignItems="center" space="$2">
+              <Label size={"$5"}><BookOpenText color={"$color8"} mr="$2" />Icon</Label>
+
+            </XStack>
+            <XStack alignItems="center" space="$2">
+              <a href="https://lucide.dev/icons/" target="_blank" rel="noreferrer">
+                <InteractiveIcon Icon={ExternalLink}></InteractiveIcon>
+              </a>
+              <IconSelect
+                icons={icons}
+                onSelect={(icon) => {
                   setCardData({
                     ...cardData,
-                    name: e.target.value,
-                  })
-                }
+                    icon,
+                  });
+                }}
+                selected={cardData.icon}
               />
-            </YStack>
-            <YStack flex={1}>
-              <XStack alignItems="center" space="$2">
-                <Label size={"$5"}><BookOpenText color={"$color8"} mr="$2" />Icon</Label>
-  
-              </XStack>
-              <XStack alignItems="center" space="$2">
-                <a href="https://lucide.dev/icons/" target="_blank" rel="noreferrer">
-                  <InteractiveIcon Icon={ExternalLink}></InteractiveIcon>
-                </a>
-                <IconSelect
-                  icons={icons}
-                  onSelect={(icon) => {
-                    setCardData({
-                      ...cardData,
-                      icon,
-                    });
-                  }}
-                  selected={cardData.icon}
-                />
-              </XStack>
-  
-            </YStack>
-            <YStack flex={1}>
-              <Label size={"$5"}><Palette color={"$color8"} mr="$2" />Color</Label>
-              <InputColor
-                color={cardData.color}
-                onChange={(e) =>
-                  setCardData({ ...cardData, color: e.hex })
-                }
-              />
-            </YStack>
+            </XStack>
+
+          </YStack>
+          <YStack flex={1}>
+            <Label size={"$5"}><Palette color={"$color8"} mr="$2" />Color</Label>
+            <InputColor
+              color={cardData.color}
+              onChange={(e) =>
+                setCardData({ ...cardData, color: e.hex })
+              }
+            />
+          </YStack>
+        </XStack>
+
+        <YStack mt="$5" height={600}>
+          <Label mb="$-3" size={"$5"}><Cog color={"$color8"} mr="$2"></Cog>Actions</Label>
+          <XStack width={"100%"} pt="$0" pr="$1" pb="$2" jc="center">
+            <ToggleGroup disableDeactivation={true} height="$3" type="single" value={tab} onValueChange={setTab}>
+              <ToggleGroup.Item value="rules">rules</ToggleGroup.Item>
+              <ToggleGroup.Item value="params">params</ToggleGroup.Item>
+            </ToggleGroup>
           </XStack>
-  
-          <YStack mt="$5" height={600}>
-            <Label mb="$-3" size={"$5"}><Cog color={"$color8"} mr="$2"></Cog>Actions</Label>
-            <RuleEditor
-              extraCompilerData={{userParams: cardData.params, actions: actions}}
+          <Tinted>
+            {(tab == 'rules' || !tab) && <RuleEditor
+              extraCompilerData={{ userParams: cardData.params, actions: actions }}
               onCodeChange={(cardData, states) => {
-                  return "rules processed"
+                return "rules processed"
               }}
               displayInput={actions}
               compiler='getActionCode'
               states={states}
               cardData={cardData}
               setCardData={setCardData}
-            />
-          </YStack>
-        </Tinted>
-      </YStack>
-    );
-  };
-  
+            />}
+            {tab == 'params' && <ParamsEditor />}
+          </Tinted>
+
+        </YStack>
+      </Tinted>
+    </YStack>
+  );
+};
---
+++ packages/protolib/src/components/autopilot/ParamsEditor.tsx
@@ -0,0 +1,12 @@
+import { YStack } from "@my/ui"
+
+export const ParamsEditor = () => {
+    return <YStack
+      flex={1}
+      height="100%"
+      borderRadius="$3"
+      p="$3"
+      backgroundColor="$gray3"
+      overflow="hidden"
+    ></YStack>
+  }
