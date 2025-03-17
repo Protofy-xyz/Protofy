a9765fd600e707892b74759c728baed97cefedc8
lluis-protofy-xyz <lluis@protofy.xyz>
2025-03-17 05:33:18 +0100

    Add twilio bundle for whatsapp support

---
+++ packages/protolib/src/bundles/twilio/context/index.ts
@@ -0,0 +1,9 @@
+import {twimlMessage} from './twimlMessage';
+import { sendWhatsAppMessage } from './sendWhatsAppMessage';
+import { generateWhatsappQrCode } from '../utils/generateWhatsappQrCode';
+
+export default {
+    twimlMessage,
+    sendWhatsAppMessage,
+    generateWhatsappQrCode
+}
