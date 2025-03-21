56d0f17df1f21f4dc146cbbcd36ed1fc7d70d29a
miquel <miquel@protofy.xyz>
2025-03-21 20:19:30 +0100

    changes styles on Board card selector

--- packages/protolib/src/components/autopilot/CardSettings.tsx
+++ packages/protolib/src/components/autopilot/CardSettings.tsx
@@ -6,43 +6,44 @@ import { IconSelect } from '../IconSelect'
 
 export const CardSettings = ({ cardData, setCardData, icons }) => {
 
-    return <XStack alignItems="center" space="$8" width="100%" mt="$-6">
-            <YStack flex={1} w={400}>
-                <Label size={"$5"}> <Type color={"$color8"} mr="$2" />Title</Label>
-                <Input
-                    value={cardData.name}
-                    onChange={(e) =>
-                        setCardData({
-                            ...cardData,
-                            name: e.target.value,
-                        })
-                    }
-                />
-            </YStack>
-            <YStack flex={1} w={400}>
-                <a href="https://lucide.dev/icons/" target="_blank" rel="noreferrer">
-                    <Label hoverStyle={{color: 'var(--blue9)', textDecorationLine: 'underline'}} size={"$5"}><BookOpenText color={"$color8"} mr="$2" />Icon</Label>
-                </a>
-                <IconSelect
-                    icons={icons}
-                    onSelect={(icon) => {
-                        setCardData({
-                            ...cardData,
-                                icon,
-                        });
-                    }}
-                    selected={cardData.icon}
-                />
-            </YStack>
-            <YStack flex={1} w={400}>
-                <Label size={"$5"}><Palette color={"$color8"} mr="$2" />Color</Label>
-                <InputColor
-                    color={cardData.color}
-                    onChange={(e) =>
-                        setCardData({ ...cardData, color: e.hex })
-                    }
-                />
-            </YStack>
-        </XStack>
+    return <XStack alignItems="center" space="$6" width="100%" >
+        <YStack flex={1} w={400} >
+            <Label ml={"$2"} h={"$3.5"} size={"$5"}> <Type color={"$color8"} mr="$2" />Title</Label>
+            <Input
+                br={"8px"}
+                value={cardData.name}
+                onChange={(e) =>
+                    setCardData({
+                        ...cardData,
+                        name: e.target.value,
+                    })
+                }
+            />
+        </YStack>
+        <YStack flex={1} w={400}>
+            <Label ml={"$2"} h={"$3.5"} size={"$5"}><BookOpenText color={"$color8"} mr="$2" />Icon</Label>
+            <IconSelect
+                br={"8px"}
+                icons={icons}
+                onSelect={(icon) => {
+                    setCardData({
+                        ...cardData,
+                        icon,
+                    });
+                }}
+                selected={cardData.icon}
+            />
+        </YStack>
+        <YStack flex={1} w={400}>
+            <Label ml={"$2"} h={"$3.5"} size={"$5"}><Palette color={"$color8"} mr="$2" />Color</Label>
+            <InputColor
+                br={"8px"}
+                color={cardData.color}
+                onChange={(e) =>
+                    setCardData({ ...cardData, color: e.hex })
+                }
+            />
+        </YStack>
+    </XStack>
 
 }
