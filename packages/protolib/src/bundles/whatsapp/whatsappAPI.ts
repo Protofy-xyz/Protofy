dd59df09ddec2483ad83d99e88a148c046a81e96
lluis@protofy.xyz <lluis@protofy.xyz>
2025-03-21 12:41:12 +0100

    Add whatsapp cards and states

--- packages/protolib/src/bundles/whatsapp/whatsappAPI.ts
+++ packages/protolib/src/bundles/whatsapp/whatsappAPI.ts
@@ -11,12 +11,22 @@ import { addCard } from "../cards/context/addCard";
 
 const logger = getLogger()
 
+const qrImg = async (context)=>{ 
+    try {
+        const base64Img = await context.whatsapp.generateWhatsappQrCode(
+            "phone",
+            "message" + ` projectId: ${"a"}`
+        );
+        const base64RawData = base64Img.replace(/^data:image\/png;base64,/, "");
+        return base64Img;
 
-export const WhatsappAPI = (app, context) => {
-    const devicesPath = '../../data/devices/'
-    const { topicSub, topicPub, mqtt } = context;
-    
-    // registerActions()
+    } catch (error) {
+        console.error('Error generando whatsapp QR:', error);
+        return "Error generando la imagen";
+    }
+}
+
+const registerActions = async (context)=>{
     addAction({
         group: 'whatsapp',
         name: 'message',
@@ -26,6 +36,62 @@ export const WhatsappAPI = (app, context) => {
         params: {phone: "E.164 format phone number started by + and country code. Example: +34666666666", message: "message value to send"},
         emitEvent: true
     })
+}
+
+const registerCards = async (context)=>{
+    addCard({
+        group: 'whatsapp',
+        tag: "message",
+        id: 'whatsapp_message_send',
+        templateName: "whatsapp send message",
+        name: "message_send",
+        defaults: {
+            name: "whatsapp_message_send",
+            icon: "whatsapp",
+            description: "send a whatsapp message to a phone number",
+            rulesCode: `return execute_action("/api/core/v1/whatsapp/send/message", { phone: userParams.phone, message: userParams.message });`,
+            params: {phone: "phone number", message: "message"},
+            type: 'action'
+        },
+        emitEvent: true
+    })
+
+    addCard({
+        group: 'whatsapp',
+        tag: "onboarding",
+        id: 'whatsapp_onboarding_qr',
+        templateName: "whatsapp oboarding qr",
+        name: "qr",
+        defaults: {
+            name: "whatsapp_onboarding_qr",
+            icon: "whatsapp",
+            html: `
+//data contains: data.value, data.icon and data.color
+return card({
+    content: \`
+        \${icon({ name: data.icon, color: data.color, size: '48' })}
+        <img src="${await qrImg(context)}" alt="qr code" />
+    \`
+});
+`,
+            description: "show a qr code to send the onboarding message",
+            rulesCode: `return null;`,
+            type: 'value'
+        },
+        emitEvent: true
+    })
+}
+
+registerActionsAndCards = async (context)=>{
+    registerActions(context)
+    registerCards(context)
+}
+
+export const WhatsappAPI = (app, context) => {
+    const devicesPath = '../../data/devices/'
+    const { topicSub, topicPub, mqtt } = context;
+    registerActionsAndCards(context)
+    
     app.get('/api/core/v1/whatsapp/send/message', handler(async (req, res, session) => {
         const { phone, message } = req.query
         // console.log("phone", phone)
@@ -58,39 +124,8 @@ export const WhatsappAPI = (app, context) => {
         res.send({img})
     }))
 
-    const qrImg = async ()=>{ 
-        try {
-            const base64Img = await context.whatsapp.generateWhatsappQrCode(
-                "phone",
-                "message" + ` projectId: ${"a"}`
-            );
-            const base64RawData = base64Img.replace(/^data:image\/png;base64,/, "");
-            // console.log("base64Img->", base64RawData)
-            
-            context.state.set({ group: 'whatsapp', tag: "onboarding", name: "qr", value: base64Img, emitEvent: true });
+   
 
-        } catch (error) {
-            console.error('Error generando whatsapp QR:', error);
-            return "Error generando la imagen";
-        }
-    }
-    qrImg()
-
-    addCard({
-        group: 'whatsapp',
-        tag: "message",
-        id: 'whatsapp_message_send',
-        templateName: "whatsapp send message",
-        name: "message_send",
-        defaults: {
-            name: "whatsapp_message_send",
-            icon: "whatsapp",
-            description: "send a whatsapp message to a phone number",
-            rulesCode: `return execute_action("/api/core/v1/whatsapp/send/message", { phone: userParams.phone, message: userParams.message });`,
-            params: {phone: "phone number", message: "message"},
-            type: 'action'
-        },
-        emitEvent: true
-    })
+    
     
 }
