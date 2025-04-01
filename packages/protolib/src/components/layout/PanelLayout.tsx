dab4fc4a2361c19b42e06c295105322c7aabc295
miquel <miquel@protofy.xyz>
2025-04-01 18:22:04 +0200

    Removes unnecessary scrollview from panel layout

--- packages/protolib/src/components/layout/PanelLayout.tsx
+++ packages/protolib/src/components/layout/PanelLayout.tsx
@@ -8,23 +8,23 @@ export const PanelLayout = ({ panelBgColor = undefined, menuContent, children, S
   const bgPanels = '$bgPanel'
   const _panelBgColor = '$bgContent'
   const panel = <Panel>
-  <div style={{ display: 'flex', flexDirection: 'column', flex: 1, height: '100%', overflow: 'auto' }}>
-    <XStack bc={"bgPanels"} elevation={3} br={"$6"} mt={appBarHeight} f={1} paddingBottom={panelBottom ? '20px' : '0px'} style={{ flex: 1 }}>
-      <ScrollView f={1} $sm={{ br: "$0" }} style={{
+    <div style={{ display: 'flex', flexDirection: 'column', flex: 1, height: '100%', overflow: 'auto' }}>
+      <XStack bc={"bgPanels"} elevation={3} br={"$6"} mt={appBarHeight} f={1} paddingBottom={panelBottom ? '20px' : '0px'} style={{ flex: 1 }}>
+        {/* <ScrollView f={1} $sm={{ br: "$0" }} style={{
         flexGrow: 1,
         overflowY: 'auto',
         display: 'flex',
         flexDirection: 'column'
       }}
         contentContainerStyle={{ flexGrow: 1 }}
-      >
+      > */}
         <YStack f={1} br={"$6"} bc={panelBgColor ?? _panelBgColor} style={{ flex: 1, overflowY: 'auto' }}>
           {children}
         </YStack>
-      </ScrollView>
-    </XStack>
-  </div>
-</Panel>
+        {/* </ScrollView> */}
+      </XStack>
+    </div>
+  </Panel>
 
   return (
     <Layout
@@ -49,20 +49,20 @@ export const PanelLayout = ({ panelBgColor = undefined, menuContent, children, S
         // </AppBar>
       }>
       {/* <Theme name={tint as any}> */}
-      <XStack f={1} p="$3" $xs={{px: "$0"}} pl={0} bc={bgPanels}>
-        {!panelBottom?panel:
-        <PanelGroup direction="vertical" style={{ height: '100%', width: '100%' }}>
-         {panel}
-          <> <CustomPanelResizeHandle
-            direction="horizontal"
-          />
-            <Panel>
-              <div style={{ display: 'flex', flex: 1, height: '100%', backgroundColor: 'grey' }}>
-                <p>Contenido del panel inferior</p>
-              </div>
-            </Panel>
-          </>
-        </PanelGroup>
+      <XStack f={1} p="$3" $xs={{ px: "$0" }} pl={0} bc={bgPanels}>
+        {!panelBottom ? panel :
+          <PanelGroup direction="vertical" style={{ height: '100%', width: '100%' }}>
+            {panel}
+            <> <CustomPanelResizeHandle
+              direction="horizontal"
+            />
+              <Panel>
+                <div style={{ display: 'flex', flex: 1, height: '100%', backgroundColor: 'grey' }}>
+                  <p>Contenido del panel inferior</p>
+                </div>
+              </Panel>
+            </>
+          </PanelGroup>
         }
       </XStack>
     </Layout>
