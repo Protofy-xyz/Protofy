b560422f5b19dbdd8876ffe0d31b685223da3d4b
jcarlosn <jcarlos.norte@gmail.com>
2025-04-06 16:29:39 +0200

    prepare useisediting to work without next

--- packages/protolib/src/visualui/useIsEditing.ts
+++ packages/protolib/src/visualui/useIsEditing.ts
@@ -2,6 +2,7 @@ import { useSearchParams } from 'next/navigation'
 
 export const useIsEditing = ()=> {
     const searchParams = useSearchParams()
+    if(!searchParams) return false
     const edit = searchParams.get('_visualui_edit_')
     return edit
 }
