0a7b37e0a657a1459c87f09dd021fb4641a79264
jcarlosn <jcarlos.norte@gmail.com>
2025-04-08 01:24:35 +0200

    copy node_modules without symlinks after electron build

---
+++ electron-copy.js
@@ -0,0 +1,23 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+console.log('Copying node_modules into electron build...')
+const sourceDir = path.join(__dirname, 'node_modules');
+const destDir = path.join(__dirname, 'dist', 'win-unpacked', 'resources', 'app', 'node_modules');
+
+//delete destDir if exists, including all its contents
+if (fs.existsSync(destDir)) {
+    fs.removeSync(destDir);
+}
+async function copyNodeModules() {
+    try {
+        await fs.copy(sourceDir, destDir, { dereference: true });
+        console.log('node_modules copied successfully (symlinks resolved)!');
+    } catch (err) {
+        console.error('Error copying node_modules:', err);
+    }
+}
+
+copyNodeModules()
+    .then(() => console.log('Copying completed!'))
+    .catch(err => console.error('Error during copying:', err));
