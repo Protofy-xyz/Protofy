7b3377e66bbbdfc621fa5b8a6d6b650b37304698
lluis@protofy.xyz <lluis@protofy.xyz>
2025-05-06 18:27:07 +0200

    Add arduino page

---
+++ apps/adminpanel/pages/arduinos/[arduino].tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import { SiteConfig } from 'app/conf'
+import { useRouter } from 'next/router';
+import ArduinoPage from 'protolib/bundles/arduino/adminPages'
+
+
+export default function Page(props: any) {
+    const router = useRouter();
+    const { arduino } = router.query;
+
+    return (
+        <>
+            <Head>
+                <title>{arduino+" Board"}</title>
+            </Head>
+            <ArduinoPage.view.component {...props} />
+        </>
+    )
+}
+
+export const getServerSideProps = ArduinoPage.view.getServerSideProps
