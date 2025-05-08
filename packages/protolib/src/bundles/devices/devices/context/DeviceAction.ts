0665870ca30ef86b862d4912a28371b781c0b6dd
DenshiNingen <denshi.ningen@gmail.com>
2025-05-08 19:00:40 +0200

    FIX DeviceAction box when no value was defined

--- packages/protolib/src/bundles/devices/devices/context/DeviceAction.ts
+++ packages/protolib/src/bundles/devices/devices/context/DeviceAction.ts
@@ -2,7 +2,13 @@ import { getServiceToken } from "protonode";
 import { API } from "protobase";
 
 export const deviceAction = async (device, subsystem, action, value?, cb?, errorCb?) => {
-    const url = `/api/core/v1/devices/${device}/subsystems/${subsystem}/actions/${action}/${value}?token=${getServiceToken()}`
+    let url = `/api/core/v1/devices/${device}/subsystems/${subsystem}/actions/${action}`;
+  
+    if (value !== undefined) {
+        url += `/${value}`;
+    }
+
+    url += `?token=${getServiceToken()}`;
     let result = await API.get(url)
     if (result.isError) {
         errorCb && errorCb()
