2c2dfe12043b0c8e5ee99cbecafd10afd032b991
RaiLaup <k4avmu51@gmail.com>
2025-05-12 12:41:52 +0200

    changes fontWeight prop for weight on sizableText in order to make it effective
    Co-authored-by: Marccc <marc@protofy.xyz>

--- packages/protolib/src/components/Chip.tsx
+++ packages/protolib/src/components/Chip.tsx
@@ -5,7 +5,7 @@ export const Chip = ({ loading, icon, text, bold, color, children, textProps, ..
     return <XStack ai="center" jc="center" bc={color??'$color5'} p={2} px={"$3"} br={25} {...props}>
             {icon && React.cloneElement(icon, { size:20, strokeWidth: 0.7, color: 'var(--color)' })}
             {children}
-            {text && <SizableText ml={icon?"$2":"$0"} o={0.97} fontWeight={bold?"900":"400"} size="$1" color="$color" {...textProps}>{text}</SizableText>}
+            {text && <SizableText ml={icon?"$2":"$0"} o={0.97} weight={bold?"900":"400"} size="$1" color="$color" {...textProps}>{text}</SizableText>}
             {loading && <><Spacer size={9} /><Spinner color={"var(--color8)"} scale={0.7} size='small' /></>}
         </XStack>
 }
