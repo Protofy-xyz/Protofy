67ee0411af8cd9ad5ed804ec219a58fbffc51827
Noemi Blazquez <ap0calipse@gmail.com>
2025-06-14 01:39:08 +0200

    Converts visualui into a extension

--- packages/protolib/src/bundles/coreApis.ts
+++ packages/protolib/src/bundles/coreApis.ts
@@ -1,10 +1,8 @@
 import { WledAPI } from './wled/api/wledApi'
-import { MasksAPI } from './visualui/masksAPI';
 import { StateMachinesDefinitionsApi } from './stateMachines/stateMachineDefinitions/stateMachineDefinitionApi'
 
 
 export const AdminAPIBundles = (app, context) => {
   WledAPI(app, context)
-  MasksAPI(app, context)
   StateMachinesDefinitionsApi(app, context)
 }
