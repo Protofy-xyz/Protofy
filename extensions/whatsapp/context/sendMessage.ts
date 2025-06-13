ed8ed6a04a4e8e84dfdf7a048c101e3533a252e2
jcarlosn <jcarlos.norte@gmail.com>
2025-06-14 00:54:34 +0200

    Converts protoinfra into an extension

--- extensions/whatsapp/context/sendMessage.ts
+++ extensions/whatsapp/context/sendMessage.ts
@@ -1,6 +1,6 @@
 
 import axios from "axios";
-import protoInfraUrls from 'protolib/bundles/protoinfra/utils/protoInfraUrls'
+import protoInfraUrls from '@extensions/protoinfra/utils/protoInfraUrls'
 
 export const sendMessage = async (phone: string, message: string) => {
     const response = axios.post(protoInfraUrls.whatsapp.api+'/sendMessage', {phone, message});
