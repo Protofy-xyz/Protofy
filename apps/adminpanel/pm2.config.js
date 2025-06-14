e2fae7bccd598accf31f419c8dfbc8e14ff271f3
Noemi Blazquez <ap0calipse@gmail.com>
2025-06-14 17:41:37 +0200

    Removes prod mode from admin panel. The prod mode of adminpanel are compiled pages in html

--- apps/adminpanel/pm2.config.js
+++ apps/adminpanel/pm2.config.js
@@ -1,6 +1,3 @@
-const isFullDev = process.env.FULL_DEV === '1';
-const isAdminDev = process.env.ADMIN_DEV === '1';
-
 const path = require('path');
 const currentDir = path.dirname(__filename);
 
@@ -23,25 +20,6 @@ const siteDev = {
   vizion: false
 }
 
-const site = {
-  name: "adminpanel",
-  script: path.join(currentDir, 'dist/apps/adminpanel/entrypoint.js'),
-  windowsHide: true,
-  watch: false,
-  env: {
-      NODE_ENV: 'production',
-      PORT: 8000,
-      DOTENV_CONFIG_PATH: path.resolve(__dirname, '../../.env'),
-      NEXT_PUBLIC_APP_ID: 'adminpanel'
-  },
-  cwd: path.join(currentDir, 'dist/apps/adminpanel'),
-  log_date_format: "YYYY-MM-DD HH:mm:ss",
-  out_file: '../../logs/raw/adminpanel.stdout.log',
-  error_file: '../../logs/raw/adminpanel.stderr.log',
-  node_args : '-r dotenv/config',
-  vizion: false
-}
-
 module.exports = {
-    apps: isFullDev || isAdminDev ? [siteDev] : [site],
+    apps: [siteDev] 
 }
