e6903e9f33efde1698d17aee33a78a45a9e25eae
jcarlosn <jcarlos.norte@gmail.com>
2025-06-16 01:47:51 +0200

    Adds state support for logs

--- extensions/logs/coreApis.ts
+++ extensions/logs/coreApis.ts
@@ -1,5 +1,14 @@
 import { handler } from 'protonode'
 
+const levelTable = {
+  '10': 'trace',
+  '20': 'debug',
+  '30': 'info',
+  '40': 'warn',
+  '50': 'error',
+  '60': 'fatal'
+}
+
 export default (app, context) => {
   app.get('/adminapi/v1/logs', handler(async (req, res, session) => {
     if (!session || !session.user.admin) {
@@ -8,4 +17,22 @@ export default (app, context) => {
     }
     res.send({})
   }))
+
+  const processMessage = async (message, topic) => {
+    console.log(`Received message on topic ${topic}:`, message)
+    // Process the message as needed
+    //logs/api/30 is a topic
+    const parts = topic.split('/')
+    if (parts.length < 3) {
+      console.error('Invalid topic format:', topic)
+      return
+    }
+    const apiId = parts[1] // Extract the API ID from the topic
+    const level = levelTable[parts[2]] // Extract the log level from the topic
+    const value = JSON.parse(message.toString())
+
+    context.state.set({ group: 'logs', tag: apiId, name: 'last', value: value, emitEvent: true});
+    context.state.append({ group: 'logs', tag: apiId, name: level, value: value, emitEvent: true});
+  }
+  context.topicSub(context.mqtt, 'logs/#', (message, topic) => processMessage(message, topic))
 }
