e9ce3680fd41df73d539338748d87f2f3ba56ea6
miquel <miquel@protofy.xyz>
2025-06-16 12:56:26 +0200

    Updates assets system

--- assets.js
+++ assets.js
@@ -2,24 +2,6 @@ const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
-const assetName = process.argv[2];
-if (!assetName) {
-    console.error('Please provide the asset name as an argument.');
-    process.exit(1);
-}
-const sourceDir = path.join(__dirname,'data', 'assets', assetName);
-
-if (!fs.existsSync(sourceDir)) {
-    console.error(`Asset "${assetName}" does not exist in the source directory.`);
-    process.exit(1);
-}
-
-const targetDir = path.join(__dirname);
-
-copyFolderStructure(sourceDir, targetDir);
-
-execSync(`yarn`, { stdio: 'inherit' });
-
 function copyFolderStructure(sourceDir, targetDir) {
     if (!fs.existsSync(sourceDir)) {
         console.error(`Source folder "${sourceDir}" does not exist.`);
@@ -48,3 +30,47 @@ function copyFolderStructure(sourceDir, targetDir) {
         }
     }
 }
+
+function installAsset(assetName) {
+    if (!assetName) {
+        console.error('Please provide the asset name as an argument.');
+        process.exit(1);
+    }
+    const sourceDir = path.join(__dirname, 'data', 'assets', assetName);
+
+    if (!fs.existsSync(sourceDir)) {
+        console.error(`Asset "${assetName}" does not exist in the source directory.`);
+        process.exit(1);
+    }
+
+    const targetDir = path.join(__dirname);
+
+    copyFolderStructure(sourceDir, targetDir);
+    execSync(`yarn`, { stdio: 'inherit' });
+}
+
+function packageAsset(assetName) {
+
+}
+
+// check if directly run or is a module
+if (require.main === module) {
+    const command = process.argv[2];
+    const assetName = process.argv[3];
+
+    if (!assetName) {
+        console.error('Please provide the asset name as an argument.');
+        process.exit(1);
+    }
+
+    if (command == 'install') {
+        installAsset(assetName);
+    } else if (command === 'package') {
+        packageAsset(assetName);
+    }
+}
+
+module.exports = {
+    install: installAsset,
+    package: packageAsset
+};
