b28b720a121bd2cd6c3869a1978d3d4665d4f8bf
lluis@protofy.xyz <lluis@protofy.xyz>
2025-06-18 16:09:44 +0200

    Add chatGPT api, and cards

---
+++ extensions/chatgpt/apis.ts
@@ -0,0 +1,95 @@
+import { chatGPTPrompt } from "./coreContext"
+import { addAction } from "@extensions/actions/coreContext/addAction";
+import { addCard } from "@extensions/cards/coreContext/addCard";
+import { getLogger, getServiceToken } from 'protobase';
+import { handler } from 'protonode'
+
+
+
+export default (app, context) => {
+
+
+    const registerActions = async (context) => {
+        addAction({
+            group: 'chatGPT',
+            name: 'message',
+            url: `/api/v1/chatgpt/send/prompt`,
+            tag: "send",
+            description: "send a chatGPT prompt",
+            params: { prompt: "message value to send" },
+            emitEvent: true,
+            token: await getServiceToken()
+        })
+    }
+
+    const registerCards = async (context) => {
+        addCard({
+            group: 'chatGPT',
+            tag: "chat",
+            id: 'chatGPT__chat_response',
+            templateName: "chatGPT last chat response",
+            name: "response",
+            defaults: {
+                name: "chatGPT_last_chat_response",
+                icon: "openai",
+                color: "#FFFFFF",
+                description: "chatGPT last chat response",
+                rulesCode: `return states?.chatGPT?.conversation?.chatResponse`,
+                type: 'value',
+                html: `
+                //data contains: data.value, data.icon and data.color
+                return card({
+                    content: \`
+                        \${icon({ name: data.icon, color: data.color, size: '48' })}    
+                        \${cardValue({ value: data.value, style: "font-size: 20px; font-weight: 400; margin-top: 10px" })}
+                    \`
+                });
+                `
+            },
+            emitEvent: true,
+            token: await getServiceToken()
+        })
+
+            addCard({
+        group: 'chatGPT',
+        tag: "message",
+        id: 'chatGPT_message_send',
+        templateName: "chatGPT send message",
+        name: "send_message",
+        defaults: {
+            name: "chatGPT_message_send",
+            icon: "openai",
+            color: "#FFFFFF",
+            description: "send a message to chatGPT",
+            rulesCode: `return execute_action("/api/v1/chatgpt/send/prompt", { message: userParams.message});`,
+            params: { message: "message",  },
+            type: 'action'
+        },
+        emitEvent: true,
+        token: await getServiceToken()
+    })
+    }
+
+    app.get("/api/v1/chatgpt/send/prompt", handler(async (req, res, session) => {
+        if (!session || !session.user.admin) {
+            res.status(401).send({ error: "Unauthorized" })
+            return
+        }
+        const { message } = req.query;
+        if(!message){
+            res.status(400).send({ error: "Message parameter is required" });
+            return;
+        }
+        chatGPTPrompt({
+            message: message, done: (response, msg) => {
+                context.state.set({ group: 'chatGPT', tag: "conversation", name: "userMessage", value: message, emitEvent: true });
+                context.state.set({ group: 'chatGPT', tag: "conversation", name: "chatResponse", value: msg, emitEvent: true });                
+            }
+        })
+        res.json({ success: true, message: "Prompt sent to ChatGPT" });
+    }))
+    registerActions(context);
+    registerCards(context);
+
+}
+
