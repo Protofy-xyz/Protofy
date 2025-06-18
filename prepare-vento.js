243f1aee12bba2a8fa386833d66535b7da82f080
jcarlosn <jcarlos.norte@gmail.com>
2025-06-19 01:13:01 +0200

    generate Vento electron build at ../dist, to be able to move the project later to ../dist/project

--- prepare-vento.js
+++ prepare-vento.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const rimraf = require('rimraf');
+const { exec } = require('child_process');
 
 const packageVentoPath = path.join(__dirname, 'package-vento.json');
 const packagePath = path.join(__dirname, 'package.json');
@@ -34,4 +35,74 @@ exec('yarn', (error, stdout, stderr) => {
         return;
     }
     console.log(`Yarn stdout: ${stdout}`);
+});
+
+//remove apps/adminpanel/.next
+const nextPath = path.join(__dirname, 'apps', 'adminpanel', '.next');
+if (fs.existsSync(nextPath)) {
+    rimraf.sync(nextPath);
+    console.log('.next directory in apps/adminpanel has been removed');
+}
+
+//remove apps/adminpanel/out
+
+const outPath = path.join(__dirname, 'apps', 'adminpanel', 'out');
+if (fs.existsSync(outPath)) {
+    rimraf.sync(outPath);
+    console.log('out directory in apps/adminpanel has been removed');
+}
+
+
+//check if its running on windows and remove bin/node-linux and bin/node-macos
+const binPathLinux = path.join(__dirname, 'bin', 'node-linux');
+const binPathMacos = path.join(__dirname, 'bin', 'node-macos');
+if (process.platform === 'win32') {
+    if (fs.existsSync(binPathLinux)) {
+        rimraf.sync(binPathLinux);
+        console.log('bin/node-linux has been removed');
+    }
+    if (fs.existsSync(binPathMacos)) {
+        rimraf.sync(binPathMacos);
+        console.log('bin/node-macos has been removed');
+    }
+}
+
+//convert git to a shallow clone, remove .git/objects and .git/refs
+const gitObjectsPath = path.join(__dirname, '.git', 'objects');
+const gitRefsPath = path.join(__dirname, '.git', 'refs');
+if (fs.existsSync(gitObjectsPath)) {
+    rimraf.sync(gitObjectsPath);
+    console.log('.git/objects has been removed');
+}
+if (fs.existsSync(gitRefsPath)) {
+    rimraf.sync(gitRefsPath);
+    console.log('.git/refs has been removed');
+}
+
+//run git init
+const gitInitCommand = 'git init';
+exec(gitInitCommand, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Error executing git init: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.error(`Git init stderr: ${stderr}`);
+        return;
+    }
+    console.log(`Git init stdout: ${stdout}`);
+});
+
+//run git fetch --depth=1 origin main
+const gitFetchCommand = 'git fetch --depth=1 origin main';
+exec(gitFetchCommand, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Error executing git fetch: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.error(`Git fetch stderr: ${stderr}`);
+        return;
+    }
+    console.log(`Git fetch stdout: ${stdout}`);
 });
