254bdcb5d4fa376a293628f9e68c88eca3bd5c84
miquel <miquel@protofy.xyz>
2025-06-19 18:11:29 +0200

    Changes collapsed side menu logo

--- packages/app/layout/SideMenu.tsx
+++ packages/app/layout/SideMenu.tsx
@@ -1,5 +1,5 @@
 import { SideMenu as ProtoSideMenu } from 'protolib/components/layout/SideMenu'
-import { Image, useThemeName } from '@my/ui';
+import { Image, useThemeName, YStack } from '@my/ui';
 
 export const SideMenu = (props) => {
     const themeName = useThemeName()
@@ -15,19 +15,17 @@ export const SideMenu = (props) => {
             resizeMode='contain'
         />}
         collapsedLogo={
-            <Image
-                key={themeName}
-                style={{ filter: themeName?.startsWith("dark") ? "invert(70%) brightness(10)" : "invert(5%)" }}
-                src={"/public/logo-y.png"}
-                alt="Logo"
-                width={30}
-                height={30}
-                resizeMode='contain'
-                // @ts-ignore
-                animation="bouncy"
-                enterStyle={{ rotate: '180deg' }}
-                exitStyle={{ rotate: '180deg' }}
-            />
+            <YStack pt={5} pl={4}>
+                <Image
+                    key={themeName}
+                    style={{ filter: themeName?.startsWith("dark") ? "invert(70%) brightness(10)" : "invert(5%)" }}
+                    src={"/public/vento-square.png"}
+                    alt="Logo"
+                    width={22}
+                    height={22}
+                    resizeMode='contain'
+                />
+            </YStack>
         }
         {...props}
     />
