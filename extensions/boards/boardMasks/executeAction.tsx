8fbf1ffe9036be21285c77381643fdbf5dc5a3c1
jcarlosn <jcarlos.norte@gmail.com>
2025-06-27 00:15:36 +0200

    Use flows in boards automations

---
+++ extensions/boards/boardMasks/executeAction.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { MaskDefinition, buildAutoMask } from 'protolib/components/GenericMask';
+
+const executeActionMask: MaskDefinition = {
+  from: 'board',
+  id: 'executeAction',
+  title: 'Execute Action',
+  category: 'Board',
+  keywords: ['board', 'execute', 'action', 'execute_action'],
+  context: 'board.execute_action',
+  icon: 'globe',
+  params: {
+    name: {
+      type: 'input',
+      label: 'action name',
+      initialValue: { value: '', kind: 'StringLiteral' },
+    },
+    params: {
+      type: 'input',
+      label: 'params',
+      initialValue: { value: '{}', kind: 'Identifier' },
+    }
+  },
+};
+
+export default buildAutoMask(executeActionMask);
