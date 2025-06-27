98708c9559d93e43071da7f6b737aa9b64de8c24
jcarlosn <jcarlos.norte@gmail.com>
2025-06-27 17:27:30 +0200

    Rename atuomation box to action

--- extensions/automations/masks/Automation.tsx
+++ extensions/automations/masks/Automation.tsx
@@ -11,7 +11,7 @@ const AutomationNode = ({ node = {}, nodeData = {}, children }: any) => {
     const [loading, setLoading] = React.useState(false)
 
     return (
-        <Node icon={Plug} node={node} isPreview={!node.id} title='Automation' color={color} id={node.id} skipCustom={true}>
+        <Node icon={Plug} node={node} isPreview={!node.id} title='Action' color={color} id={node.id} skipCustom={true}>
             <NodeParams id={node.id} params={[
                 { label: 'Name', field: 'mask-name', type: 'input' },
                 { label: 'Description', field: 'mask-description', type: 'input' },
