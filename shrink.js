e2b48f6aa5aa2b9e19316e5840d4b93966f34017
jcarlosn <jcarlos.norte@gmail.com>
2025-06-28 02:26:43 +0200

    Recover package-vento

--- shrink.js
+++ shrink.js
@@ -6,6 +6,17 @@ const path = require('path');
 const rimraf = require('rimraf');
 const { execSync } = require('child_process');
 
+const packageVentoPath = path.join(__dirname, 'package-vento.json');
+const packagePath = path.join(__dirname, 'package.json');
+// Check if package-vento.json exists
+if (fs.existsSync(packageVentoPath)) {
+    // Read the contents of package-vento.json
+    const packageVentoContent = fs.readFileSync(packageVentoPath, 'utf8');
+    // Write the contents to package.json
+    fs.writeFileSync(packagePath, packageVentoContent, 'utf8');
+    console.log('package-vento.json has been moved to package.json');
+}
+
 // Remove node_modules directory
 const nodeModulesPath = path.join(__dirname, 'node_modules');
 if (fs.existsSync(nodeModulesPath)) {
