fb1aaf2e4fc2747961fda9e94e85bffe0ab002e7
jcarlosn <jcarlos.norte@gmail.com>
2025-06-28 02:59:27 +0200

    Adjust proxy-main for osx

--- proxy-main.js
+++ proxy-main.js
@@ -5,7 +5,7 @@ const projectDir = 'project';
 const Module = require('module');
 
 //check if osx
-const MONOREPO_ROOT = path.resolve(__dirname, (process.platform === 'darwin' ? '../../../../' : '../../')+projectDir)
+const MONOREPO_ROOT = path.resolve(process.platform === 'darwin' ? process.execPath + '../../../../' : __dirname+ '../../'+projectDir)
 let ENTRYPOINT = path.join(MONOREPO_ROOT, 'main.js')
 
 if (!fs.existsSync(ENTRYPOINT)) { //if the entrypoint is not found in the project root, try to find it in the current directory
