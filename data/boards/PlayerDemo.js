5b029a4db5848ee51d5fe3b24c2bd3b8c345fad7
jcarlosn <jcarlos.norte@gmail.com>
2025-07-03 13:47:33 +0200

    Removes noisy console

--- data/boards/PlayerDemo.js
+++ data/boards/PlayerDemo.js
@@ -3,33 +3,32 @@ const { Protofy } = require('protobase')
 
 const run = Protofy("code",
     async ({ states, board }) => {
+    board.execute_action({
+        name: 'time',
+        params: {
+            time: 1
+        }
+    })
+
+    board.execute_action({
+        name: 'system_state',
+        params: {
+            name: 'stop'
+        }
+    })
+
+    setInterval(() => {
+        if(states.system_state == 'play') {
+            let time = parseInt(states.time, 10)
+            time = time + 1
             board.execute_action({
                 name: 'time',
                 params: {
-                    time: 1
+                    time: time
                 }
             })
-
-            board.execute_action({
-                name: 'system_state',
-                params: {
-                    name: 'stop'
-                }
-            })
-
-            setInterval(() => {
-                console.log('staaaaate: ', states)
-                if(states.system_state == 'play') {
-                    let time = parseInt(states.time, 10)
-                    time = time + 1
-                    board.execute_action({
-                        name: 'time',
-                        params: {
-                            time: time
-                        }
-                    })
-                }
-            }, 1000)
+        }
+    }, 1000)
     }
 )
 
