46bab7cb85fc7962d5ed3dfe1a827168d1d1c5fd
jcarlosn <jcarlos.norte@gmail.com>
2025-07-06 13:25:25 +0200

    Removes legacy, broken and incorrectly configured test infrastructure.

--- apps/core/jest/jest-globals.d.ts
+++
@@ -1,9 +0,0 @@
-// jest-globals.d.ts
-declare global {
-    function describeLongTest(name: string, fn: () => void): void;
-    namespace describeLongTest {
-      var skip: (name: string, fn: () => void) => void;
-    }
-  }
-  
-  export {};
