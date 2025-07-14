1a10f2fd9630a53b709cdd902bddb5a375a14585
DenshiNingen <denshi.ningen@gmail.com>
2025-07-14 19:07:03 +0200

    Refactors SEN0377

--- packages/protodevice/src/device/SEN0377.ts
+++ packages/protodevice/src/device/SEN0377.ts
@@ -1,65 +1,117 @@
 class SEN0377 {
     name;
-    platform;
     address;
-    updateInterval;
     i2cBusId;
-    sclPin;
-    mqttTopicPrefix;
-    constructor(name, platform, address, updateInterval, i2cBusId, sclPin) {
+    updateInterval;
+    platform;
+
+    constructor(name, address, i2cBusId, updateInterval) {
         this.name = name
-        this.platform = platform
+        this.platform = "mics_4514"
         this.address = address
         this.updateInterval = updateInterval
         this.i2cBusId = i2cBusId
-        this.sclPin = sclPin
-        this.mqttTopicPrefix = ''
     }
-
-    getPlatform() {
-        return this.platform;
-    }
-
-    setMqttTopicPrefix(setMqttTopicPrefix) {
-        this.mqttTopicPrefix = setMqttTopicPrefix;
-    }
-
-    attach(pin) {
-        return [
+    
+    attach(pin, deviceComponents) {
+        let componentObjects = [
             {
-                componentName: "i2c",
-                payload: `    sda: ${pin}
-    scl: ${this.sclPin}
-    #scan: true
-    id: ${this.i2cBusId}
-`
+                name: "sensor",
+                config: {
+                    platform: this.platform,
+                    address: this.address,
+                    i2c_id: this.i2cBusId,
+                    update_interval: this.updateInterval,
+                    nitrogen_dioxide: {
+                        name: this.name + "_nitrogen_dioxide"
+                    },
+                    carbon_monoxide: {
+                        name: this.name + "_carbon_monoxide"
+                    },
+                    hydrogen: {
+                        name: this.name + "_hydrogen"
+                    },
+                    ethanol: {
+                        name: this.name + "_ethanol"
+                    },
+                    methane: {
+                        name: this.name + "_methane"
+                    },
+                    ammonia: {
+                        name: this.name + "_ammonia"
+                    }
+
+                },
+                subsystem: this.getSubsystem()
             },
-            {
-                componentName: `sensor`,
-                payload: 
-`    - platform: ${this.platform} 
-      id: ${this.name}
-      address: ${this.address}
-      update_interval: ${this.updateInterval}
-      nitrogen_dioxide:
-        name: ${this.name}-nitrogen-dioxide
-      carbon_monoxide:
-        name: ${this.name}-carbon-monoxide
-      hydrogen:
-        name: ${this.name}-hydrogen
-      ethanol:
-        name: ${this.name}-ethanol
-      methane:
-        name: ${this.name}-methane
-      ammonia:
-        name: ${this.name}-ammonia
-`
+        ]
+
+        componentObjects.forEach((element, j) => {
+            if (!deviceComponents[element.name]) {
+                deviceComponents[element.name] = element.config
+            } else {
+                if (!Array.isArray(deviceComponents[element.name])) {
+                    deviceComponents[element.name] = [deviceComponents[element.name]]
+                }
+                deviceComponents[element.name] = [...deviceComponents[element.name], element.config]
             }
-        ];
+        })
+        return deviceComponents
+    }
+
+    getSubsystem() {
+        let monitors = [{
+            label: `Nitrogen Dioxide`,
+            name: `nitrogen_dioxide`,
+            description: `Get nitrogen dioxide level`,
+            endpoint: "/sensor/" +this.name + "_nitrogen_dioxide"+ "/state",
+            connectionType: "mqtt",
+        },
+        {
+            label: `Carbon Monoxide`,
+            name: `carbon_monoxide`,
+            description: `Get carbon monoxide level`,
+            endpoint: "/sensor/" +this.name + "_carbon_monoxide"+ "/state",
+            connectionType: "mqtt",
+        },
+        {
+            label: `Hydrogen`,
+            name: `hydrogen`,
+            description: `Get hydrogen level`,
+            endpoint: "/sensor/" +this.name + "_hydrogen"+ "/state",
+            connectionType: "mqtt",
+        },
+        {
+            label: `Ethanol`,
+            name: `ethanol`,
+            description: `Get ethanol level`,
+            endpoint: "/sensor/" +this.name + "_ethanol"+ "/state",
+            connectionType: "mqtt",
+        },
+        {
+            label: `Methane`,
+            name: `methane`,
+            description: `Get methane level`,
+            endpoint: "/sensor/" +this.name + "_methane"+ "/state",
+            connectionType: "mqtt",
+        },
+        {
+            label: `Ammonia`,
+            name: `ammonia`,
+            description: `Get ammonia level`,
+            endpoint: "/sensor/" +this.name + "_ammonia"+ "/state",
+            connectionType: "mqtt",
+        }]
+        
+        return {
+            name: this.name,
+            type: "sensor",
+            monitors: monitors
+        }
     }
 }
 
 
-export function sen0377(name, sclPin, address, updateInterval) {
-    return new SEN0377(name, 'mics_4514', address, updateInterval, "bus_a", sclPin)
+export function sen0377(name, address, i2cBusId, updateInterval) {
+    return new SEN0377(name, address, i2cBusId, updateInterval)
 }
