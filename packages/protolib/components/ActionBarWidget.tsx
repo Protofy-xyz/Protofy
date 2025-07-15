a0a99a537d1b5521cba17c3ffa619c68f13551a6
miquel <miquel@protofy.xyz>
2025-07-15 09:57:46 +0200

    Adds enter animation to action bar widget

--- packages/protolib/components/ActionBarWidget.tsx
+++ packages/protolib/components/ActionBarWidget.tsx
@@ -20,21 +20,35 @@ export const ActionBarButton = ({ Icon, beating = false, ...props }) => {
 }
 
 export const useActionBar = (actionBar?, onActionBarEvent?) => {
-    const router = useRouter()
+  const router = useRouter()
 
-    let currentBar
+  let currentBar
 
-    if (actionBar) {
-        currentBar = actionBar
-    } else {
-        currentBar = processActionBar(router, actionBar, onActionBarEvent)
+  if (actionBar) {
+    currentBar = actionBar
+  } else {
+    currentBar = processActionBar(router, actionBar, onActionBarEvent)
+  }
+
+  return currentBar && currentBar.content?.length > 0 && <XStack
+    p="$2"
+    als="center"
+    pos="fixed"
+    elevation={10}
+    bw={1}
+    boc="$gray6"
+    animation="quick"
+    bc="$bgPanel"
+    zi={99999}
+    b={currentBar.visible === false ? -200 : 16}
+    gap="$3"
+    br="$5"
+    enterStyle={{ b: -200 }}
+  >
+    {
+      currentBar.content?.map((item, index) => {
+        return item
+      })
     }
-
-    return currentBar && currentBar.content?.length > 0 && <XStack p="$2" als="center" pos="fixed" elevation={10} bw={1} boc="$gray6" animation="quick" bc="$bgPanel" zi={99999} b={currentBar.visible === false ? -200 : 16} gap="$3" br="$5">
-        {
-            currentBar.content?.map((item, index) => {
-                return item
-            })
-        }
-    </XStack>
+  </XStack>
 }   
