3fe181c40221f1b2bdf2b685907ade5e89d7fb06
Noemi Blazquez <ap0calipse@gmail.com>
2025-07-16 14:57:13 +0200

    Makes interactive icon more clickable

--- packages/protolib/components/InteractiveIcon.tsx
+++ packages/protolib/components/InteractiveIcon.tsx
@@ -1,5 +1,4 @@
-
-import { XStack, Stack, StackProps } from '@my/ui';
+import { XStack, StackProps } from '@my/ui';
 
 export const InteractiveIcon = ({ Icon, IconColor = 'var(--color8)', DisabledIconColor = 'var(--gray9)', disabled = false, size = 18, ...props }: { IconColor?: string, DisabledIconColor?: string, Icon: any, size?: number } & StackProps) => {
   let mode = "react"
@@ -8,32 +7,41 @@ export const InteractiveIcon = ({ Icon, IconColor = 'var(--color8)', DisabledIco
   }
 
   return (
-    <XStack cursor={disabled ? "dafault" : "pointer"} {...props}>
+    <XStack
+      {...props}
+      cursor={disabled ? 'default' : 'pointer'}
+      ai="center"
+      jc="center"
+      br="$5"
+      p="$2"
+      o={0.8}
+      als="flex-start"
+      hoverStyle={!disabled ? { o: 1, bc: '$color5' } : {}}
+      pressStyle={!disabled ? { o: 0.7 } : {}}
+    >
+      {props.children}
 
-      <XStack
-        ai='center' jc='center'
-        o={0.8}
-        br={"$5"} p={"$2"}
-        als="flex-start" cursor={disabled ? "dafault" : "pointer"}
-        pressStyle={!disabled ? { o: 0.7 } : {}}
-        hoverStyle={!disabled ? { o: 1, bc: "$color5" } : {}
-        }>
-                {props.children}
-        {mode == "react" && <Icon size={size} color={disabled ? DisabledIconColor : IconColor} strokeWidth={2} />}
-        {mode == "string" && <div style={{
-                    width: size+"px",
-                    height: size+"px",
-                    backgroundColor: IconColor,
-                    maskImage: `url(/public/icons/${Icon}.svg)`,
-                    WebkitMaskImage: `url(/public/icons/${Icon}.svg)`,
-                    maskRepeat: "no-repeat",
-                    WebkitMaskRepeat: "no-repeat",
-                    maskSize: "contain",
-                    WebkitMaskSize: "contain",
-                    maskPosition: "center",
-                    WebkitMaskPosition: "center"
-                }} />}
-      </XStack>
+      {mode === 'react' && (
+        <Icon size={size} color={disabled ? DisabledIconColor : IconColor} strokeWidth={2} />
+      )}
+
+      {mode === 'string' && (
+        <div
+          style={{
+            width: size + 'px',
+            height: size + 'px',
+            backgroundColor: disabled ? DisabledIconColor : IconColor,
+            maskImage: `url(/public/icons/${Icon}.svg)`,
+            WebkitMaskImage: `url(/public/icons/${Icon}.svg)`,
+            maskRepeat: 'no-repeat',
+            WebkitMaskRepeat: 'no-repeat',
+            maskSize: 'contain',
+            WebkitMaskSize: 'contain',
+            maskPosition: 'center',
+            WebkitMaskPosition: 'center',
+          }}
+        />
+      )}
     </XStack>
   );
 };
