08545985629ac3b83719d4e41a402ccc713fdcbc
jcarlosn <jcarlos.norte@gmail.com>
2025-07-19 00:32:46 +0200

    split shrink into a script to remove dev mode and shrink itself

--- scripts/shrink.js
+++ scripts/shrink.js
@@ -8,30 +8,6 @@ const { execSync } = require('child_process');
 
 const dirname = path.join(__dirname, '..')
 
-
-const packagePath = path.join(dirname, 'package.json');
-const workspaces = [
-    "apps/api",
-    "apps/core",
-    "packages/app",
-    "packages/config",
-    "packages/protobase",
-    "packages/protodevice",
-    "packages/protonode",
-    "extensions/*",
-    "scripts"
-]
-// load package.json and replace workspaces with the ones we want to keep
-if (fs.existsSync(packagePath)) {
-    const packageJson = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
-    packageJson.workspaces = workspaces;
-    fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2), 'utf8');
-    console.log('package.json workspaces have been updated');
-} else {
-    console.error('package.json not found');
-    process.exit(1);
-}
-
 // Remove node_modules directory
 //only if not osx
 if (process.platform === 'darwin') {
