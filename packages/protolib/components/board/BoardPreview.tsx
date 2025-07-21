f258f2baebb3d97411f3139c57339d7669262d64
lluis@protofy.xyz <lluis@protofy.xyz>
2025-07-21 17:45:15 +0200

    Add form of settings in boards view

--- packages/protolib/components/board/BoardPreview.tsx
+++ packages/protolib/components/board/BoardPreview.tsx
@@ -1,18 +1,19 @@
-import { YStack, Text, XStack, Tooltip, Paragraph, Dialog } from '@my/ui';
+import { YStack, Text, XStack, Tooltip, Paragraph, Dialog, Label, Input, Button} from '@my/ui';
 import { Tinted } from '../Tinted';
-import { Sparkles, Pencil } from "@tamagui/lucide-icons";
+import { Sparkles, Cog, Type } from "@tamagui/lucide-icons";
 import { BoardModel } from '@extensions/boards/boardsSchemas';
 import { useRouter } from 'solito/navigation';
 import { getIconUrl } from '../IconSelect';
 import { ItemMenu } from '../ItemMenu';
 import { useState } from 'react';
+import { API, setErrorMap } from 'protobase';
 
 export default ({ element, width, onDelete, ...props }: any) => {
     const board = new BoardModel(element);
     const [editSettingsDialog, seteditSettingsDialog] = useState(false);
     const [selectedBoard, setSelectedBoard] = useState(null);
     const router = useRouter();
-    console.log("Board", board);
+    // console.log("Board", board);
 
     return (
         <YStack
@@ -28,11 +29,11 @@ export default ({ element, width, onDelete, ...props }: any) => {
             gap="$4"
             {...props}
         >
-           
+
             <XStack jc={"space-between"} ai={"start"} >
                 <XStack gap="$2" ai={"start"} >
                     <YStack>
-                        <Text fos="$8" fow="600" >{board?.get("displayName")?? board?.get("name")}</Text>
+                        <Text fos="$8" fow="600" >{board?.get("displayName") ?? board?.get("name")}</Text>
                         <Text fos="$2" fow="600" >{board?.get("name")}</Text>
                     </YStack>
                 </XStack>
@@ -45,14 +46,14 @@ export default ({ element, width, onDelete, ...props }: any) => {
                         element={board}
                         deleteable={() => true}
                         onDelete={onDelete}
-                        // extraMenuActions={[
-                        //     {
-                        //         text: "Change display name",
-                        //         icon: Pencil,
-                        //         action: (element) => { console.log("pressed: ",element); seteditSettingsDialog(true); setSelectedBoard(element)},
-                        //         isVisible: (element) => true
-                        //     }
-                        // ]}
+                        extraMenuActions={[
+                            {
+                                text: "Settings",
+                                icon: Cog,
+                                action: (element) => { console.log("pressed: ", element); seteditSettingsDialog(true); setSelectedBoard(element) },
+                                isVisible: (element) => true
+                            }
+                        ]}
                     />
                 </XStack>
             </XStack>
@@ -61,7 +62,7 @@ export default ({ element, width, onDelete, ...props }: any) => {
                 {
                     board?.get("cards")?.length
                         ? <XStack gap="$2" f={1} mah={"$9"} flexWrap="wrap" overflow="scroll">
-                            {board.get("cards")?.filter(i=>i).map((card: any, index: number) => (
+                            {board.get("cards")?.filter(i => i).map((card: any, index: number) => (
                                 <Tinted key={card.name}>
                                     <Tooltip>
                                         <Tooltip.Trigger>
@@ -71,7 +72,7 @@ export default ({ element, width, onDelete, ...props }: any) => {
                                                 br={card.type == "action" ? "$10" : "$2"}
                                                 jc={"center"}
                                                 ai={"center"}
-                                                bc={card.color?? "$color6"}
+                                                bc={card.color ?? "$color6"}
                                             >
                                                 <img
                                                     src={getIconUrl(card.icon)}
@@ -110,14 +111,43 @@ export default ({ element, width, onDelete, ...props }: any) => {
                         : <Text color={"$color9"}>No rules added yet</Text>
                 }
             </YStack>
-            
+
             <Dialog open={editSettingsDialog} onOpenChange={seteditSettingsDialog}>
                 <Dialog.Portal className='DialogEditDisplayName'>
-                    <Dialog.Overlay className='DialogEditDisplayName'/>
-                    <Dialog.Content overflow="hidden" p={"$8"} height={'600px'} width={"600px"} className='DialogEditDisplayName'>
-                        <Text fos="$8" fow="600" className='DialogEditDisplayName'>Edit the display name</Text>
-                        <Text className='DialogEditDisplayName'>{selectedBoard?.name}</Text>
-
+                    <Dialog.Overlay className='DialogEditDisplayName' />
+                    <Dialog.Content overflow="hidden" p={"$8"} height={'400px'} width={"400px"} className='DialogEditDisplayName'>
+                        <YStack height="100%" justifyContent="space-between">
+                            <Text fos="$8" fow="600" mb="$3" className='DialogEditDisplayName'>Settings</Text>
+                            <XStack ai={"center"} className='DialogEditDisplayName'>
+                                <Label ml={"$2"} h={"$3.5"} size={"$5"} className='DialogEditDisplayName'> <Type color={"$color8"} mr="$2" />Display Name</Label>
+                            </XStack>
+                            <Input
+                                br={"8px"}
+                                className='DialogEditDisplayName'
+                                value={selectedBoard?.data.displayName}
+                                // color={error ? '$red9' : null}
+                                onChange={(e) => {
+                                    setSelectedBoard({
+                                        data: {
+                                            ...selectedBoard.data,
+                                            displayName: e.target.value
+                                        }
+                                    })
+                                }
+                                }
+                            />
+                            <YStack flex={1} className='DialogEditDisplayName' />
+                            <Button className='DialogEditDisplayName' onPress={async () => {
+                                try {
+                                    await API.post(`/api/core/v1/boards/${selectedBoard?.data?.name}`, selectedBoard.data)
+                                    setSelectedBoard(null);
+                                    seteditSettingsDialog(false);
+                                } catch (e) {
+                                    console.log('e: ', e)
+                                }
+                            }}>Save
+                            </Button>
+                        </YStack>
                         <Dialog.Close />
                     </Dialog.Content>
                 </Dialog.Portal>
