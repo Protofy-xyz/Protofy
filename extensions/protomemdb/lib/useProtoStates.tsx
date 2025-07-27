d5b824d29df1136a3ab0b481fc5354adfca3eff3
jcarlosn <jcarlos.norte@gmail.com>
2025-07-28 00:27:42 +0200

    Update cards using the mqtt inforamtion without refetching the entire states object

--- extensions/protomemdb/lib/useProtoStates.tsx
+++ extensions/protomemdb/lib/useProtoStates.tsx
@@ -1,41 +1,43 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import { API } from 'protobase'
 import { useEventEffect } from '@extensions/events/hooks/useEventEffect'
 
-export const useProtoStates = (initialState, filter='states/#', chunk='states') => {
+function setNestedValue(obj, path, value, chunk) {
+  if (path.length === 0) {
+    return chunk === 'states' ? value.value : value;
+  }
+  const [key, ...rest] = path;
+  return {
+    ...obj,
+    [key]: setNestedValue(obj?.[key] ?? {}, rest, value, chunk)
+  };
+}
+
+export const useProtoStates = (initialState, filter = 'states/#', chunk = 'states') => {
   const [state, setState] = useState(initialState);
-  const timer = useRef(null);
 
   const readState = (chunk) => {
-    API.get("/api/core/v1/protomemdb/"+chunk)
+    API.get("/api/core/v1/protomemdb/" + chunk)
       .then((response) => setState(response.data))
       .catch((error) => console.error("Error al obtener datos:", error));
   };
 
-  // Llamada inicial a `readState`
   useEffect(() => {
     readState(chunk);
-    return () => {
-      if (timer.current) {
-        clearTimeout(timer.current);
-        timer.current = null;
-      }
-    };
-  }, []);
+  }, [chunk]);
 
-  //skip subscription if filter is null or undefined
-  if(filter === null || filter === undefined) {
-    return state;
-  }
+  if (filter == null) return state;
 
   useEventEffect(
     (payload, msg) => {
-      if (timer.current) return;
-
-      timer.current = setTimeout(() => {
-        readState(chunk);
-        timer.current = null;
-      }, 500);
+      if (msg.topic.endsWith('/update')) {
+        const path = msg.parsed.path?.split('/').slice(0, -1);
+        if (Array.isArray(path) && path.length > 1 && path[0] === chunk) {
+          setState(prev =>
+            setNestedValue(prev, path.slice(1), msg.parsed.payload, chunk)
+          );
+        }
+      }
     },
     { path: filter }
   );
