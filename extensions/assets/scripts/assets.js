258f1e04167cdfa5f3aa6bd3b32e76d88d4ec62a
lluis@protofy.xyz <lluis@protofy.xyz>
2025-07-29 12:31:48 +0200

    adjust asset creation script for new locations

--- extensions/assets/scripts/assets.js
+++ extensions/assets/scripts/assets.js
@@ -31,7 +31,7 @@ if (!assetName) {
     console.error('Please provide an asset name as a parameter.');
     process.exit(1);
 }
-const assetsReleasePath = path.join(__dirname, '../../../assets-release.json');
+const assetsReleasePath = path.join(__dirname, '../../../data/assets/', assetName + '/.vento/asset.json');
 const assetsReleaseContent = [
     {
         "name": assetName,
@@ -39,6 +39,12 @@ const assetsReleaseContent = [
         ]
     }
 ];
+// create the directory if it does not exist
+const dir = path.dirname(assetsReleasePath);
+if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+}
+// write the content to the file
 fs.writeFileSync(assetsReleasePath, JSON.stringify(assetsReleaseContent, null, 4));
 console.log(`Created ${assetsReleasePath} with content:`, JSON.stringify(assetsReleaseContent, null, 4));
 // print the route wher the file was created
