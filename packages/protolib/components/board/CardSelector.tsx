08e13457fdc6fdf26ee9358c5741ac8140ad44a7
jcarlosn <jcarlos.norte@gmail.com>
2025-07-31 01:04:14 +0200

    Value cards are now react

--- packages/protolib/components/board/CardSelector.tsx
+++ packages/protolib/components/board/CardSelector.tsx
@@ -165,14 +165,25 @@ const SecondSlide = ({ card, board, selected, states, icons, actions, setCard, e
 }
 
 const typeCodes = {
-  value: `
-//data contains: data.value, data.icon and data.color
-return card({
-    content: \`
-        \${cardIcon({ data, size: '48' })}    
-        \${cardValue({ value: data.value })}
-    \`
-});
+  value: `//@card/react
+
+function Widget(card) {
+  const value = card.value;
+  return (
+      <Tinted>
+        <ProtoThemeProvider forcedTheme={window.TamaguiTheme}>
+            <YStack f={1} height="100%" ai="center" jc="center" width="100%">
+                {card.icon && card.displayIcon !== false && (
+                    <Icon name={card.icon} size={48} color={card.color}/>
+                )}
+                {card.displayResponse !== false && (
+                    <CardValue value={value ?? "N/A"} />
+                )}
+            </YStack>
+        </ProtoThemeProvider>
+      </Tinted>
+  );
+}
 `,
   action: `//@card/react
 
