3a878271dc5d9bed7bfe902c41270d2d1b732168
miquel <miquel@protofy.xyz>
2025-07-31 17:06:26 +0200

    Adds tooltip to action bar button

--- packages/protolib/components/ActionBarWidget.tsx
+++ packages/protolib/components/ActionBarWidget.tsx
@@ -1,4 +1,4 @@
-import { XStack, YStack } from "@my/ui"
+import { TooltipSimple, XStack, YStack } from "@my/ui"
 import { processActionBar } from "app/bundles/actionBar"
 import { useRouter } from 'next/router'
 import { useState } from "react"
@@ -6,19 +6,21 @@ import { ArrowDown, ArrowUp, ChevronUp } from "@tamagui/lucide-icons"
 
 export const ActionBarButton = ({ Icon, selected = false, ...props }) => {
   const size = 34
-  return <YStack
-    jc="center"
-    ai="center"
-    br="$4"
-    cursor='pointer'
-    scaleIcon={1.8}
-    w={size}
-    h={size}
-    hoverStyle={{ bg: '$gray2' }}
-    {...props}
-  >
-    <Icon size={20} color={selected ? "var(--color8)" : "var(--color)"} fill={props.fill ? "var(--color)" : "transparent"} {...props.iconProps} />
-  </YStack>
+  return <TooltipSimple disabled={!(props.tooltipText)} placement="top" delay={{ open: 500, close: 0 }} restMs={0} label={props.tooltipText}>
+    <YStack
+      jc="center"
+      ai="center"
+      br="$4"
+      cursor='pointer'
+      scaleIcon={1.8}
+      w={size}
+      h={size}
+      hoverStyle={{ bg: '$gray2' }}
+      {...props}
+    >
+      <Icon size={20} color={selected ? "var(--color8)" : "var(--color)"} fill={props.fill ? "var(--color)" : "transparent"} {...props.iconProps} />
+    </YStack>
+  </TooltipSimple>
 }
 
 export const useActionBar = (actionBar?, onActionBarEvent?) => {
@@ -69,10 +71,10 @@ export const useActionBar = (actionBar?, onActionBarEvent?) => {
       b={5}
       zi={99999}
       br="100px"
-      hoverStyle={{ transform: 'scale(1.1)'}}
+      hoverStyle={{ transform: 'scale(1.1)' }}
       enterStyle={{ b: -20 }}
     >
-      <ChevronUp color="var(--gray12)"/>
+      <ChevronUp color="var(--gray12)" />
     </YStack>}
   </>
 }   
