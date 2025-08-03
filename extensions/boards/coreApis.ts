e7f345d2db62ffe03cc5dc608aa47aa55c939ef5
jcarlosn <jcarlos.norte@gmail.com>
2025-08-03 16:26:19 +0200

    fix code generation for value cards

--- extensions/boards/coreApis.ts
+++ extensions/boards/coreApis.ts
@@ -529,7 +529,7 @@ export default async (app, context) => {
     });
 
     app.post('/api/core/v1/autopilot/getValueCode', requireAdmin(), async (req, res) => {
-        const prompt = await context.autopilot.getPromptFromTemplate({ board: req.body.board, templateName: "valueRules", states: JSON.stringify({ boards: req.body.states }, null, 4), rules: JSON.stringify(req.body.rules, null, 4) });
+        const prompt = await context.autopilot.getPromptFromTemplate({ board: req.body.board, templateName: "valueRules", states: JSON.stringify(req.body.states , null, 4), rules: JSON.stringify(req.body.rules, null, 4) });
         if (req.query.debug) {
             console.log("Prompt: ", prompt)
         }
