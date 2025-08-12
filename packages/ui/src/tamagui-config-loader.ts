670bf93e03a1c08ae37001531afd18eaeff31f06
MiquelCos <miquel@protofy.xyz>
2025-08-12 10:22:29 +0200

    Tries fixes themes git bug

--- packages/ui/src/tamagui-config-loader.ts
+++ packages/ui/src/tamagui-config-loader.ts
@@ -3,20 +3,22 @@ import path from 'path';
 import { getRoot } from 'protonode'
 
 export const getConfigFromDisk = (): Record<string, any> => {
+  let config = {};
+  let selectedConfig = 'default';
   try {
     const root = getRoot();
     const settingsPath = path.join(root, 'data', 'settings', 'theme.json');
-    const settingsData = fs.readFileSync(settingsPath, 'utf8');
-    const settings = JSON.parse(settingsData || '{}');
-    const selectedConfig = settings?.value ?? 'default';
-
+    if (fs.existsSync(settingsPath)) {
+      const settingsData = fs.readFileSync(settingsPath, 'utf8');
+      const settings = JSON.parse(settingsData || '{}');
+      selectedConfig = settings?.value ?? selectedConfig;
+    }
     const themesPath = path.join(root, 'data', 'themes', `${selectedConfig}.json`);
     const themeData = fs.readFileSync(themesPath, 'utf8');
     const themeConf = JSON.parse(themeData);
-
-    return { ...themeConf?.config };
+    config = { ...themeConf?.config };
   } catch (err) {
     console.warn('The selected theme has no json file');
-    return {};
   }
+  return config;
 };
